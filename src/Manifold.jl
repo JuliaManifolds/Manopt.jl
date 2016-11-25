d689dffadd7eda5d56e01b890c4229ba7143ffc1
Ronny Bergmann <mail@rbergmann.info>
2016-11-25 23:13:16 +0100

    implements a midpoint function.

--- src/Manifold.jl
+++ src/Manifold.jl
@@ -31,15 +31,15 @@ abstract ManifoldTangentialPoint
 # proximal Maps
 """
     proxTuple = proxTV(lambda,pointTuple)
-  Compute the proximal map prox_f(x,y) for f(x,y) = dist(x,y) with parameter
-  lambda
-  # INPUT
-    * lambda : a real value, parameter of the proximal map
-    * pointTuple : a tuple of size 2 containing two ManifoldPoints x and y
-  # OUTPUT
-    * proxTuple : resulting two-ManifoldPoint-Tuple of the proximal map
+Compute the proximal map prox_f(x,y) for f(x,y) = dist(x,y) with parameter
+lambda
+### INPUT
+* lambda : a real value, parameter of the proximal map
+* pointTuple : a tuple of size 2 containing two ManifoldPoints x and y
+# OUTPUT
+* proxTuple : resulting two-ManifoldPoint-Tuple of the proximal map
 ---
-    ManifoldValuedImageProcessing 0.8, R. Bergmann, 2016-11-25
+ManifoldValuedImageProcessing 0.8, R. Bergmann, 2016-11-25
 """
 function proxTV(lambda::Float64,pointTuple::Tuple{ManifoldPoint,ManifoldPoint})::Tuple{ManifoldPoint,ManifoldPoint}
   step = min(0.5, lambda/distance(pointTuple[1],pointTuple[2]))
@@ -51,17 +51,33 @@ end
   Compute the proximal map prox_f(x,y) for f(x,y) = dist(x,y)^2 with parameter
   lambda
   # INPUT
-    * lambda : a real value, parameter of the proximal map
-    * pointTuple : a tuple of size 2 containing two ManifoldPoints x and y
+  * lambda : a real value, parameter of the proximal map
+  * pointTuple : a tuple of size 2 containing two ManifoldPoints x and y
   # OUTPUT
-    * proxTuple : resulting two-ManifoldPoint-Tuple of the proximal map
+  * proxTuple : resulting two-ManifoldPoint-Tuple of the proximal map
 ---
     ManifoldValuedImageProcessing 0.8, R. Bergmann, 2016-11-25
 """
 function proxTVSquared(lambda::Float64,pointTuple::Tuple{ManifoldPoint,ManifoldPoint})::Tuple{ManifoldPoint,ManifoldPoint}
-  step = lambda/(1+2*lambda)*distance(pointTuple[1],pointTuple[2]))
+  step = lambda/(1+2*lambda)*distance(pointTuple[1],pointTuple[2])
   return (  exp(pointTuple[1], step*log(pointTuple[1],pointTuple[2])),
             exp(pointTuple[2], step*log(pointTuple[2],pointTuple[1])) )
+end
+
+#
+# other auxilgary functions
+"""
+midPoint(x,z) - Compute the (geodesic) mid point of x and z.
+* INPUT
+  x,z : two `ManifoldPoint`s
+
+* OUTPUT
+  m : resulting mid point( sets)
+---
+ManifoldValuedImageProcessing, R. Bergmann ~ 2015-11-25
+"""
+function midPoint(p::ManifoldPoint, q::ManifoldPoint)::ManifoldPoint
+  return exp(p,0.5*log(p,q))
 end
 #
 # fallback functions for not yet implemented cases
