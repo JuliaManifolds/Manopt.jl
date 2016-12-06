5acf0eb8313fce2de89113e51b4314ae8b7897e1
Ronny Bergmann <mail@rbergmann.info>
2016-12-06 15:33:48 +0100

    introduces a faster version of the artificialInSARImage() just to learn things.

--- src/artificialDataFunctions.jl
+++ src/artificialDataFunctions.jl
@@ -20,34 +20,38 @@ export artificialInSARImage
 function artificialInSARImage(pts::Integer)::Array{Float64,2}
   # variables
   # rotation of ellipse
-  aEll = 35.0/180.0*pi
-  aStep = 45.0/180.0*pi
+  aEll = 35.0; cosE = cosd(aEll); sinE = sind(aEll)
+  aStep = 45.0; cosA = cosd(aStep); sinA = sind(aStep)
   # main and minor axis of the ellipse
-  axes = [1/6,1/25]
+  axes_inv = [6,25]
   # values for the hyperboloid
-  midPoint = 0.275*[1,1]
+  midPoint = [0.275;0.275]
   radius = 0.18
-  values = collect(linspace(-0.5,0.5,pts))
+  values = linspace(-0.5,0.5,pts)
   # Steps
-  aSteps::Float64 = 60.0/180.0*pi
+  aSteps = 60.0; cosS = cosd(aSteps); sinS = sind(aSteps)
   l = 0.075
-  # meshes
-  gridX = [i for i in values, j in values]
-  gridY = [j for i in values, j in values]
-  # 1) generate ellipse
-  # rotate by alpha
-  Xr = cos(aEll).*gridX - sin(aEll).*gridY
-  Yr = cos(aEll).*gridY + sin(aEll).*gridX
-  img = ((1/axes[1]*Xr.^2 + 1/axes[2]*Yr.^2).<=1).*( 10*pi*Yr + 4.0*pi*(Yr.<0));
-  # 2) generate steps
-  Xr = cos(aStep).*gridX - sin(aStep).*gridY
-  Yr = cos(aStep).*gridY + sin(aStep).*gridX
-  img += 4.0*pi/(radius^2)*( ((gridX-midPoint[1]).^2 + (gridY-midPoint[2]).^2) .<= radius^2).*(radius^2 - ((gridX-midPoint[1]).^2 + (gridY-midPoint[2]).^2))
-  Xr = cos(aSteps).*gridX - sin(aSteps).*gridY
-  Yr = cos(aSteps).*gridY + sin(aSteps).*gridX
-  for i=1:8
-    midP = [i*l;i*l]+[-.125;-.55]
-    img += 2*pi*(i/8)*( (abs(Xr-midP[1])+abs(Yr-midP[2])) .<= l)
+  midP = [-.475,-.0625];#.125, .55]
+  img = zeros(Float64,pts,pts)
+  @inbounds @fastmath for j in eachindex(values), i in eachindex(values)
+    # ellipse
+    Xr = cosE*values[i] - sinE*values[j]
+    Yr = cosE*values[j] + sinE*values[i]
+    v = axes_inv[1]*Xr^2 + axes_inv[2]*Yr^2
+    k1 = v <= 1 ? 10*pi*Yr : 0.0
+    # circle
+    Xr = cosA*values[i] - sinA*values[j]
+    Yr = cosA*values[j] + sinA*values[i]
+    v = (Xr-midPoint[1])^2 + (Yr-midPoint[2])^2/radius^2
+    k2 = v <= 1 ? 4.0*pi*(1-v) : 0.0
+    #
+    Xr = cosS*values[i] - sinS*values[j]
+    Yr = cosS*values[j] + sinS*values[i]
+    k3=0.0
+    for m=1:8
+      k3 += (abs(Xr+midP[1]+m*l)+abs(Yr+midP[2]+m*l)) <= l ? 2*pi*(m/8) : 0.0
+    end
+    img[i,j] = mod(k1+k2+k3-pi,2*pi)+pi
   end
-  return mod(img-pi,2*pi)+pi
+  return img
 end
