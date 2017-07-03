02d1fee316ba3113fe53423c645386c1bf226e36
Ronny Bergmann <mail@rbergmann.info>
2017-07-03 21:00:59 +0200

    implements a first CPPA first order differences algorithm.

--- src/CPPAlgorithms.jl
+++ src/CPPAlgorithms.jl
@@ -10,7 +10,7 @@ export TV_Regularization_CPPA
  given data array f and paramater α and internal operator start λ.
 
  # Arguments
- * `f` an d-dimensional array of `ManifoldPoint`s
+ * `f` an d-dimensional array of `MPoint`s
  * `α` parameter of the model (may be given as a vector to provide different
       weights to different directions)
  * `λ` internal parameter of the cyclic proximal point algorithm
@@ -32,39 +32,30 @@ export TV_Regularization_CPPA
 
  ~ ManifoldValuedImageProcessing.jl ~ R. Bergmann ~ 2016-11-25
 """
-function TV_Regularization_CPPA{T <: ManifoldPoint, N}(
-      f::Array{T,N}, α::Array{Float64,1}, λ::Number; kwargs...)::Array{T,N}
+function TV_Regularization_CPPA{mT <: Manifold, T <: MPoint, N}(M::mT,
+      f::Array{T,N}, α::Float64, 𝛽::Float64, λ::Float64; kwargs...)::Array{T,N}
       kwargs_dict = Dict(kwargs);
       # load optionals
       MinimalChange::Float64=get(kwargs_dict, "MinimalChange", 10.0^-9)
       MaxIterations::Int64=get(kwargs_dict, "MaxIterations", 400)
       FixedMask::Array{Bool,N} = get(kwargs_dict, "FixedMask", falses(f))
       UnknownMask::Array{Bool,N} = get(kwargs_dict, "UnknownMask", falses(f))
-  if length(α) == 1
-    αV::Array{Float64,1} = α*ones(length(size(f)))
-  else
-    if length(α) ≠ length(size(f))
-      sig1 = length(α);
-      throw( ErrorException(string(" Length of α vector (",length(α),
-      ") as to be the same as the number of dimensions of f (",length(size(f)),").")) )
-    end
-    αV = α;
-  end
+      # call internal function withourt kwargs for speed reasons
 #  @code_warntype _TVRegCPPA(f,αV,λ,MinimalChange,7,FixedMask,UnknownMask)
   @code_warntype _TV1CPPA(f,αV,λ,MinimalChange,MaxIterations,FixedMask,UnknownMask)
   return _TV1CPPA(f,αV,λ,MinimalChange,MaxIterations,FixedMask,UnknownMask)
 end
-function _TV1CPPA{T <: ManifoldPoint,N}(
-      f::Array{T,N}, α::Array{Float64,1}, λ::Float64,
-      MinimalChange::Float64,MaxIterations::Int64,FixedMask::Array{Bool,N},UnknownMask::Array{Bool,N}
-    )::Array{T,N}
+function _TV1CPPA{mT <: Manifold, T <: MPoint,N}(M::mT, f::Array{T,N}, α::Float64,𝛽::Float64, λ::Float64,
+      MinimalChange::Float64, MaxIterations::Int, FixedMask::Array{Bool,N}, UnknownMask::Array{Bool,N}
+  )::Array{T,N}
   iter::Int64 = 1
   x::Array{T,N} = f
   xold::Array{T,N} = x
   stillUnknownMask::BitArray{N} = copy(UnknownMask);
-  R = CartesianRange(size(f))
+  foGraph::Array{Tuple} = constructImageGraph(f,"firstOrderDifference")
+  soGraph::Array{Tuple} = constructImageGraph(f,"secondOrderDifference")
   while (  ( (1.0/length(f)*sum( [ distance(ξ,xoldi) for (ξ,xoldi) in zip(x[~stillUnknownMask],xold[~stillUnknownMask]) ] ) > MinimalChange)
-            && (iter < MaxIterations) ) || (iter==1)  )
+    && (iter < MaxIterations) ) || (iter==1)  )
     iter = iter+1
     xold = x;
     x = similar(xold)
@@ -72,27 +63,25 @@ function _TV1CPPA{T <: ManifoldPoint,N}(
     @fastmath @inbounds for k in eachindex(x)
       x[k] = proxDistanceSquared(f[k],λ/k,xold[k])
     end
-    @fastmath @inbounds for d = 1:N
-      unitOffset::CartesianIndex{N} = CartesianIndex{N}(ntuple(i::Integer->i::Integer==d ? 1: 0, N))
-      for i in R
-        i2 = i+unitOffset
-        if (i2 in R)
-          if stillUnknownMask[i]
-            x[i] = x[i2]; stillUnknownMask[i] = false;
-          elseif stillUnknownMask[i2]
-            x[i2] = x[i]; stillUnknownMask[i] = false;
-          else # both known
-            (a,b) = proxTV((x[i], x[i2]),α[d]*λ/iter)
-            if ~FixedMask[i]
-              x[i] = a
-            end
-            if ~FixedMask[i2]
-              x[i2] = b
-            end
-          end #endif known
-        end #endif in Range
-      end #endfor i
-    end #endfor d
+    # first order
+    @fastmath @inbounds for k in foGraph
+      (ind1,ind2) = foGraph[k]
+      if stillUnknownMask[ind1]
+        x[ind1] = x[ind2]
+        stillUnknownMask[ind2] = false
+      elseif stillUnknownMask[ind2]
+        x[ind2] = x[ind1]
+        stillUnknownMask[ind2] = false
+      else # both known
+        (a,b) = proxTV((x[i], x[i2]),α[d]*λ/iter)
+        if ~FixedMask[ind1] # lazy fixed handling
+          x[ind1] = a
+        end
+        if ~FixedMask[ind2]
+          x[ind2] = b
+        end
+      end #endif known
+    end #end first order for
   end #endwhile
   return x
 end
---
+++ src/examples/artificialInSARDenoising.jl
@@ -0,0 +1,41 @@
+#
+# artificialInSARDenoising.jl
+# A script providing an example of denoising phase valued data using the
+# `artificialInSARImage(256)` and wrapped Gaussian noise.
+#
+# This script recreates the examples appeared in
+# > R. Bergmann, F. Laus, G. Steidl, A. Weinmann:
+# > Second Order Differences of Cyclic Data and Applications in Variational Denoising,
+# > SIAM Journal on Imaging Sciences, Vol. 7, No. 4, pp. 2916–2953, 2014.
+#
+# ManifoldValuedImageProcessing ~ R. Bergmsnn ~ 2016-12-09
+
+# include("../ManifoldValuedImageProcessing.jl")
+
+# Global Names and parameters
+dataName = "src/examples/artInSAR/data"
+experimentName = "src/examples/artInSAR/experiment1"
+σ = 0.3;
+loadData = false;
+
+if loadData
+  # load phases
+  InSARimage = readdlm(string(dataName,"-original.txt"))
+  InSARimageNoisy = readdlm(string(dataName,"-noisy.txt"))
+  # convert to manifold valued data
+  S1image = S1Point.(InSARimage)
+  S1imageNoisy = S1Point.(InSARimageNoisy)
+else
+  InSARImage = artificialInSARImage(256);
+  S1image = S1Point.(InSARImage)
+  S1imageNoisy = addNoise.(S1image,σ)
+  InSARImageNoisy = [p.value for p in S1imageNoisy] #extract noisy data again
+  writedlm(string(dataName,"-original.txt"),InSARImage)
+  writedlm(string(dataName,"-noisy.txt"),InSARImageNoisy)
+end
+
+@time S1imageReconstruction = TV_Regularization_CPPA(S1imageNoisy,[3.0/8.0,1.0/4.0],pi/2; MaxIterations=200)
+#@time S1imageReconstruction = TV_Regularization_CPPA(S1imageNoisy,[3.0/8.0,1.0/4.0],pi/2)
+#@time S1imageReconstruction = TV_Regularization_CPPA(S1imageNoisy,[3.0/8.0,1.0/4.0],pi/2)
+#@code_warntype TV_Regularization_CPPA(S1imageNoisy,[3.0/8.0,1.0/4.0],pi/2)
+#@code_warntype TV_Regularization_CPPA(S1imageNoisy,[3.0/8.0,1.0/4.0],pi/2; MaxIterations=100)
