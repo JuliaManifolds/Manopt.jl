6de3e22859bb5425a063c7894433a1027a743c4c
Ronny Bergmann <git@ronnybergmann.net>
2018-09-03 08:52:33 +0100

    Fixes array exports to have rounded brackets

--- src/helpers/exports/Asymptote.jl
+++ src/helpers/exports/Asymptote.jl
@@ -2,9 +2,9 @@ using ColorTypes, Colors
 export asyExportS2
 
 function asyExportS2(filename::String;
-    points::Array{SnPoint,2} = Array{SnPoint,2}(undef,0,0),
-    curves::Array{SnPoint,2} = Array{SnPoint,2}(undef,0,0),
-    tVectors::Array{TVectorE{SnTVector,SnPoint},1} = Array{TVectorE{SnTVector,SnPoint},1}(undef,0),
+    points::Array{Array{SnPoint,1},1} = Array{Array{SnPoint,1},1}(undef,0),
+    curves::Array{Array{SnPoint,1},1} = Array{Array{SnPoint,1},1}(undef,0),
+    tVectors::Array{Array{TVectorE{SnTVector,SnPoint},1},1} = Array{Array{TVectorE{SnTVector,SnPoint},1},1}(undef,0),
     colors::Dict{Symbol, Array{T,1} where T}  = Dict{Symbol,Array{RGBA{Float},1}}(),
     arrowHeadSize::Float64 = 6.,
     cameraPosition::Tuple{Float64,Float64,Float64} = (1., 1., 0.),
@@ -56,7 +56,7 @@ function asyExportS2(filename::String;
         for pSet in points
             i=i+1
             for point in pSet
-                write(io,string("dot($(getValue(point)), pointStyle$(i));\n"));
+                write(io,string("dot( (",string([string(v,",") for v in getValue(point)]...)[1:end-1],"), pointStyle$(i));\n"));
             end
         end
         i=0
@@ -67,9 +67,9 @@ function asyExportS2(filename::String;
             for point in pSet
                 j=j+1
                 if j>1
-                    write( io," .. $(getValue(point))" )
+                    write( io," .. (",[string(v,",") for v in getValue(point)]...,")" )
                 else
-                    write( io,"$(getValue(point))" )
+                    write( io,"(",[string(v,",") for v in getValue(point)]...,")" )
                 end
                 write( io,string(";\n draw(p$(i), curveStyle$(i));\n") );
             end
@@ -80,64 +80,13 @@ function asyExportS2(filename::String;
             j=0
             for vector in tVecs
                 j=j+1
-                write(io,string("draw($(getBase(vector))--$(getBase(vector)+getValue(vector)),",
-                    "tVectorStyle$(j),Arrow3($(arrowHeadSize)));\n"));
+                write(io,string("draw( (",
+                    string( [string(v,",") for v in getBase(vector)]...)[1:end-1],")--(",
+                    string( [string(v,",") for v in getValue(vectort)]...)[1:end-1],
+                    "), tVectorStyle$(j),Arrow3($(arrowHeadSize)));\n"));
             end
         end
     finally
         close(io)
     end
-    # for i=1:m %linestyles
-    #     lColor = vars.colors{i};
-    #     line = sprintf(['\npen LineStyle',num2str(i),' = ',...
-    #         'rgb(',num2str(lColor(1)),',',num2str(lColor(2)),',',num2str(lColor(3)),')+',...
-    #         'linewidth(',num2str(dS(i)),')+opacity(',num2str(vars.OpacityVector(i)),');']);
-    #     if ~isempty(vars.File)
-    #         fprintf(fID,line);
-    #     end
-    #     lfileStr = [lfileStr,line]; %#ok<*AGROW>
-    # end
-    # % for the signals, 1,...,s
-    # for i=1:s %for each signal
-    #     lSig = vars.pts{i};
-    #     l = size(lSig,2);
-    #     for j=1:l %for all dots each
-    #         line = sprintf(['dot((',num2str(lSig(1,j)),',',num2str(lSig(2,j)),',',num2str(lSig(3,j)),'),LineStyle',num2str(i),');\n']);
-    #         if ~isempty(vars.File)
-    #             fprintf(fID,line);
-    #         end
-    #         lfileStr = [lfileStr,line];
-    #     end
-    # end
-    # % for curves
-    # for i=1:t
-    #     lCurve = vars.curves{i};
-    #     l = size(lCurve,2);
-    #     line = ['path3 p',num2str(i),' = '];
-    #     for j=1:l %for all dots each
-    #         if j > 1
-    #             line = sprintf([line,' .. ']);
-    #         end
-    #         line = sprintf([line,'(',num2str(lCurve(1,j)),',',num2str(lCurve(2,j)),',',num2str(lCurve(3,j)),')']);
-    #     end
-    #     line = sprintf([line,';\ndraw(p',num2str(i),',LineStyle',num2str(s+i),');\n']);
-    #     if ~isempty(vars.File)
-    #         fprintf(fID,line);
-    #     end
-    #     lfileStr = [lfileStr,line];
-    # end
-    # % for tangent vectors
-    # for i=1:u
-    #     lxi = vars.xi{i};
-    #     l = size(lxi,3);
-    #     for j=1:l
-    #         lineS = sprintf(['draw((',num2str(lxi(1,1,j)),',',num2str(lxi(2,1,j)),',',num2str(lxi(3,1,j))... %base vec
-    #             ,')--(',num2str(lxi(1,1,j)+lxi(1,2,j)),',',num2str(lxi(2,1,j)+lxi(2,2,j)),',',num2str(lxi(3,1,j)+lxi(3,2,j))...
-    #             ,'),LineStyle',num2str(s+t+i),',Arrow3(',num2str(vars.ArrowHead),'));\n']);
-    #          if ~isempty(vars.File)
-    #             fprintf(fID,lineS);
-    #          end
-    #         lfileStr = [lfileStr,lineS];
-    #     end
-    # end
 end
