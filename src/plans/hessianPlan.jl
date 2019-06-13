abdc78f217217e89ae06cb48912523cf7649725d
Tom-Christian Riemer <riemertom720@googlemail.com>
2019-06-13 10:43:44 +0200

    continues the work on the problem/options-structure.

--- src/plans/hessianPlan.jl
+++ src/plans/hessianPlan.jl
@@ -4,6 +4,7 @@ struct HessianProblem <: Problem
     costFunction::Function
     gradient::Function
     hessian::Union{Function,Missing}
+    precon::Function
 end
 
 # injectivity_radius.
@@ -12,12 +13,27 @@ abstract type HessianOptions <: Options end
 
 struct TruncatedConjugateGradientOptions <: HessianOptions
     x::P where {P <: MPoint}
+
 end
 
 struct TrustRegionOptions <: HessianOptions
-    x::P where {P <: MPoint}
+    # x::P where {P <: MPoint}
+    # is actually passed as a parameter of the function
     stop::stoppingCriterion
+    # do we pass the limits to the criterion or do we pass them the options?
+    tolgradnorm::Float64
+    maxiter::Int64
+    miniter::Int64
+    maxtime::Float64
+    mininner::Int64
+    maxinner::Int64
+    Delta_bar::Float64
+    Delta0::Float64
+    useRand::Bool
+    kappa::Float64
+    theta::Float64
+    rho_prime::Float64
+    rho_regularization::Float64
 end
 
 getHessian(p::Pr,x::P) where {Pr <: Problem, P <: MPoint} = error("Not yet implemented")
-
