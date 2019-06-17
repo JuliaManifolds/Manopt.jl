a6d3cfc5c5202554cce690ddd588465a0a819e59
Tom-Christian Riemer <riemertom720@googlemail.com>
2019-06-17 10:21:50 +0200

    implements a function for the hessian-approximation.

--- src/plans/hessianPlan.jl
+++ src/plans/hessianPlan.jl
@@ -50,3 +50,23 @@ end
 getHessian(p::Pr,x::P,ξ::V) where {Pr <: Problem, P <: MPoint, V <: MTVector} = p.hessian(x,ξ)
 getGradient(p::Pr,x::P) where {Pr <: Problem, P <: MPoint} = p.gradient(x)
 getPreconditioner(p::Pr,x::P, ξ::V) where {Pr <: Problem, P <: MPoint, V <: MTVector} = p.precon(x,ξ)
+
+function approxHessianFD(problem::HessianProblem, x::MPoint, ξ::MTVec, stepsize::Float64=2.0^(-14))
+    M = getManifold(problem)
+    norm_xi = norm(M,x,ξ)
+
+    if norm_xi < eps(Float64)
+        return zeroTVector(M, x)
+    end
+
+    c = stepsize / norm_xi
+
+    grad = getGradient(problem, x)
+
+    x1 = retraction(M, x, ξ, c)
+    grad1 = getGradient(problem, x1)
+
+    grad1 = parallelTransport(M, x1, x, grad1)
+
+    return TVector((getValue(grad1)-getValue(grad))/c)
+end
