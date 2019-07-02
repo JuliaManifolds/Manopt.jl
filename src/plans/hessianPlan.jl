94956b84438b343e8494f21520e25a5eaa05529e
Tom-Christian Riemer <riemertom720@googlemail.com>
2019-07-02 11:21:15 +0200

    extends the documentation.

--- src/plans/hessianPlan.jl
+++ src/plans/hessianPlan.jl
@@ -46,7 +46,7 @@ method, with
 a default value is given in brackets if a parameter can be left out in initialization.
 
 * `x` : an [`MPoint`](@ref) as starting point
-* `stoppingCriterion` :
+* `stoppingCriterion` : a function
 * `η` :
 * `Hη` :
 * `mδ` :
@@ -67,11 +67,11 @@ a default value is given in brackets if a parameter can be left out in initializ
 construct a truncated Conjugate Gradient Option with the fields as above.
 
 # See also
-[`steepestDescent`](@ref), [`GradientProblem`](@ref)
+[`truncatedConjugateGradient`](@ref), [`trustRegionsSolver`](@ref)
 """
 struct TruncatedConjugateGradientOptions <: HessianOptions
     x::P where {P <: MPoint}
-    stoppingCriterion::stoppingCriterion
+    stop::stoppingCriterion
     η::T where {T <: TVector}
     Hη::T where {T <: TVector}
     mδ::T where {T <: TVector}
@@ -87,6 +87,33 @@ struct TruncatedConjugateGradientOptions <: HessianOptions
     TruncatedConjugateGradientOptions(x::P,η::T,Hη::T,δ::T,Δ::Float64,d_Pd::Float64,e_Pd::Float64,e_Pe::Float64,residual::T,z::T,model_value::Float64,uR::Bool) where {P <: MPoint, T <: TVector} = new(x,η,Hη,δ,Δ,d_Pd,e_Pd,e_Pe,residual,z,model_value,uR)
 end
 
+"""
+    TrustRegionOptions <: HessianOptions
+
+Describes the Trust Regions Solver, with
+
+# Fields
+a default value is given in brackets if a parameter can be left out in initialization.
+
+* `x` : an [`MPoint`](@ref) as starting point
+* `stop` :
+* `δ` :
+* `δ_bar` :
+* `δ0` :
+* `useRand` :
+* `ρ_prime` :
+* `ρ_regularization` :
+* `norm_grad` :
+
+# Constructor
+
+    TrustRegionOptions()
+
+construct a Trust Regions Option with the fields as above.
+
+# See also
+[`trustRegionsSolver`](@ref)
+"""
 struct TrustRegionOptions <: HessianOptions
     x::P where {P <: MPoint}
     stop::stoppingCriterion
@@ -102,11 +129,30 @@ struct TrustRegionOptions <: HessianOptions
     norm_grad::Float64) where {P <: MPoint} = new(x,stop,δ,δ_bar,δ0,useRand,ρ_prime,ρ_regularization,norm_grad)
 end
 
+"""
+    getHessian(p,x,ξ)
+
+evaluate the Hessian of a [`HessianProblem`](@ref)`p` at the [`MPoint`](@ref) `x`
+times a [`TVector`](@ref) `ξ`.
+"""
 getHessian(p::Pr,x::P,ξ::V) where {Pr <: HessianProblem, P <: MPoint, V <: TVector} = ismissing(p.hessian) ? approxHessianFD(p,x,ξ) : p.hessian(x,ξ)
+"""
+    getGradient(p,x)
+
+evaluate the gradient of a [`HessianProblem`](@ref)`p` at the [`MPoint`](@ref) `x`.
+"""
 getGradient(p::Pr,x::P) where {Pr <: HessianProblem, P <: MPoint} = p.gradient(x)
+"""
+    getCost(p,x)
+
+evaluate the cost function of a [`HessianProblem`](@ref)`p` at the [`MPoint`](@ref) `x`.
+"""
 getCost(p::Pr,x::P) where {Pr <: Problem, P <: MPoint} = p.costFunction(x)
 getPreconditioner(p::Pr,x::P, ξ::V) where {Pr <: Problem, P <: MPoint, V <: TVector} = p.precon(x,ξ)
 
+@doc doc"""
+    approxHessianFD(p,x,ξ,[stepsize=2.0^(-14)])
+"""
 function approxHessianFD(p::HessianProblem, x::P, ξ::T, stepsize::Float64=2.0^(-14)) where {P <: MPoint, T <: TVector}
     norm_xi = norm(p.M,x,ξ)
     if norm_xi < eps(Float64)
