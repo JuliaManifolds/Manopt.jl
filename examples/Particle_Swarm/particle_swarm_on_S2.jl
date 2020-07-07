cfcafb1d719a45e1b5885862cd770589c42143b3
Ronny Bergmann <git@ronnybergmann.net>
2020-07-07 11:02:57 +0200

    modifies code.

--- examples/Particle_Swarm/particle_swarm_on_S2.jl
+++ examples/Particle_Swarm/particle_swarm_on_S2.jl
@@ -8,13 +8,13 @@ rot_z(φ) = [cos(φ) -sin(φ) 0;sin(φ) cos(φ) 0; 0 0 1]
 A = Diagonal([2.0, 1.0, 0.5])
 B = rot_z(pi/4)*Diagonal([4.0, 1.0, 2.0])rot_z(-π/4)
 C = rot_x(π/8)*rot_y(-π/4)*Diagonal([0.5, 2.0, 3.0])*rot_y(π/4)*rot_x(-π/8)
-F(x) = x'*(A+B+C)*x
+F(x) = x'*(A+B-C)*x
 M = Manifolds.Sphere(2)
 
 #
 # Plot Settings
-n_lon = 201
-n_lat = 101
+n_lon = 401
+n_lat = 201
 lat = range(0, π; length=n_lat)
 lon = range(-π, π; length=n_lon)
 sphere_color = colorant"rgb(223 186 105)"
@@ -29,23 +29,11 @@ mesh_pts = [spherical_to_euclidean(θ,φ) for θ ∈ lat, φ ∈ lon]
 data = [F(spherical_to_euclidean(θ,φ)) for θ ∈ lat, φ ∈ lon]
 # Init contour plot
 scene = Scene()
-mesh!(scene, Makie.Sphere(Point3f0(0), 0.975f0), color=data, colormap = ColorSchemes.viridis, levels=50, interpolate=false)
+mesh!(scene, Makie.Sphere(Point3f0(0), 0.99f0), color=data, colormap = ColorSchemes.viridis, levels=50, interpolate=false, show_axis = false)
 
 #
 # Run PSO
-s1 = 7
-s2 = 20
-x0 = Array{Array{Float64,1},1}()
-for θ ∈ range(0,π,length=s1)
-    n = Integer(floor(s2*(1-abs(cos(θ)))))
-    if n==0
-        push!(x0,spherical_to_euclidean(θ,0.0))
-    else
-        for φ ∈ range(0, 2π; length = n+1)
-            push!(x0,spherical_to_euclidean(θ,φ))
-        end
-    end
-end
+x0 = [random_point(M) for i ∈ 1:50]
 # o = particle_swarm(M,F,x0; record = [:x])
 # path = get_record(o)
 path = [ [rot_y(i*π/360)*rot_z(1*i*π/180)*x for x in x0] for i=1:360]
@@ -53,8 +41,16 @@ path = [ [rot_y(i*π/360)*rot_z(1*i*π/180)*x for x in x0] for i=1:360]
 # Animate / record
 t = Node(path[1]) # create a life signal
 
-scatter!(scene, lift(t->[x[1] for x in t],t), lift(t -> [x[2] for x in t],t), lift(t->[x[3] for x in t],t))
+scatter!(# plot population
+    scene,
+    lift(t->[x[1] for x in t],t),
+    lift(t -> [x[2] for x in t],t),
+    lift(t->[x[3] for x in t],t),
+    markersize = 0.05
+)
+# plot personal best per point
+# plot global best
 N = length(path)
-record(scene, "record_video.mp4", 1:N) do i
+record(scene, string(@__DIR__, "/record_video.mp4"), 1:N) do i
     t[] = path[i]
 end
