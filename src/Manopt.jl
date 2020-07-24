4f6095438d460e27923241875833d8237a912be1
Renée Dornig <renee.dornig@s2015.tu-chemnitz.de>
2020-07-24 16:39:33 +0200

    adds import of AbstractInverseRetractionMethod

--- src/Manopt.jl
+++ src/Manopt.jl
@@ -17,6 +17,7 @@ import ManifoldsBase:
     ^,
     AbstractVectorTransportMethod,
     AbstractRetractionMethod,
+    AbstractInverseRetractionMethod,
     ExponentialRetraction,
     ParallelTransport,
     Manifold,
