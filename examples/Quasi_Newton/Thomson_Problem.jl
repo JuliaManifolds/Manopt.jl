8c2d3b901ff98f2070d7391bce7ff3fb377ff414
Tom-Christian Riemer <riemertom720@googlemail.com>
2020-09-25 17:12:40 +0200

    add the Thomson Problem and the Brockett Cost Function as examples.

---
+++ examples/Quasi_Newton/Thomson_Problem.jl
@@ -0,0 +1,39 @@
+#
+#   Thomson Problem on Oblique(n,m)
+#
+using Manopt, Manifolds, ManifoldsBase, LinearAlgebra, Random
+Random.seed!(42)
+n = 50
+m = 20
+M = Oblique(n,m)
+
+function F(X::Array{Float64,2})
+    f = 0
+    for i in 1:m
+        for j in 1:m
+            if i != j
+                f = f + 1/(norm(X[:,i]-X[:,j])^2)
+            end
+        end
+    end
+    return f
+end
+
+function ∇F(X::Array{Float64,2})
+    g = zeros(n,m)
+    I = one(zeros(n,n))
+    for i in 1:m
+        f = zeros(n,1)
+        for j in 1:m
+            if i != j
+                f = f + 1 / (1. - X[:,i]'*X[:,j]) * X[:,j]
+            end
+        end
+        g[:,i] = (I - X[:,i]*X[:,i]')*f
+    end
+    return g
+end
+
+x = random_point(M)
+
+quasi_Newton(M,F,∇F,x; debug = [:Iteration, " ", :Cost, "\n", 1, :Stop])
