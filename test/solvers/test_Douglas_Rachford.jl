12cadfbd762dc878aaf9812811c54ac08bdff221
Ronny Bergmann <git@ronnybergmann.net>
2020-10-10 11:42:53 +0200

    fixes DR tests.

--- test/solvers/test_Douglas_Rachford.jl
+++ test/solvers/test_Douglas_Rachford.jl
@@ -5,14 +5,14 @@
     q = [0.0, 1.0, 0.0]
     r = [0.0, 0.0, 1.0]
     start = [0.0, 0.0, 1.0]
-    result = geodesic(M, p, q, 0.5)
+    result = result = geodesic(M, p, q, distance(M,p,q)/2)
     F(x) = distance(M, x, p)^2 + distance(M, x, q)^2
     prox1 = (η, x) -> prox_distance(M, η, p, x)
     prox2 = (η, x) -> prox_distance(M, η, q, x)
     @test_throws ErrorException DouglasRachford(M, F, Array{Function,1}([prox1]), start) # we need more than one prox
     xHat = DouglasRachford(M, F, [prox1, prox2], start)
     @test F(start) > F(xHat)
-    @test_broken distance(M, xHat, result) ≈ 0
+    @test distance(M, xHat, result) ≈ 0
     # but we can also compute the riemannian center of mass (locally) on Sn
     # though also this is not that useful, but easy to test that DR works
     F2(x) = distance(M, x, p)^2 + distance(M, x, q)^2 + distance(M, x, r)^2
@@ -28,7 +28,7 @@
     )
     xHat2 = get_solver_result(o)
     drec2 = get_record(o)
-    xCmp = 1 / sqrt(3) * ones(3)
+    result2 = mean(M,[p,q,r])
     # since the default does not run that long -> rough estimate
-    @test_broken distance(M, xHat2, xCmp) ≈ 0 atol = 10^(-5)
+    @test distance(M, xHat2, result2) ≈ 0
 end
