88dd29a73c0027c455d738203875b5866f8851df
Tom-Christian Riemer <riemertom720@googlemail.com>
2020-10-15 09:24:15 +0200

    updates an example.

--- examples/Quasi_Newton/Rayleigh_Quotient_Minimization.jl
+++ examples/Quasi_Newton/Rayleigh_Quotient_Minimization.jl
@@ -12,6 +12,8 @@ A = randn(n,n)
 A = (A + A')/2
 M = Sphere(n-1)
 F(X::Array{Float64,1}) = transpose(X)*A*X
-∇F(X::Array{Float64,1}) = 2*(A*X - X*X'A*X)
+∇F(X::Array{Float64,1}) = 2*project(M,X,A*X)
+∇2F(X::Array{Float64,1}, ξ::Array{Float64,1}) = 2*project(M,X,A*ξ - ξ*X'*A*X)
 x = random_point(M)
-quasi_Newton(M,F,∇F,x; memory_size = 250, debug = [:Iteration, " ", :Cost, "\n", 1, :Stop])
+op = operator_to_matrix(M,x,(ξ) -> ∇2F(x, ξ))
+quasi_Newton(M,F,∇F,x; memory_size = -250, debug = [:Iteration, " ", :Cost, "\n", 1, :Stop])
