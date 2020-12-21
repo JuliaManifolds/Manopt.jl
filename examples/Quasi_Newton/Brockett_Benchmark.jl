992066629cc0739b2e4dd5adc571b9b091da3859
Ronny <git@ronnybergmann.net>
2020-12-21 20:56:56 +0100

    Reduces time up to a factor of 2 (small problems) up to 10 (medium 1000,2,4) again down to a factor of 3 (1000,5,4).

--- examples/Quasi_Newton/Brockett_Benchmark.jl
+++ examples/Quasi_Newton/Brockett_Benchmark.jl
@@ -8,8 +8,8 @@ function run_brocket_experiment(n::Int, k::Int, m::Int; seed=42)
     A = randn(n, n)
     A = (A + A')/2
     N = diagm(k:-1:1)
-    F(X::Array{Float64,2}) = tr(X' * A * X * N)
-    ∇F(X::Array{Float64,2}) = 2 * A * X * N - X * X' * A * X * N - X * N * X' * A * X
+    F(X::Array{Float64,2}) = tr( (X' * A * X) * N)
+    ∇F(X::Array{Float64,2}) = 2 * A * X * N .- X * (X' * A * X) * N .- X * N * (X' * A * X)
     x = random_point(M)
     return quasi_Newton(
         M,
@@ -28,14 +28,13 @@ end
 io = IOBuffer()
 
 for e ∈ [
-    (32, 32, 1), (32, 32, 2), (32, 32, 4),(32, 32, 8), (32, 32, 16), (32, 32, 32),
-    (1000, 2, 4),
-    (1000, 3, 4),
-    (1000, 4, 4),
-    (1000, 5, 4),
+    (32, 32, 1), (32, 32, 2), (32, 32, 4),(32, 32, 8), (32, 32, 16),
+    (32, 32, 32), (1000, 2, 4),
+    (1000, 3, 4), (1000, 4, 4), (1000, 5, 4),
 ]
     println("Benchmarking $(e):")
     b =  @benchmark run_brocket_experiment($(e[1]),$(e[2]), $(e[3])) samples = 5
+    #run_brocket_experiment(e[1], e[2], e[3])
     show(io, "text/plain", b)
     s = String(take!(io))
     println(s, "\n\n")
