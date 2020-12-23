dbfe998fbd5d4831c8cfd0a7a118ef6857fdd8b6
Tom-Christian Riemer <riemertom720@googlemail.com>
2020-12-23 19:20:34 +0100

    adds the first tests for quasi-Newton.

--- test/solvers/test_quasi_Newton.jl
+++ test/solvers/test_quasi_Newton.jl
@@ -1,32 +1,54 @@
-using Manopt, Manifolds, ManifoldsBase, LinearAlgebra, Test
+using Manopt, Manifolds, LinearAlgebra, Test, Random
+Random.seed!(42)
 
-@testset "Riemannian Limited Memory BFGS Method" begin
+@testset "Riemannian quasi-Newton Methods" begin
     A = [18.0 0.0 0.0 0.0; 0.0 0.0 0.0 0.0; 0.0 0.0 0.0 0.0; 0.0 0.0 0.0 0.0]
     B = [0.0 0.0 0.0 0.009; 0.0 0.0 0.0 0.0; 0.0 0.0 0.0 0.0; 0.0 0.0 0.0 0.0]
     C = [0.0 0.0 0.0 0.0; 0.0 0.0 0.0 0.0; 0.0 0.0 0.0 0.0; -5.0 0.0 0.0 0.0]
-
+    ABC = [A, B, C]
+    x_solution = mean(ABC)
     F(x) = 0.5 * norm(A - x)^2 + 0.5 * norm(B - x)^2 + 0.5 * norm(C - x)^2
     ∇F(x) = -A - B - C + 3 * x
-
     M = Euclidean(4, 4)
     x = [0.0 0.0 0.0 0.0; 0.0 0.0 0.0 0.0; 0.0 0.0 0.0 0.0; 0.0 0.0 0.0 0.0]
+    x_lrbfgs = quasi_Newton(
+        M,
+        F,
+        ∇F,
+        x;
+        stopping_criterion=StopWhenGradientNormLess(10^(-6))
+        )
+    x_clrbfgs = quasi_Newton(
+        M,
+        F,
+        ∇F,
+        x;
+        cautious_update = true,
+        stopping_criterion=StopWhenGradientNormLess(10^(-6))
+        )
+    x_rbfgs = quasi_Newton(
+        M,
+        F,
+        ∇F,
+        x;
+        memory_size=-1,
+        stopping_criterion=StopWhenGradientNormLess(10^(-6))
+        )
+    x_crbfgs = quasi_Newton(
+        M,
+        F,
+        ∇F,
+        x;
+        memory_size=-1,
+        cautious_update = true,
+        stopping_criterion=StopWhenGradientNormLess(10^(-6))
+        )
 
-    steps = [zero_tangent_vector(M, x) for i in 1:30]
-    graddiffs = [zero_tangent_vector(M, x) for i in 1:30]
-    approximation = get_vectors(M, x, get_basis(M, x, DefaultOrthonormalBasis()))
-    grad_x = zero_tangent_vector(M, x)
+    @test norm(x_lrbfgs - x_solution) ≈ 0 atol = 10.0^(-14)
+    @test norm(x_clrbfgs - x_solution) ≈ 0 atol = 10.0^(-14)
+    @test norm(x_rbfgs - x_solution) ≈ 0 atol = 10.0^(-14)
+    @test norm(x_crbfgs - x_solution) ≈ 0 atol = 10.0^(-14)
+    
 
-    p = GradientProblem(M, F, ∇F)
-    o_lm = RLBFGSOptions(x, graddiffs, steps)
-    o_lm_strong = RLBFGSOptions(x, graddiffs, steps; stepsize=StrongWolfePowellLineseach())
-    o = QuasiNewtonOptions(x, grad_x, approximation)
-    s = WolfePowellLineseach()
-    s(p, o_lm, 1)
-
-    s_strong = StrongWolfePowellLineseach()
-    s_strong(p, o_lm, 1)
-
-    @test step_solver!(p, o_lm, 1)
-    @test step_solver!(p, o_lm_strong, 1)
-    @test step_solver!(p, o, 1)
+    
 end
