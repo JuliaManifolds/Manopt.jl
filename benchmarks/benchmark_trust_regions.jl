c60f2155eaeefd086fad2168c516e7e150f83005
Mateusz Baran <mateuszbaran89@gmail.com>
2021-02-05 20:35:28 +0100

    nested -> array power representation in benchmark

--- benchmarks/benchmark_trust_regions.jl
+++ benchmarks/benchmark_trust_regions.jl
@@ -6,11 +6,11 @@ include("../test/solvers/trust_region_model.jl")
 n=size(A,1)
 p=2
 N = Grassmann(n,p)
-M = PowerManifold(N, NestedPowerRepresentation(),2)
+M = PowerManifold(N, ArrayPowerRepresentation(),2)
 x = random_point(M)
 
 @btime x_opt = trust_regions($M, $cost, $rgrad, $rhess, $x; max_trust_region_radius=8.0)
 
-h = RHess(A,p)
-g = RGrad(A,p)
+h = RHess(M,A,p)
+g = RGrad(M,A)
 @btime trust_regions($M, $cost, $g, $h, x2; max_trust_region_radius=8.0, evaluation=$(MutatingEvaluation()))  setup = (x2 = deepcopy($x))
--- test/solvers/trust_region_model.jl
+++ test/solvers/trust_region_model.jl
@@ -1,30 +1,32 @@
 const A = [1.0 2.0 3.0; 4.0 5.0 6.0; 7.0 8.0 9.0]
 
-cost(::PowerManifold, p) = cost(p)
-cost(X::Array{Matrix{Float64},1}) = -0.5 * norm(transpose(X[1]) * A * X[2])^2
+cost(M::PowerManifold, p) = -0.5 * norm(transpose(p[M, 1]) * A * p[M, 2])^2
 
-function egrad(X::Array{Matrix{Float64},1})
-    U = X[1]
-    V = X[2]
+function egrad(M::PowerManifold, X::Array)
+    U = X[M, 1]
+    V = X[M, 2]
     AV = A * V
     AtU = transpose(A) * U
-    return [-AV * (transpose(AV) * U), -AtU * (transpose(AtU) * V)]
+    AR = similar(X)
+    AR[:, :, 1] .= -AV * (transpose(AV) * U)
+    AR[:, :, 2] .= -AtU * (transpose(AtU) * V)
+    return AR
 end
-struct EGrad{T}
+struct EGrad{T,TM}
+    M::TM
     A::Matrix{T}
 end
-Egrad(A::Matrix{T}) where {T} = EGrad{T}(A)
-function (e::EGrad{Float64})(Y::Array{Matrix{Float64},1}, X::Array{Matrix{Float64},1})
-    Y[1] .= - e.A * X[2] * (transpose(e.A * X[2]) * X[1])
-    Y[2] .= - transpose(e.A) * X[1] * (transpose(X[1]) * e.A * X[2])
+function (e::EGrad)(Y::Array, X::Array)
+    view(Y, :, :, 1) .= - e.A * X[e.M, 2] * (transpose(e.A * X[e.M, 2]) * X[e.M, 1])
+    view(Y, :, :, 2) .= - transpose(e.A) * X[e.M, 1] * (transpose(X[e.M, 1]) * e.A * X[e.M, 2])
     return Y
 end
 
-rgrad(M::PowerManifold, p) = project(M, p, egrad(p))
-struct RGrad{T}
-    egrad::EGrad{T}
+rgrad(M::PowerManifold, p) = project(M, p, egrad(M, p))
+struct RGrad{T,TM}
+    egrad::EGrad{T,TM}
 end
-RGrad(A::Matrix{T},p) where {T} = RGrad{T}(EGrad{T}(A))
+RGrad(M::PowerManifold, A::Matrix{T}) where {T} = RGrad{T,typeof(M)}(EGrad{T,typeof(M)}(M, A))
 function (r::RGrad)(M::PowerManifold, X, p)
     return project!(M, X, p, r.egrad(X, p))
 end
@@ -38,60 +40,78 @@ function e2rhess!(M::Grassmann, Y, p, X, e_grad, e_Hess)
     return project!(M, Y, p, Y)
 end
 
-function eHess(X::Array{Matrix{Float64},1}, H::Array{Matrix{Float64},1})
-    U = X[1]
-    V = X[2]
-    Udot = H[1]
-    Vdot = H[2]
+function eHess(M::Manifold, X::Array{Float64,3}, H::Array{Float64,3})
+    U = X[M, 1]
+    V = X[M, 2]
+    Udot = H[M, 1]
+    Vdot = H[M, 2]
     AV = A * V
     AtU = transpose(A) * U
     AVdot = A * Vdot
     AtUdot = transpose(A) * Udot
-    return [
-        -(
-            AVdot * transpose(AV) * U +
-            AV * transpose(AVdot) * U +
-            AV * transpose(AV) * Udot
-        ),
-        -(
-            AtUdot * transpose(AtU) * V +
-            AtU * transpose(AtUdot) * V +
-            AtU * transpose(AtU) * Vdot
-        ),
-    ]
+    R = similar(X)
+    view(R, :, :, 1) .= -(
+        AVdot * transpose(AV) * U +
+        AV * transpose(AVdot) * U +
+        AV * transpose(AV) * Udot
+    )
+    view(R, :, :, 2) .= -(
+        AtUdot * transpose(AtU) * V +
+        AtU * transpose(AtUdot) * V +
+        AtU * transpose(AtU) * Vdot
+    )
+    return R
 end
-struct EHess{T}
+struct EHess{T, TM}
+    M::TM
     A::Matrix{T}
 end
-EHess(A::Matrix{T}) where {T} = EHess{T}(A)
 function (e::EHess)(Y,X,H)
-    Y[1] .= -e.A*H[2]*transpose(e.A*X[2])*X[1] - e.A*X[2] * transpose(e.A*H[2])*X[1] - e.A*X[2]*transpose(e.A*X[2])*H[1]
-    Y[2] .= transpose(e.A)*H[1]*transpose(X[1])*e.A*X[2] + transpose(e.A)*X[1] * transpose(H[1])*e.A*X[2] + transpose(e.A)*X[1]*transpose(X[1])*e.A*H[2]
+    view(Y, :, :, 1) .= -e.A*H[e.M, 2]*transpose(e.A*X[e.M, 2])*X[e.M, 1] - e.A*X[e.M, 2] * transpose(e.A*H[e.M, 2])*X[e.M,1] - e.A*X[e.M,2]*transpose(e.A*X[e.M, 2])*H[e.M,1]
+    view(Y, :, :, 2) .= transpose(e.A)*H[e.M, 1]*transpose(X[e.M, 1])*e.A*X[e.M, 2] + transpose(e.A)*X[e.M, 1] * transpose(H[e.M, 1])*e.A*X[e.M, 2] + transpose(e.A)*X[e.M, 1]*transpose(X[e.M, 1])*e.A*H[e.M, 2]
     return Y
 end
 
 function rhess(M::PowerManifold, p, X)
-    eG = egrad(p)
-    eH = eHess(p, X)
-    return e2rHess.(Ref(M.manifold), p, X, eG, eH)
+    eG = egrad(M, p)
+    eH = eHess(M, p, X)
+    Ha = similar(p)
+    for i in 1:2
+        e2rhess!(
+            M.manifold,
+            view(Ha, :, :, i),
+            view(p, :, :, i),
+            view(X, :, :, i),
+            view(eG, :, :, i),
+            view(eH, :, :, i))
+    end
+    return Ha
 end
-struct RHess{T}
-    e_grad!::EGrad{T}
-    e_hess!::EHess{T}
-    G::Array{Matrix{T},1}
-    H::Array{Matrix{T},1}
+struct RHess{T,TM}
+    e_grad!::EGrad{T,TM}
+    e_hess!::EHess{T,TM}
+    G::Array{T,3}
+    H::Array{T,3}
 end
-function RHess(A::Matrix{T},p) where {T}
-    return RHess{T}(
-        EGrad(A),
-        EHess(A),
-        [zeros(T,size(A,1),p) for _ ∈ 1:2],
-        [zeros(T,size(A,1),p) for _ ∈ 1:2],
+function RHess(M::Manifold, A::Matrix{T},p) where {T}
+    return RHess{T,typeof(M)}(
+        EGrad(M, A),
+        EHess(M, A),
+        zeros(T, 3, 2, 2),
+        zeros(T, 3, 2, 2),
     )
 end
 function (r::RHess)(M::PowerManifold, Y, p, X)
     r.e_grad!(r.G, p)
     r.e_hess!(r.H, p, X)
-    e2rhess!.(Ref(M.manifold), Y, p, X, r.G, r.H)
+    for i in 1:2
+        e2rhess!(
+            M.manifold,
+            view(Y, :, :, i),
+            view(p, :, :, i),
+            view(X, :, :, i),
+            view(r.G, :, :, i),
+            view(r.H, :, :, i))
+    end
     return Y
 end
