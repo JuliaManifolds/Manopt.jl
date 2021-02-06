fd87d0f597f0bebcee049e99aa18163e91539a2c
Ronny <git@ronnybergmann.net>
2021-02-06 15:50:42 +0100

    extends Benchmark.

--- benchmarks/benchmark_trust_regions.jl
+++ benchmarks/benchmark_trust_regions.jl
@@ -1,5 +1,5 @@
 
-using Manifolds, Manopt, BenchmarkTools
+using Manifolds, Manopt, BenchmarkTools, Test
 
 include("../test/solvers/trust_region_model.jl")
 
@@ -9,8 +9,19 @@ N = Grassmann(n,p)
 M = PowerManifold(N, ArrayPowerRepresentation(),2)
 x = random_point(M)
 
+x_opt = trust_regions(M, cost, rgrad, rhess, x; max_trust_region_radius=8.0)
 @btime x_opt = trust_regions($M, $cost, $rgrad, $rhess, $x; max_trust_region_radius=8.0)
 
 h = RHess(M,A,p)
 g = RGrad(M,A)
+x_opt2 = trust_regions(M, cost, g, h, x; max_trust_region_radius=8.0, evaluation=MutatingEvaluation())
+
 @btime trust_regions($M, $cost, $g, $h, x2; max_trust_region_radius=8.0, evaluation=$(MutatingEvaluation()))  setup = (x2 = deepcopy($x))
+
+x3 = deepcopy(x)
+trust_regions!(M, cost, g, h, x3; max_trust_region_radius=8.0, evaluation=MutatingEvaluation())
+
+@btime trust_regions!($M, $cost, $g, $h, x3; max_trust_region_radius=8.0, evaluation=$(MutatingEvaluation()))  setup = (x3 = deepcopy($x))
+
+@test distance(M, x_opt, x_opt2) ≈ 0
+@test distance(M, x_opt, x3) ≈ 0
