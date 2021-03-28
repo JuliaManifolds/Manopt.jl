f58c97e12c98d39fcf4160c9d4e993370b7326d5
Ronny Bergmann <git@ronnybergmann.net>
2021-03-28 12:37:49 +0200

    Apply suggestions from code review
    Co-authored-by: Mateusz Baran <mateuszbaran89@gmail.com>

--- src/helpers/initialize_data.jl
+++ src/helpers/initialize_data.jl
@@ -4,10 +4,10 @@
 copy the values from `b` to `a` by recursively copying the contents.
 On the lowest level of `AbstractArray`s this falls back to `copyto!`.
 """
-function recursive_copyto!(a::AbstractArray{T}, b::AbstractArray{T}) where {T<:AbstractArray}
+function recursive_copyto!(a::AbstractArray{T}, b::AbstractArray{U}) where {T<:AbstractArray,U<:AbstractArray}
     foreach(recursive_copyto!, a, b)
     return a
 end
-function recursive_copyto!(a::AbstractArray{T}, b::AbstractArray{T}) where {T}
+function recursive_copyto!(a::AbstractArray, b::AbstractArray)
     return copyto!(a, b)
 end
