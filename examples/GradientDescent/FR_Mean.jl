c603113e2fbbc3e37e231b1a1885100f39a28191
kellertuer <git@ronnybergmann.net>
2021-03-29 09:04:28 +0200

    runs formatter.

--- examples/GradientDescent/FR_Mean.jl
+++ examples/GradientDescent/FR_Mean.jl
@@ -1,15 +1,14 @@
 using Manifolds, Manopt, Random
 Random.seed!(42)
-m=10
-n=10
-k=4
-G = Gressmann(n,k)
+m = 10
+n = 10
+k = 4
+G = Gressmann(n, k)
 pts = [
-    SVDMPoint(random_point(G), Diagonal(1 .+ randn(4)), random_point(G)', k)
-    for _ = 1:10
-    ]
-F(M::FixedRankMatrices, p) = sum([ 0.5*distance(M, p, q)^2 for q ∈ pts ])
-gradF(M::FixedRankMatrices, p) = sum([ log(M, p, q) for q in pts ])
-M = FixedRankMatrices(m,n,k)
+    SVDMPoint(random_point(G), Diagonal(1 .+ randn(4)), random_point(G)', k) for _ in 1:10
+]
+F(M::FixedRankMatrices, p) = sum([0.5 * distance(M, p, q)^2 for q in pts])
+gradF(M::FixedRankMatrices, p) = sum([log(M, p, q) for q in pts])
+M = FixedRankMatrices(m, n, k)
 x0 = deepcopy(pts[1])
-gradient_descent(M,F,gradF,x0)
+gradient_descent(M, F, gradF, x0)
--- src/helpers/initialize_data.jl
+++ src/helpers/initialize_data.jl
@@ -4,7 +4,9 @@
 copy the values from `b` to `a` by recursively copying the contents.
 On the lowest level of `AbstractArray`s this falls back to `copyto!`.
 """
-function recursive_copyto!(a::AbstractArray{T}, b::AbstractArray{U}) where {T<:AbstractArray,U<:AbstractArray}
+function recursive_copyto!(
+    a::AbstractArray{T}, b::AbstractArray{U}
+) where {T<:AbstractArray,U<:AbstractArray}
     foreach(recursive_copyto!, a, b)
     return a
 end
