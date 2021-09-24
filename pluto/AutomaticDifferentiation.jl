b45b7af9d02e24edb82200b72ed13e0a816393c3
Ronny Bergmann <git@ronnybergmann.net>
2021-09-24 17:08:05 +0200

    Further Pluto work.

--- pluto/AutomaticDifferentiation.jl
+++ pluto/AutomaticDifferentiation.jl
@@ -6,11 +6,11 @@ using InteractiveUtils
 
 # ╔═╡ b0769dfa-28cf-440e-9ba2-1ef488f171a9
 begin # dev mode – use global env on my machine
-    import Pkg
+    using Pkg: Pkg
     # careful: this is _not_ a reproducible environment
     # activate the global environment
     Pkg.activate()
-   using ReverseDiff, Manifolds, Manopt, Test, Random, LinearAlgebra, FiniteDiff
+    using ReverseDiff, Manifolds, Manopt, Test, Random, LinearAlgebra, FiniteDiff
 end
 
 # ╔═╡ 0213d26a-18ac-11ec-03fd-ada5992bcea8
@@ -20,12 +20,11 @@ md"""
 
 # ╔═╡ f3bc91ee-5871-4cba-ac89-190deb71ad0f
 md"""
-Since Manifolds.jl 0.6.7 the support of automatic differentiation support has been extended. With [ForwardDiff.jl](https://juliadiff.org/ForwardDiff.jl/stable/) or [ReverseDiff.jl](https://juliadiff.org/ReverseDiff.jl/) you can now use automatic differentiation on manifolds for embedded manifolds.
-"""
+Since [Manifolds.jl](https://juliamanifolds.github.io/Manifolds.jl/) 0.6.9 the support of automatic differentiation support has been extended.
 
-# ╔═╡ 35f02ab2-30d1-4c3c-8ba7-d07f391019e0
-md"""
-In general there is currenlty two ways to get the gradient of a function ``f\colon\mathcal M \to ℝ`` on a Riemannian manifold ``\mathcal M``.
+This tutorial explains how to use Euclidean tools to derive a gradient for a real-valued function ``F\colon \mathcal M → ℝ``. We will consider two methods: an intrinsic variant and a variant employing the embedding.
+
+While by default we use [FiniteDifferences.jl](https://juliadiff.org/FiniteDifferences.jl/latest/), you can also use [FiniteDiff.jl](https://github.com/JuliaDiff/FiniteDiff.jl), [ForwardDiff.jl](https://juliadiff.org/ForwardDiff.jl/stable/), [ReverseDiff.jl](https://juliadiff.org/ReverseDiff.jl/), or  [Zygote.jl](https://fluxml.ai/Zygote.jl/).
 """
 
 # ╔═╡ d9be6c2f-65fd-4685-9005-da22bf985e28
@@ -113,19 +112,19 @@ so we can compare it to the approximation by finite differences.
 
 # ╔═╡ 19747159-d383-4547-9315-0ed2494904a6
 begin
-	Random.seed!(42)
-	n = 200
-	A = randn(n+1,n+1)
-	A = Symmetric(A)
-	M = Sphere(n)
-	nothing
+    Random.seed!(42)
+    n = 200
+    A = randn(n + 1, n + 1)
+    A = Symmetric(A)
+    M = Sphere(n)
+    nothing
 end
 
 # ╔═╡ 41c204dd-6e4e-4a70-8f06-209a469e0680
-f1(p) = p'*A'p
+f1(p) = p' * A'p
 
 # ╔═╡ 2e33de5e-ffaa-422a-91d9-61f588ed1211
-gradf1(p) = 2*(A*p - p*p'*A*p) 
+gradf1(p) = 2 * (A * p - p * p' * A * p)
 
 # ╔═╡ bbd9a010-1981-45b3-bf7d-c04bcd2c2128
 md"""Manifolds provides a finite difference scheme in Tangent spaces, that you can introduce to use an existing framework (if the wrapper is implemented) form Euclidean space. Here we use `FiniteDiff.jl`."""
@@ -138,11 +137,11 @@ gradf1_FD(p) = Manifolds.gradient(M, f1, p, r_backend)
 
 # ╔═╡ 07f9a630-e53d-45ea-b109-3d4de190723d
 begin
-	p = zeros(n+1)
-	p[1] = 1.0
-	X1 = gradf1(p)
-	X2 = gradf1_FD(p)
-	norm(M, p, X1-X2)
+    p = zeros(n + 1)
+    p[1] = 1.0
+    X1 = gradf1(p)
+    X2 = gradf1_FD(p)
+    norm(M, p, X1 - X2)
 end
 
 # ╔═╡ 8e5f677d-dafa-49b9-b678-3f129be31dcf
@@ -179,7 +178,7 @@ We continue with the Rayleigh Quotient from before, now just starting with the d
 """
 
 # ╔═╡ c3f3aeba-2849-4715-94e2-0c44613a2ce9
-F(x) = x'*A*x/(x'*x);
+F(x) = x' * A * x / (x' * x);
 
 # ╔═╡ 786fce04-53ef-448d-9657-31208b35fb7e
 md"The cost function is the same by restriction"
@@ -191,39 +190,28 @@ f2(M, p) = F(p);
 md"The gradient is now computed combining our gradient scheme with ReverseDiff."
 
 # ╔═╡ 89cd6b4b-f9ef-47ac-afd3-cf9aacf43256
-grad_f2_AD(M,p) = Manifolds.gradient(M, F, p,
-	RiemannianProjectionBackend(Manifolds.ReverseDiffBackend()),
-)
+function grad_f2_AD(M, p)
+    return Manifolds.gradient(
+        M, F, p, RiemannianProjectionBackend(Manifolds.ReverseDiffBackend())
+    )
+end
 
 # ╔═╡ 7c5a8a17-6f63-4587-a94a-6936bdd3cec6
-X3 = grad_f2_AD(M,p)
+X3 = grad_f2_AD(M, p)
 
 # ╔═╡ b3e7f57f-d87a-47c5-b8ad-48b6d205fa73
-norm(M, p, X1-X3)
+norm(M, p, X1 - X3)
 
-# ╔═╡ 81dc2210-8bff-44c6-8a64-bb3324d1662f
+# ╔═╡ 893db402-283f-4e3e-8bf7-c6f22e485efb
 md"""
-# Technical Note
+# A Larger Example
 
-While you can specify the backend on every call, it is recommended (for type stability and hence performance) to set the backends using
+on the manifold ``\mathcal P(3)`` of symmetric positive definite matrices.
 """
-
-# ╔═╡ 1fd7c720-d3da-45b9-b76e-82ab38213408
-Manifolds.rdifferential_backend!(RiemannianProjectionBackend(Manifolds.ReverseDiffBackend()));
-
-# ╔═╡ 1363aa3c-ee35-42ed-8fe7-5b5167f423c6
-md"Then the call simplifies to "
-
-# ╔═╡ 79c328b6-b01a-4176-9e1b-fac0d258917b
-grad_f2_AD2(M,p) = Manifolds.gradient(M, F, p)
-
-# ╔═╡ 9f77f99f-b70a-4588-956b-6066d3678aa9
-norm(M, p, grad_f2_AD2(M,p) - X1)
 
 # ╔═╡ Cell order:
 # ╟─0213d26a-18ac-11ec-03fd-ada5992bcea8
 # ╟─f3bc91ee-5871-4cba-ac89-190deb71ad0f
-# ╟─35f02ab2-30d1-4c3c-8ba7-d07f391019e0
 # ╟─d9be6c2f-65fd-4685-9005-da22bf985e28
 # ╠═f88b15de-cec6-4bc8-9b68-2a407b5aeded
 # ╠═b0769dfa-28cf-440e-9ba2-1ef488f171a9
@@ -247,8 +235,4 @@ norm(M, p, grad_f2_AD2(M,p) - X1)
 # ╠═89cd6b4b-f9ef-47ac-afd3-cf9aacf43256
 # ╠═7c5a8a17-6f63-4587-a94a-6936bdd3cec6
 # ╠═b3e7f57f-d87a-47c5-b8ad-48b6d205fa73
-# ╟─81dc2210-8bff-44c6-8a64-bb3324d1662f
-# ╠═1fd7c720-d3da-45b9-b76e-82ab38213408
-# ╟─1363aa3c-ee35-42ed-8fe7-5b5167f423c6
-# ╠═79c328b6-b01a-4176-9e1b-fac0d258917b
-# ╠═9f77f99f-b70a-4588-956b-6066d3678aa9
+# ╠═893db402-283f-4e3e-8bf7-c6f22e485efb
--- pluto/Brocket.jl
+++ pluto/Brocket.jl
@@ -21,10 +21,10 @@ where ``\mathrm{St}(n,k)`` denotes the [Stiefel](https://juliamanifolds.github.i
 
 # ╔═╡ 4f6af949-9e3e-4b58-abc8-a8c74ff109ae
 begin
-	Random.seed!(42)
-	n=10
-	k=5
-	nothing
+    Random.seed!(42)
+    n = 10
+    k = 5
+    nothing
 end
 
 # ╔═╡ 8ad35f54-d774-47a4-8dd0-d62ad05be20b
@@ -32,25 +32,25 @@ md"## Gradient of the cost function"
 
 # ╔═╡ 944f7644-2428-4f79-885b-54ff326eb331
 begin
-	struct GradF!
-	    A::Matrix{Float64}
-	    N::Diagonal{Float64,Vector{Float64}}
-	end
-	function (gradF!::GradF!)(::Stiefel, X, p)
-	    Ap = gradF!.A * p
-	    pTAp = p' * Ap
-		print(size(X))
-		print(size(
-				2 .* Ap * gradF!.N #.- p * pTAp * gradF!.N .- p * gradF!.N * pTAp
-		))
-	    copyto!(X, 2 .* Ap * gradF!.N .- p * pTAp * gradF!.N .- p * gradF!.N * pTAp)
-		return X
-	end
+    struct GradF!
+        A::Matrix{Float64}
+        N::Diagonal{Float64,Vector{Float64}}
+    end
+    function (gradF!::GradF!)(::Stiefel, X, p)
+        Ap = gradF!.A * p
+        pTAp = p' * Ap
+        print(size(X))
+        print(size(
+            2 .* Ap * gradF!.N, #.- p * pTAp * gradF!.N .- p * gradF!.N * pTAp
+        ))
+        copyto!(X, 2 .* Ap * gradF!.N .- p * pTAp * gradF!.N .- p * gradF!.N * pTAp)
+        return X
+    end
 end
 
 # ╔═╡ c10796ce-1f12-4d70-b643-9c0b44caa853
 begin
-	A = randn(n, n)
+    A = randn(n, n)
     A = (A + A') / 2
 end
 
@@ -58,23 +58,23 @@ end
 F(::Stiefel, p) = tr((p' * A * p) * Diagonal(k:-1:1))
 
 # ╔═╡ fe4155cc-ff04-4d56-a254-6d642a5c570b
-	gradF! = GradF!(A, Diagonal(Float64.(collect(k:-1:1))))
+gradF! = GradF!(A, Diagonal(Float64.(collect(k:-1:1))))
 
 # ╔═╡ bdb2e4b0-7a14-4bd4-83ee-72b31d509196
-M = Stiefel(n,k)
+M = Stiefel(n, k)
 
 # ╔═╡ e60da1b8-04b1-45fd-9926-f326f69e5fa6
-x0 = copy(M,A)
+x0 = copy(M, A)
 
 # ╔═╡ 3198c2b2-3c38-46f6-a6a3-cecc5ae4e1e0
 begin
-	X = zero_vector(M,x0)
-	gradF!(M, X, x0)
+    X = zero_vector(M, x0)
+    gradF!(M, X, x0)
 end
 
 # ╔═╡ c95073cc-7447-4217-a4e7-f477f76fd292
 quasi_Newton(
-	M,
+    M,
     F,
     gradF!,
     x0;
@@ -83,9 +83,9 @@ quasi_Newton(
     retraction_method=QRRetraction(),
     stopping_criterion=StopWhenGradientNormLess(norm(M, x0, gradF(M, x0)) * 10^(-6)),
     cautious_update=true,
-	evaluation=MutatingEvaluation(),
-#    debug = [:Iteration," ", :Cost, " ", DebugGradientNorm(), "\n", 10],
-    )
+    evaluation=MutatingEvaluation(),
+    #    debug = [:Iteration," ", :Cost, " ", DebugGradientNorm(), "\n", 10],
+)
 
 # ╔═╡ Cell order:
 # ╠═9b5cf8fa-b358-4966-baf9-8a6abc86bd5d
