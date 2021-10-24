dad8e6d7c1ba6b3aeb05a4777188f07400eab9fa
Renée Dornig <renee.dornig@mathematik.tu-chemnitz.de>
2021-10-24 19:22:11 +0200

    renames Riemannian augmented Lagrangian method to just augmented Lagrangian method, changes the options accordingly and keeps working on both

---
+++ src/plans/alm_plan.jl
@@ -0,0 +1,79 @@
+#
+# Options
+#
+@doc raw"""
+    ALMOptions{P,T} <: Options
+
+Describes the augmented Lagrangian method, with
+
+# Fields
+a default value is given in brackets if a parameter can be left out in initialization.
+
+* `x` – a set point on a manifold as starting point
+
+# Constructor
+
+    ALMOptions(x)
+
+construct an augmented Lagrangian Option with the fields and defaults as above.
+
+# See also
+[`augmented_Lagrangian_method`](@ref)
+"""
+mutable struct ALMOptions{P, Pr <: Problem, Op <: Options, TStopping <: StoppingCriterion} <: Options
+    x::P
+    sub_problem::Pr
+    sub_options::Op
+    num_outer_itertgn::Int
+    ϵ::Real #(starting)tolgradnorm
+    ϵ_min::Real #endingtolgradnorm
+    bound::Real
+    λ::Vector
+    γ::Vector
+    ρ::Real
+    τ::Real
+    θ_ρ::Real
+    stop::TStopping
+    
+    function ALMOptions(
+        x0::P,
+        n_ineq::Int,
+        n_eq::Int,
+        sub_problem::Pr,
+        sub_options::Op,
+        num_outer_itertgn::Int=30,
+        ϵ::Real=1e-3, #(starting)tolgradnorm
+        ϵ_min::Real=1e-6, #endingtolgradnorm
+        bound::Real=20.0,
+        λ::Vector=ones(n_ineq),
+        γ::Vector=ones(n_eq),
+        ρ::Real=1.0, 
+        τ::Real=0.8,
+        θ_ρ::Real=0.3, 
+        stopping_criterion::StoppingCriterion=StopAfterIteration(300), #maxOuterIter
+    ) where {P, Pr <: Problem, Op <: Options} 
+        θ_ϵ=(ϵ_min/ϵ)^(1/num_outer_itertgn), 
+        o = new{
+            P,
+            Pr,
+            Op,
+            typeof(stopping_criterion),
+        }()
+        o.x = x0
+        o.sub_problem,
+        o.sub_options,
+        o.num_outer_itertgn = num_outer_itertgn
+        o.ϵ = ϵ
+        o.ϵ_min = ϵ_min
+        o.bound = bound
+        o.λ = λ
+        o.γ = γ
+        o.ρ = ρ
+        o.τ = τ
+        o.θ_ρ = θ_ρ
+        o.θ_ϵ = θ_ϵ
+        o.old_acc = old_acc
+        o.stop = stopping_criterion
+        return o
+    end
+end
---
+++ src/solvers/augmented_Lagrangian_method.jl
@@ -0,0 +1,120 @@
+# Recreating Changshuo Liu's Matlab source code in Julia
+# original code by Changshuo Liu: https://github.com/losangle/Optimization-on-manifolds-with-extra-constraints/blob/master/solvers/almbddmultiplier.m
+# uses Riemannian limited memory BFGS solver
+# get n_ineq_constraint, n_eq_constraint
+
+function augmented_Lagrangian_method(
+    M::Manifold,
+    F::TF,
+    n_ineq_constraint::Int,
+    n_eq_constraint::Int;
+    x::random_point(M),
+    kwargs...,
+) where {TF}
+    x_res = allocate(x)
+    copyto!(Ref(M), x_res, x)
+    return augmented_Lagrangian_method!(M, F, sub_problem, sub_options, n_ineq_constraint, n_eq_constraint; x=x_res, kwargs...)
+end
+
+function augmented_Lagrangian_method!(
+    M::Manifold,
+    F::TF,
+    sub_problem,
+    sub_options,
+    n_ineq_constraint::Int,
+    n_eq_constraint::Int;
+    x::random_point(M),
+    max_inner_iter::Int=200,
+    num_outer_itertgn::Int=30,
+    ϵ::Real=1e-3, #(starting)tolgradnorm
+    ϵ_min::Real=1e-6, #endingtolgradnorm
+    bound::Int=20, 
+    λ::Vector=ones(n_ineq_constraint),
+    γ::Vector=ones(n_eq_constraint),
+    ρ::Real=1.0, 
+    τ::Real=0.8,
+    θ_ρ::Real=0.3, 
+    θ_ϵ::Real=(ϵ_min/ϵ)^(1/num_outer_itertgn), 
+    oldacc::Real=Inf, 
+    min_stepsize::Real=1e-6, ### put in stopping criterion alongside the ϵ condition
+    stopping_criterion::StoppingCriterion==StopWhenAny(StopAfterIteration(300), StopWhenAll()), #maxOuterIter
+    kwargs...,
+) where {TF}
+    p = CostProblem(M, F, n_ineq_constraint, n_eq_constraint)
+    o = RALMOptions(
+        x,
+        max_inner_iter,
+        num_outer_itertgn,
+        ϵ,
+        ϵ_min,
+        bound,
+        λ,
+        γ,
+        ρ,
+        τ,
+        θ_ρ,
+        θ_ϵ,
+        oldacc,
+        stopping_criterion,
+    )
+    o = decorate_options(o; kwargs...)
+    resultO = solve(p, o)
+    if return_options
+        return resultO
+    else
+        return get_solver_result(resultO)
+    end
+end
+
+#
+# Solver functions
+#
+function initialize_solver!(p::CostProblem, o::ALMOptions)
+###x0
+old_acc=Inf, 
+end
+function step_solver!(p::CostProblem, o::ALMOptions, iter)
+    # use subsolver to minimize the augmented Lagrangian within a tolerance ϵ and with max_inner_iter
+    
+    ###o.x=
+
+    # update multipliers
+    cost_ineq = get_cost_ineq(p, o.x)
+    λ = min.(ones(n_ineq_constraint)* o.bound, max.(λ + o.ρ .* cost_ineq, zeros(n_ineq_constraint)))
+    cost_eq = get_cost_eq(p, o.x)
+    γ = min.(ones(n_eq_constraint)* o.bound, max.(ones(n_eq_constraint) * (-o.bound), γ + o.ρ .* cost_eq))
+
+    # get new evaluation of penalty
+    new_acc = max(max(abs.(max.(-λ./ρ, Ref(cost_ineq)))), max(abs.(cost_eq)))
+
+    # update ρ if necessary
+    if iter == 1 || new_acc > o.τ * o.old_acc 
+        o.ρ = o.ρ/o.θ_ρ 
+    end
+    o.old_acc = new_acc
+
+    # update the tolerance ϵ
+    ϵ = max(o.ϵ_min, ϵ * o.θ_ϵ)
+end
+get_solver_result(o::ALMOptions) = o.x
+
+function get_Lagrangian_cost(p::CostProblem, o::ALMOptions) #als Funktion
+    cost = get_cost(p, o.x)
+    cost_ineq = sum(max.(zeros(o.n_ineq), o.λ ./ o.ρ .+ get_cost_ineq(p, o.x)))
+    cost_eq = sum((get_cost_eq(p, o.x) .+ o.γ./o.ρ)^2)
+    return cost + (o.ρ/2) * (cost_ineq + cost_eq)
+end
+
+function get_Lagrangian_gradient(p::CostProblem, o::ALMOptions) #als Funktion
+    grad = get_gradient(p, o.x)
+    cost_shifted_ineq = get_cost_ineq(p, o.x) + o.λ./o.ρ 
+    index = findall(x -> x > 0, cost_shifted_ineq) #gets the index of all elements greater 0
+    grad_ineq = get_grad_ineq(p, o.x)
+    ### transform with egrad2rgrad
+    grad_ineq = grad_ineq[index]
+      
+    cost_eq = get_cost_eq(p, o.x)
+    grad_eq = get_grad_eq(p, o.x)
+    ### transform with egrad2rgrad
+    return ### linear combination
+end
