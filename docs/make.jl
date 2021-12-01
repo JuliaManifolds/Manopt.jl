c795392e03a1ceebe986d54eb4fdfe1b3bc84dd4
Ronny Bergmann <git@ronnybergmann.net>
2021-12-01 11:24:31 +0100

    Switch notebook to use Pluto Package manager, try without the JS trick.

--- docs/make.jl
+++ docs/make.jl
@@ -68,21 +68,6 @@ for (i, f) in enumerate(pluto_files)
         }
         </style>
         $html
-        <script>
-        require(['jquery', 'katex', 'katex-auto-render'], function(\$, katex, renderMathInElement) {
-            \$(document).ready(function() {
-                renderMathInElement(document.body);
-                /* Apparently, Pluto wraps LaTeX inside a span class 'tex'. */
-                var equations = \$('p.tex');
-                for (var i = 0; i < equations.length; i++) {
-                    var text = equations[i].textContent;
-                    text = text.split('\$').join('');
-                    console.log(text);
-                    render(text, equations[i]);
-                }
-            })
-        })
-        </script>
         ```
         """,
     )
