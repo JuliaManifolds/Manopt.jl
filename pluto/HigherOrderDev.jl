f8033b6637f22d533a348ccb1b800328facc970e
Ronny Bergmann <git@ronnybergmann.net>
2021-12-01 17:48:52 +0100

    add a dummy for testing SSN

---
+++ pluto/HigherOrderDev.jl
@@ -0,0 +1,15 @@
+### A Pluto.jl notebook ###
+# v0.17.2
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 4a1dd24e-52c6-11ec-20a6-3d677b47806f
+begin
+	using Pkg
+	Pkg.activate() # use global enrivonment of current Julia, esp Manopt in dev mode for now
+	using Manifolds, Manopt
+end
+
+# ╔═╡ Cell order:
+# ╠═4a1dd24e-52c6-11ec-20a6-3d677b47806f
