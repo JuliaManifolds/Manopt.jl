73ad8b9f11852c6bf832d7526f0c3f6709035604
Ronny Bergmann <git@ronnybergmann.net>
2022-03-11 16:50:02 +0100

    add differentials for geodesics and rename the previous ones (consistently with Manifolds.jl) shortest geodesics.

--- src/functions/differentials.jl
+++ src/functions/differentials.jl
@@ -189,34 +189,34 @@ function differential_bezier_control!(
 end
 
 @doc raw"""
-    differential_geodesic_startpoint(M, p, q, t, X)
-    differential_geodesic_startpoint!(M, Y, p, q, t, X)
+    differential_shortest_geodesic_startpoint(M, p, q, t, X)
+    differential_shortest_geodesic_startpoint!(M, Y, p, q, t, X)
 
-computes ``D_p g(t;p,q)[η]`` (in place of `Y`).
+computes ``D_p \gamma(t;p,q)[η]`` (in place of `Y`).
 
 # See also
  [`differential_geodesic_endpoint`](@ref), [`jacobi_field`](@ref)
 """
-function differential_geodesic_startpoint(M::AbstractManifold, p, q, t, X)
+function differential_shortest_geodesic_startpoint(M::AbstractManifold, p, q, t, X)
     return jacobi_field(M, p, q, t, X, βdifferential_geodesic_startpoint)
 end
-function differential_geodesic_startpoint!(M::AbstractManifold, Y, p, q, t, X)
+function differential_shortest_geodesic_startpoint!(M::AbstractManifold, Y, p, q, t, X)
     return jacobi_field!(M, Y, p, q, t, X, βdifferential_geodesic_startpoint)
 end
 
 @doc raw"""
-    differential_geodesic_endpoint(M, p, q, t, X)
-    differential_geodesic_endpoint!(M, Y, p, q, t, X)
+    differential_shortest_geodesic_endpoint(M, p, q, t, X)
+    differential_shortest_geodesic_endpoint!(M, Y, p, q, t, X)
 
-computes ``D_qg(t;p,q)[X]`` (in place of `Y`).
+computes ``D_q \gamma(t;p,q)[X]`` (in place of `Y`).
 
 # See also
- [`differential_geodesic_startpoint`](@ref), [`jacobi_field`](@ref)
+ [`differential_shortest_geodesic_startpoint`](@ref), [`jacobi_field`](@ref)
 """
-function differential_geodesic_endpoint(M::AbstractManifold, p, q, t, X)
+function differential_shortest_geodesic_endpoint(M::AbstractManifold, p, q, t, X)
     return jacobi_field(M, q, p, 1 - t, X, βdifferential_geodesic_startpoint)
 end
-function differential_geodesic_endpoint!(M::AbstractManifold, Y, p, q, t, X)
+function differential_shortest_geodesic_endpoint!(M::AbstractManifold, Y, p, q, t, X)
     return jacobi_field!(M, Y, q, p, 1 - t, X, βdifferential_geodesic_startpoint)
 end
 
@@ -241,7 +241,7 @@ end
     differential_exp_argument!(M, Z, p, X, Y)
 
 computes ``D_X\exp_pX[Y]`` (in place of `Z`).
-Note that ``X ∈  T_X(T_p\mathcal M) = T_p\mathcal M`` is still a tangent vector.
+Note that ``Y ∈  T_X(T_p\mathcal M) = T_p\mathcal M`` is still a tangent vector.
 
 # See also
  [`differential_exp_basepoint`](@ref), [`jacobi_field`](@ref)
@@ -251,6 +251,33 @@ function differential_exp_argument(M::AbstractManifold, p, X, Y)
 end
 function differential_exp_argument!(M::AbstractManifold, Z, p, X, Y)
     return jacobi_field!(M, Z, p, exp(M, p, X), 1.0, Y, βdifferential_exp_argument)
+end
+
+@doc raw"""
+    differential_geodesic_argument(M, p, X, t, Y)
+    differential_geodesic_argument!(M, Z, p, X, t, Y)
+
+computes ``D_X\gamma_{p,X}(t)[Y]`` (in place of `Z`).
+Note that ``Y ∈  T_X(T_[\p\mathcal M) = T_p\mathcal M`` is still a tangent vector.
+"""
+function differential_geodesic_argument(M::AbstractManifold, p, X, t, Y)
+    return jacobi_field(M, p, exp(M, p, X), t, Y, βdifferential_exp_argument)
+end
+function differential_geodesic_argument!(M::AbstractManifold, Z, p, X, t, Y)
+    return jacobi_field!(M, Z, p, exp(M, p, X), t, Y, βdifferential_exp_argument)
+end
+
+@doc raw"""
+    differential_geodesic_startpoint(M, p, X, t, Y)
+    differential_geodesic_startpoint!(M, Z, p, X, t, Y)
+
+computes ``D_p\gamma_{p,X}(t)[Y]`` (in place of `Z`).
+"""
+function differential_geodesic_startpoint(M::AbstractManifold, p, X, t, Y)
+    return differential_shortest_geodesic_startpoint(M, p, exp(M, p, X), t, Y)
+end
+function differential_geodesic_argument!(M::AbstractManifold, Z, p, X, t, Y)
+    return differential_shortest_geodesic_startpoint!(M, Z, p, exp(M, p, X), t, Y)
 end
 
 @doc raw"""
