130deab00929a4982f02f3a7544646f1841b9ddc
Ronny Bergmann <git@ronnybergmann.net>
2022-04-11 12:16:36 +0200

    Improve line styles such that E(t) can be seen behind slope estimate.

--- src/helpers/check_plots.jl
+++ src/helpers/check_plots.jl
@@ -14,16 +14,22 @@ function plot_slope(x, y; slope=2, line_base=0, a=0, b=2.0, i=1, j=length(x))
         xaxis=:log,
         yaxis=:log,
         label="\$E(t)\$",
-        linewidth=2,
+        linewidth=3,
         legend=:topleft,
         color=:lightblue,
     )
     s_line = [exp10(line_base + t * slope) for t in log10.(x)]
-    plot!(fig, x, s_line; label="slope s=2", linestyle=:dash, color=:black)
+    plot!(fig, x, s_line; label="slope s=2", linestyle=:dash, color=:black, linewidth=2)
     if (i != 0) && (j != 0)
         best_line = [exp10(a + t * b) for t in log10.(x[i:j])]
         plot!(
-            fig, x[i:j], best_line; label="best slope $(@sprintf("%.4f", b))", color=:blue
+            fig,
+            x[i:j],
+            best_line;
+            label="best slope $(@sprintf("%.4f", b))",
+            color=:blue,
+            linestyle=:dot,
+            linewidth=2,
         )
     end
     gui(fig)
