74c6440f4e8891cf20bb597a2fdd17e37977df4a
Ronny Bergmann <git@ronnybergmann.net>
2022-07-19 20:12:15 +0200

    trying to fix the closed form oracle further.

--- examples/ConstrainedSPDMean.jl
+++ examples/ConstrainedSPDMean.jl
@@ -6,9 +6,9 @@ using InteractiveUtils
 
 # ╔═╡ e2dd0dd2-0444-11ed-1e55-5b3126a23f62
 begin
-    using Pkg
-    Pkg.activate()#use branch in dev mode for now
-    using Manopt, Manifolds, LinearAlgebra
+	using Pkg
+	Pkg.activate()#use branch in dev mode for now
+	using Manopt, Manifolds, LinearAlgebra
 end
 
 # ╔═╡ 285f33c1-e8ed-45d4-ba33-9e5eb17ce422
@@ -30,10 +30,10 @@ We first define some variables.
 
 # ╔═╡ 397f4aa4-bd8b-4f98-9c5a-c3514e50ef37
 begin
-    # dimension of the SPD matrices
-    n = 50
-    # number of data points
-    N = 100
+	# dimension of the SPD matrices
+	n = 50
+	# number of data points
+	N = 100	
 end
 
 # ╔═╡ 8e59bb69-e073-40cc-a304-d1256463dcbb
@@ -51,8 +51,8 @@ for `N` points `pts` on the SPD matrices, this computes the weighted harmonic me
 	\biggl( \sum_{i=1}^N w_i p_i^{-1} \biggr)^{-1}
 ```
 """
-function harmonic_mean(pts, w=1 / length(pts) .* ones(length(pts)))
-    return inv(sum([wi * inv(pi) for (wi, pi) in zip(w, pts)]))
+function harmonic_mean(pts, w=1/length(pts) .* ones(length(pts)))
+	return inv(sum( [wi*inv(pi) for (wi,pi) in zip(w,pts)]  ))
 end
 
 # ╔═╡ a56c69d9-768b-4ce5-99d7-986820b8b53c
@@ -65,8 +65,8 @@ for `N` points `pts` on the SPD matrices, this computes the weighted arithmetic 
 	\sum_{i=1}^N w_i p_i
 ```
 """
-function arithmetic_mean(pts, w=1 / length(pts) .* ones(length(pts)))
-    return sum([wi * pi for (wi, pi) in zip(w, pts)])
+function arithmetic_mean(pts, w=1/length(pts) .* ones(length(pts)))
+	return sum( [wi*pi for (wi,pi) in zip(w,pts)]  )
 end
 
 # ╔═╡ 58594aa5-1dc8-4193-914f-98ab4bfcdc03
@@ -74,19 +74,19 @@ M = SymmetricPositiveDefinite(n)
 
 # ╔═╡ 09e304f8-b153-4076-81d7-a3a01644d65a
 begin
-    data = [random_point(M) for _ in 1:N]
-    weights = rand(N)
-    weights ./= sum(weights)
+	data = [random_point(M) for _ ∈ 1:N]
+	weights = rand(N)
+	weights ./= sum(weights)
 end
 
 # ╔═╡ b2fdb060-31c6-4d2c-9e04-e5fa0caab5d0
 function weighted_mean_cost(M, p)
-    return sum([wi * distance(M, p, di)^2 for (wi, di) in zip(weights, data)])
+	return sum([ wi*distance(M,p,di)^2 for (wi,di) in zip(weights,data) ] )
 end
 
 # ╔═╡ e5fc5216-5aab-4638-9444-02dd9b1cb4e3
 function grad_weighted_mean_cost(M, p)
-    return sum([wi * grad_distance(M, p, di) for (wi, di) in zip(weights, data)])
+	return sum([ wi*grad_distance(M,p,di) for (wi,di) in zip(weights,data) ] )
 end
 
 # ╔═╡ 701ace87-ef6e-42a6-9e81-563c3abc55b4
@@ -104,23 +104,23 @@ which has a closed form solution, cf. (38) in Weber & Sra.
 
 """
 function FW_oracle(M::SymmetricPositiveDefinite, L, U, p, X)
-    e = eigen(Symmetric(p))
+	e = eigen(Symmetric(p))
     U = e.vectors
     Ud = max.(e.values, floatmin(eltype(e.values)))
     Dsqrt = Diagonal(sqrt.(Ud))
-    DqrtInv = Diagonal(1 ./ sqrt.(p))
+    DsqrtInv = Diagonal(1 ./ sqrt.(Ud))
     pSqrt = Symmetric(U * Dsqrt * transpose(U))
     pSqrtInv = Symmetric(U * DsqrtInv * transpose(U))
 
-    e2 = eigen(pSqrt * X * pSqrt)
-    D2 = Diagonal(1.0 .* (e2.values .< 0))
-    Q = e2.vectors
-
-    Uprime = Q' * pSqrtInv * U * pSqrtInv * Q
-    Lprime = Q' * pSqrtInv * L * pSqrtInv * Q
-    P = cholesky(Uprime - Lprime)
-    z = P' * D * P + Lprima
-    return pSqrt * Q * z * Q' * pSqrt
+	e2 = eigen(pSqrt*X*pSqrt)
+	D2 = Diagonal(1.0 .* (e2.values .< 0))
+	Q = e2.vectors
+	
+	Uprime = Q'*pSqrtInv*U*pSqrtInv*Q
+	Lprime = Q'*pSqrtInv*L*pSqrtInv*Q
+	P = cholesky(Uprime - Lprime)
+	z =  P'*D*P+Lprima
+	return pSqrt*Q*z*Q'*pSqrt
 end
 
 # ╔═╡ 41ad71e7-708f-42e9-a92b-902c6324215f
@@ -130,12 +130,10 @@ H = harmonic_mean(data, weights);
 A = arithmetic_mean(data, weights);
 
 # ╔═╡ 17ec6a97-d7af-4b35-b388-523537e88a0f
-special_oracle(M, p) = FW_oracle(M, H, A, p, grad_weighted_mean_cost(M, p))
+special_oracle(M,p,X) = FW_oracle(M, H, A, p, X)
 
 # ╔═╡ 3d369862-dc1c-4e06-acde-9eb1baa1a425
-Frank_Wolfe_algorithm(
-    M, weighted_mean_cost, grad_weighted_mean_cost, data[1]; subtask=special_oracle
-)
+Frank_Wolfe_algorithm(M, weighted_mean_cost, grad_weighted_mean_cost, data[1]; subtask=special_oracle)
 
 # ╔═╡ Cell order:
 # ╠═285f33c1-e8ed-45d4-ba33-9e5eb17ce422
