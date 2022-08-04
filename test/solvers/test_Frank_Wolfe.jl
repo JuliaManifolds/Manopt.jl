d2f940c1ca70dcdb6559a80c101c7afa3f961cba
Ronny Bergmann <git@ronnybergmann.net>
2022-08-04 17:59:21 +0200

    Update test/solvers/test_Frank_Wolfe.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test/solvers/test_Frank_Wolfe.jl
+++ test/solvers/test_Frank_Wolfe.jl
@@ -3,7 +3,6 @@ using Manifolds, Manopt, Test, LinearAlgebra
 @testset "Frank Wolfe Method" begin
     M = SymmetricPositiveDefinite(2)
     p = Matrix{Float64}(I, 2, 2)
-    @testset "Basics and access functions" begin
-    end
+    @testset "Basics and access functions" begin end
     @testset "Two small Test runs" begin end
 end
