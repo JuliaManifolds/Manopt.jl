b9ea77bf8f0125109e3447bd79801d49d16f96a7
Hajg Jasa <hajg.jasa@ntnu.no>
2023-01-09 13:52:39 +0100

    Makes benchmarking script into a function of the dimension

--- test/test_time_bundle_method.jl
+++ test/test_time_bundle_method.jl
@@ -1,10 +1,11 @@
 using Manopt, Manifolds, Random, QuadraticModels, RipQP, Plots
 
-function time_bundle(man_name)
-    t = []
-    for n in 3:100
-        M = man_name(n)
-        Random.seed!(n)
+function compare_times(manifold_name, n::Int)
+    t = Float64[]
+    s = Float64[]
+    for j in 3:n
+        M = manifold_name(j)
+        Random.seed!(42)
         data = [rand(M; σ=0.4) for i in 1:100]
 
         F(M, y) = sum(1 / (2 * length(data)) * distance.(Ref(M), data, Ref(y)) .^ 2)
@@ -30,20 +31,41 @@ function time_bundle(man_name)
                     F3,
                     gradF3,
                     data[1];
-                    m = 0.125,
-                    tol = 1e-8,
-                    stopping_criterion=StopWhenAny(
-                        StopAfterIteration(20), StopWhenChangeLess(1e-8)
-                    ),
-                    #debug=[:Iteration, :Cost, "\n", 10],
+                    # m = 0.0125,
+                    # tol = 1e-8,
+                    # stopping_criterion=StopWhenAny(
+                    #     StopAfterIteration(20), StopWhenChangeLess(1e-8)
+                    # ),
+                    # debug=[:Iteration, :Cost, "\n", 10],
+                )
+            ).time,
+        )
+
+        push!(
+            s,
+            @timed(
+                subgradient_method(
+                    M,
+                    F3,
+                    gradF3,
+                    data[1];
+                    # stopping_criterion=StopWhenAny(
+                    #     StopAfterIteration(20), StopWhenChangeLess(1e-8)
+                    # ),
+                    # debug=[:Iteration, :Cost, "\n", 10],
                 )
             ).time,
         )
     end
-    return t
+    return t, s
 end
 
-t1 = time_bundle(Hyperbolic)
-t2 = time_bundle(SymmetricPositiveDefinite)
+function plot_time_graphs(m::Int)
+    t1, s1 = compare_times(Hyperbolic, m)
+    t2, s2 = compare_times(SymmetricPositiveDefinite, m)
 
-display(plot([3:100], [t1 t2]; title="", label = ["Hyperbolic Space" "Symmetric Pos Def Matirces"], xlabel = "Dimension", ylabel = "Time"))
+    p1 = plot([3:m], [t1 t2]; title = "Bundle Method", label = ["Hyperbolic Space" "Symmetric Pos Def Matirces"], xlabel = "Dimension", ylabel = "Time")
+    p2 = plot([3:m], [s1 s2]; title = "Subgradient Method", label = ["Hyperbolic Space" "Symmetric Pos Def Matirces"], xlabel = "Dimension", ylabel = "Time")
+    #display(plot(p1, p2))
+    return nothing
+end
