b1ff732ff481dcfec9ec7115ec9becfc30a232d5
Hajg Jasa <hajg.jasa@ntnu.no>
2023-01-18 10:18:02 +0100

    Simplifies matrix creation in subsolver

--- src/solvers/bundle_method_sub_solver.jl
+++ src/solvers/bundle_method_sub_solver.jl
@@ -2,10 +2,7 @@ function bundle_method_sub_solver(
     M::A, bms::BundleMethodState, X::T
 ) where {A<:AbstractManifold,T}
     d = length(bms.index_set)
-    H =
-        reduce(
-            hcat, [[inner(M, bms.p_last_serious, X[i], X[j]) for j in 1:d] for i in 1:d]
-        )'
+    H = [inner(M, bms.p_last_serious, X[i], X[j]) for j in 1:d, i in 1:d]
     B = reshape(ones(d), 1, d)
     qm = QuadraticModel(
         bms.lin_errors, tril(H); A=B, lcon=[1.0], ucon=[1.0], lvar=zeros(d), c0=0.0
