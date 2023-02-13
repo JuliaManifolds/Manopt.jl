99e9f814e549e5d43fb90f65677f37cdb0cb5061
Hajg Jasa <hajg.jasa@ntnu.no>
2023-02-13 14:05:46 +0100

    Edits example

--- examples/bundle_method_examples/mean_median_bundle_method.jl
+++ examples/bundle_method_examples/mean_median_bundle_method.jl
@@ -1,7 +1,7 @@
 using Manopt, Manifolds, Random, QuadraticModels, RipQP
 
-M = Sphere(4)
-Random.seed!(55)
+M = Hyperbolic(4)
+# Random.seed!(55)
 data = [rand(M; σ=0.4) for i in 1:100]
 
 F(M, y) = sum(1 / (2 * length(data)) * distance.(Ref(M), data, Ref(y)) .^ 2)
@@ -11,14 +11,15 @@ gradF(M, y) = sum(1 / length(data) * grad_distance.(Ref(M), data, Ref(y)))
     F,
     gradF,
     data[1];
-    stopping_criterion=StopAfterIteration(100)#StopWhenAny(StopWhenChangeLess(1e-12), StopAfterIteration(5000)),
+    stopping_criterion=StopAfterIteration(500)#StopWhenAny(StopWhenChangeLess(1e-12), StopAfterIteration(5000)),
 )
-@time m_mean = mean(M, data)
+@time m_mean = mean(M, data; stop_iter = 500)
 mean_dist = distance(M, b_mean, m_mean)
 println("Distance between means: $mean_dist")
 println(
-    "$(F(M, b_mean) <= F(M,m_mean) ? "F(bundle_mean) ≤ F(manifolds_mean)" : "F(bundle_mean) > F(manifolds_mean)")",
+    "$(F(M, b_mean) < F(M,m_mean) ? "F(bundle_mean) < F(manifolds_mean)" : "F(bundle_mean) ≥ F(manifolds_mean)")",
 )
+println("    |F(bundle_mean) - F(manifolds_mean)| = $(abs(F(M, b_mean) - F(M, m_mean)))")
 
 F2(M, y) = sum(1 / (2 * length(data)) * distance.(Ref(M), Ref(y), data))
 gradF2(M, y) = sum(1 / (2 * length(data)) * grad_distance.(Ref(M), data, Ref(y), 1))
@@ -27,11 +28,12 @@ gradF2(M, y) = sum(1 / (2 * length(data)) * grad_distance.(Ref(M), data, Ref(y),
     F2,
     gradF2,
     data[1];
-    stopping_criterion=StopAfterIteration(100) #StopWhenAny(StopWhenChangeLess(1e-12), StopAfterIteration(5000)),
+    stopping_criterion=StopAfterIteration(500) #StopWhenAny(StopWhenChangeLess(1e-12), StopAfterIteration(5000)),
 )
-@time m_median = median(M, data)
+@time m_median = median(M, data; stop_iter = 500)
 median_dist = distance(M, b_median, m_median)
 println("Distance between medians: $median_dist")
 println(
-    "$(F2(M, b_median) <= F2(M,m_median) ? "F2(bundle_median) ≤ F2(manifolds_median)" : "F2(bundle_median) > F2(manifolds_median)")",
+    "$(F2(M, b_median) < F2(M,m_median) ? "F2(bundle_median) < F2(manifolds_median)" : "F2(bundle_median) ≥ F2(manifolds_median)")",
 )
+println("    |F2(bundle_median) - F2(manifolds_median)| = $(abs(F2(M, b_median) - F2(M, m_median)))")
