0bdc370c66d00ca3638d5a866e2e4983bfd71a23
Hajg Jasa <hajg.jasa@ntnu.no>
2023-02-22 18:11:40 +0100

    Edits examples

--- examples/bundle_method_examples/max_function_bundle_method.jl
+++ examples/bundle_method_examples/max_function_bundle_method.jl
@@ -25,16 +25,16 @@ function check_maxfunc(M)
         end
 
         # Find intersection point between F and F2
-        G(M, y) = (F(M, y) - F2(M, y))^2
-        m = NelderMead(M, G)
+        # G(M, y) = (F(M, y) - F2(M, y))^2
+        # m = NelderMead(M, G)
         #m = subgradient_method(M, G, gradG, rand(M))
 
         bundle_min = bundle_method(
             M,
             F3,
             subgradF3,
-            m;
-            stopping_criterion=StopAfterIteration(1000),
+            data[51];
+            # stopping_criterion=StopAfterIteration(1000),
             # debug=[:Iteration, :Cost, "\n"],
         )
     end
