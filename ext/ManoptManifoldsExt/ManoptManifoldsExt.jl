bad756db09ccb1b20b7ce23d00a2b905799b177a
Ronny Bergmann <git@ronnybergmann.net>
2023-05-16 16:43:16 +0200

    Hä?

--- ext/ManoptManifoldsExt/ManoptManifoldsExt.jl
+++ ext/ManoptManifoldsExt/ManoptManifoldsExt.jl
@@ -2,7 +2,7 @@ module ManoptManifoldsExt
 
 if isdefined(Base, :get_extension)
     using Manifolds
-    using ManifoldsBase
+    using ManifoldsBase: exp, log, ParallelTransport, vector_transport_to
     using Manopt
     import Manopt:
         max_stepsize,
@@ -24,7 +24,7 @@ else
     # imports need to be relative for Requires.jl-based workflows:
     # https://github.com/JuliaArrays/ArrayInterface.jl/pull/387
     using ..Manifolds
-    using ..ManifoldsBase
+    using ..ManifoldsBase: exp, log, ParallelTransport, vector_transport_to
     using ..Manopt
     import ..Manopt:
         max_stepsize,
