0dde5fe270ffcf3037da36e647af0f8278038e29
Benoît Legat <benoit.legat@gmail.com>
2023-06-04 17:53:46 +0200

    Fix docstring for TwoManifoldProblem (#261)

--- src/plans/primal_dual_plan.jl
+++ src/plans/primal_dual_plan.jl
@@ -1,7 +1,7 @@
 @doc raw"""
     TwoManifoldProblem{
-    MT<:AbstractManifold,NT<:AbstractManifold,O<:AbstractManifoldObjective
-} <: AbstractManoptProblem{MT}
+        MT<:AbstractManifold,NT<:AbstractManifold,O<:AbstractManifoldObjective
+    } <: AbstractManoptProblem{MT}
 
 An abstract type for primal-dual-based problems.
 """
