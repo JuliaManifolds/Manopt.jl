8b7050a64aea412185aa4b532731b9a7561a00df
Hajg Jasa <hajg.jasa@ntnu.no>
2023-06-07 15:21:19 +0200

    Changes update to mu

--- src/solvers/prox_bundle_method.jl
+++ src/solvers/prox_bundle_method.jl
@@ -339,7 +339,8 @@ function step_solver!(mp::AbstractManoptProblem, bms::ProxBundleMethodState, i)
         #                 bms.vector_transport_method,
         #             ),
         #         )
-        bms.μ += bms.δ * bms.μ
+        # bms.μ += bms.δ * bms.μ
+        (length(bms.bundle) < bms.bundle_size) && (bms.μ = (π / (4 * i))^-1)
         copyto!(M, bms.p_last_serious, bms.p)
     end
     l = length(bms.bundle)
