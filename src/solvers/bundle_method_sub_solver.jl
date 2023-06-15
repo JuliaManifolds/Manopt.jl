ed2bb199f488925e9b13b70d0062593296bf74ed
Hajg Jasa <hajg.jasa@ntnu.no>
2023-06-15 17:55:24 +0200

    Introduces dispatch for prox bundle method

--- src/solvers/bundle_method_sub_solver.jl
+++ src/solvers/bundle_method_sub_solver.jl
@@ -1,19 +1,34 @@
 function bundle_method_sub_solver(
-    M::A, bms::Union{BundleMethodState,ProxBundleMethodState}
+    M::A, bms::BundleMethodState
 ) where {A<:AbstractManifold}
     d = length(bms.approx_errors)
-    if typeof(bms) === ProxBundleMethodState # Use multiple dispatch later
-        H =
-            1 / bms.μ * [
-                inner(M, bms.p_last_serious, X, Y) for X in bms.transported_subgradients,
-                Y in bms.transported_subgradients
-            ]
-    else
-        H = [
+    H = [
+        inner(M, bms.p_last_serious, X, Y) for X in bms.transported_subgradients,
+        Y in bms.transported_subgradients
+    ]
+    B = reshape(ones(d), 1, d)
+    qm = QuadraticModel(
+        bms.approx_errors,
+        sparse(tril(H));
+        A=B,
+        lcon=[1.0],
+        ucon=[1.0],
+        lvar=zeros(d),
+        uvar=[Inf for i in 1:d],
+        c0=0.0,
+    )
+    return ripqp(qm; display=false).solution
+    # return ripqp(qm; itol=RipQP.InputTol(ϵ_rb=1e-6, ϵ_rc=1e-6), mode=:zoom).solution
+end
+function bundle_method_sub_solver(
+    M::A, bms::ProxBundleMethodState
+) where {A<:AbstractManifold}
+    d = length(bms.approx_errors)
+    H =
+        1 / bms.μ * [
             inner(M, bms.p_last_serious, X, Y) for X in bms.transported_subgradients,
             Y in bms.transported_subgradients
         ]
-    end
     B = reshape(ones(d), 1, d)
     qm = QuadraticModel(
         bms.approx_errors,
