4adacd348db737f35f964ff1e0bb90658b9cbfcc
Hajg Jasa <hajg.jasa@ntnu.no>
2023-06-29 11:09:38 +0200

    Minor type-stable changes

--- src/solvers/bundle_method_sub_solver.jl
+++ src/solvers/bundle_method_sub_solver.jl
@@ -12,34 +12,33 @@ function bundle_method_sub_solver(M::A, bms::BundleMethodState) where {A<:Abstra
         bms.lin_errors[bms.positive_indices],
         sparse(tril(H));
         A=reshape(ones(d), 1, d),
-        lcon=[1.0],
-        ucon=[1.0],
+        lcon=[one(eltype(d))],
+        ucon=[one(eltype(d))],
         lvar=zeros(d),
         uvar=[Inf for i in 1:d],
-        c0=0.0,
+        c0=zero(eltype(d)),
     )
     return ripqp(qm; display=false).solution
     # return ripqp(qm; itol=RipQP.InputTol(ϵ_rb=1e-6, ϵ_rc=1e-6), mode=:zoom).solution
 end
 function bundle_method_sub_solver(
-    M::A, bms::ProxBundleMethodState
+    M::A, pbms::ProxBundleMethodState
 ) where {A<:AbstractManifold}
-    d = length(bms.approx_errors)
+    d = length(pbms.approx_errors)
     H =
-        1 / bms.μ * [
-            inner(M, bms.p_last_serious, X, Y) for X in bms.transported_subgradients,
-            Y in bms.transported_subgradients
+        1 / pbms.μ * [
+            inner(M, pbms.p_last_serious, X, Y) for X in pbms.transported_subgradients,
+            Y in pbms.transported_subgradients
         ]
-    B = reshape(ones(d), 1, d)
     qm = QuadraticModel(
-        bms.approx_errors,
-        tril(H);
-        A=B,
-        lcon=[1.0],
-        ucon=[1.0],
+        pbms.approx_errors,
+        sparse(tril(H));
+        A=reshape(ones(d), 1, d),
+        lcon=[one(eltype(d))],
+        ucon=[one(eltype(d))],
         lvar=zeros(d),
         uvar=[Inf for i in 1:d],
-        c0=0.0,
+        c0=zero(eltype(d)),
     )
     return ripqp(qm; display=false).solution
 end
