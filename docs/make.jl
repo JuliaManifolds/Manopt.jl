4b825149bc376fc5b9e390623849a1855e955e82
Ronny Bergmann <git@ronnybergmann.net>
2023-07-13 20:21:24 +0200

    runs formatter.

--- docs/make.jl
+++ docs/make.jl
@@ -56,8 +56,11 @@ end
 
 # (e) ...finally! make docs
 bib = CitationBibliography(joinpath(@__DIR__, "src", "references.bib"); style=:alpha)
-makedocs(bib;
-    format=Documenter.HTML(; mathengine=MathJax3(), prettyurls=get(ENV, "CI", nothing) == "true"),
+makedocs(
+    bib;
+    format=Documenter.HTML(;
+        mathengine=MathJax3(), prettyurls=get(ENV, "CI", nothing) == "true"
+    ),
     modules=[Manopt],
     sitename="Manopt.jl",
     pages=[
