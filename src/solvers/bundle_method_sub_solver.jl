b1ccfec1bc001203b1572d41bbb983464a6fca8a
Hajg Jasa <hajg.jasa@ntnu.no>
2023-07-20 11:56:49 +0200

    Conforms subsolver to faster bundle branch

--- src/solvers/bundle_method_sub_solver.jl
+++ src/solvers/bundle_method_sub_solver.jl
@@ -4,39 +4,36 @@ function bundle_method_sub_solver(M::A, bms::BundleMethodState) where {A<:Abstra
         inner(M, bms.p_last_serious, X, Y) for X in bms.transported_subgradients,
         Y in bms.transported_subgradients
     ]
-    B = reshape(ones(d), 1, d)
     qm = QuadraticModel(
         bms.approx_errors,
         sparse(tril(H));
-        A=B,
-        lcon=[1.0],
-        ucon=[1.0],
+        A=reshape(ones(d), 1, d),
+        lcon=[one(eltype(bms.approx_errors))],
+        ucon=[one(eltype(bms.approx_errors))],
         lvar=zeros(d),
         uvar=[Inf for i in 1:d],
-        c0=0.0,
+        c0=zero(eltype(bms.approx_errors)),
     )
     return ripqp(qm; display=false).solution
-    # return ripqp(qm; itol=RipQP.InputTol(ϵ_rb=1e-6, ϵ_rc=1e-6), mode=:zoom).solution
 end
 function bundle_method_sub_solver(
-    M::A, bms::ProxBundleMethodState
+    M::A, pbms::ProxBundleMethodState
 ) where {A<:AbstractManifold}
-    d = length(bms.approx_errors)
+    d = length(pbms.approx_errors)
     H =
-        1 / bms.μ * [
-            inner(M, bms.p_last_serious, X, Y) for X in bms.transported_subgradients,
-            Y in bms.transported_subgradients
+        1 / pbms.μ * [
+            inner(M, pbms.p_last_serious, X, Y) for X in pbms.transported_subgradients,
+            Y in pbms.transported_subgradients
         ]
-    B = reshape(ones(d), 1, d)
     qm = QuadraticModel(
-        bms.approx_errors,
-        tril(H);
-        A=B,
-        lcon=[1.0],
-        ucon=[1.0],
+        pbms.approx_errors,
+        sparse(tril(H));
+        A=reshape(ones(d), 1, d),
+        lcon=[one(eltype(pbms.lin_errors))],
+        ucon=[one(eltype(pbms.lin_errors))],
         lvar=zeros(d),
         uvar=[Inf for i in 1:d],
-        c0=0.0,
+        c0=zero(eltype(pbms.lin_errors)),
     )
     return ripqp(qm; display=false).solution
 end
