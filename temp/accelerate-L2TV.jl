85494d351a7dad9f6f039e2d0076d82c4a98eecc
Ronny Bergmann <git@ronnybergmann.net>
2024-02-12 08:26:50 +0100

    Finalize L2TV experiment – maybe lets not use that.

--- temp/accelerate-L2TV.jl
+++ temp/accelerate-L2TV.jl
@@ -66,21 +66,79 @@ end;
 # ╔═╡ 25edd57a-2c23-46fc-a1ee-fd199cafd36c
 begin # mutating versions
 	function prox1!(N2, q, λ, p)
-		ManifoldDiff.prox_distance!(N2.manifold, @view(q[1]), λ, f, @view(p[1]))
-		ManoptExamples.prox_parallel_TV!(N2.manifold, @view(q[2:5]), w * λ, @view(p[2:5]))
+		ManifoldDiff.prox_distance!(N2.manifold, q[1], λ, f, p[1])
+		ManoptExamples.prox_parallel_TV!(N2.manifold, q[2:5], w * λ, p[2:5])
 		return q
 	end
 	function prox2!(N2, q, λ, p)
-		q1 = @view(q[1])
-		mean!(N2,manifold, q1, p, GradientDescentEstimation())
-		for i = 2:length(q)
-			copyto!(N2.manifold, @view(q[i]), q1)
+		q_ = copy(N2.manifold, q[1])
+		mean!(N2.manifold, q_, p, GradientDescentEstimation())
+		for i = 1:length(q)
+			copyto!(N2.manifold, q[i], q_)
 		end
 	end
 end
 
 # ╔═╡ f882b4c6-b124-4a5e-9e6c-5857a9e17045
-sc = StopWhenChangeLess(1e-6);
+sc = StopWhenChangeLess(1e-6) | StopAfterIteration(1500);
+
+# ╔═╡ b166715e-00ee-4661-8ff4-80e3d281b9ec
+begin # Fix prox! for now
+	import ManoptExamples: prox_parallel_TV!
+function prox_parallel_TV!(
+    M::PowerManifold, y::AbstractVector, λ, x::AbstractVector, p::Int=1
+)
+    R = CartesianIndices(x[1])
+    d = ndims(x[1])
+    if length(x) != 2 * d
+        throw(
+            ErrorException(
+                "The number of inputs from the array ($(length(x))) has to be twice the data dimensions ($(d)).",
+            ),
+        )
+    end
+    maxInd = Tuple(last(R))
+    # init y
+    for i in 1:length(x)
+        copyto!(M.manifold, y[i], x[i])
+    end
+    yV = reshape(y, d, 2)
+    xV = reshape(x, d, 2)
+    for k in 1:d # for all directions
+        ek = CartesianIndex(ntuple(i -> (i == k) ? 1 : 0, d)) #k th unit vector
+        for l in 0:1 # even odd
+            for i in R # iterate over all pixel
+                if (i[k] % 2) == l
+                    J = i.I .+ ek.I #i + e_k is j
+                    if all(J .<= maxInd) # is this neighbor in range?
+                        j = CartesianIndex(J...) # neighbour index as Cartesian Index
+                        # parallel means we apply each (direction even/odd) to a separate copy of the data.
+                        ManoptExamples.prox_Total_Variation!(
+                            M.manifold,
+                            [yV[k, l + 1][i], yV[k, l + 1][j]],
+                            λ,
+                            (xV[k, l + 1][i], xV[k, l + 1][j]),
+                            p,
+                        ) # Compute TV on these in place of y
+                    end
+                end
+            end # i in R
+        end # even odd
+    end # directions
+    return y
+end
+end
+
+# ╔═╡ 0fa3ccff-d866-4922-a5b0-18fa55676536
+begin
+	Nt = PowerManifold(M, NestedPowerRepresentation(), 2,2)
+	N2t = PowerManifold(Nt, 5)
+	xT = f[1:2,1:2]
+	yT = [deepcopy(xT), deepcopy(xT),deepcopy(xT),deepcopy(xT),deepcopy(xT)]
+	zT = deepcopy(yT)
+	zT2 = ManoptExamples.prox_parallel_TV(N2t, w * λ, yT[2:5])
+	prox_parallel_TV!(N2t, zT[2:5], w * λ, yT[2:5])
+end
 
 # ╔═╡ a604aaa8-b929-4e50-a0e0-4b90dd97a7f2
 @time s1 = DouglasRachford(
@@ -97,16 +155,81 @@ sc = StopWhenChangeLess(1e-6);
 )
 
 # ╔═╡ c686565c-4639-4fb8-b80c-2e51fb17e9f8
-q1 = y = get_solver_result(s1)[1];
+q1 = get_solver_result(s1)[1];
 
 # ╔═╡ 1f2366dd-1965-4138-bbb3-1da7fed6547d
 r1 = get_record(s1)
 
+# ╔═╡ 730b20f7-5f44-4ed7-91a2-737aa7d4647a
+@time s2 = DouglasRachford(
+    N2,
+    cost,
+    [prox1!, prox2!],
+    p0;
+    λ=i -> λ,
+    α=i -> α,
+	debug =  [:Iteration, " | ", :Cost, :Change, "\n", 100, :Stop],
+	evaluation= InplaceEvaluation(),
+    record = [:Iteration, :Cost],
+    return_state=true,
+    stopping_criterion=sc,
+)
+
+# ╔═╡ 979746e1-9e60-4a19-bd99-c315c2726221
+q2 = get_solver_result(s2)[1];
+
+# ╔═╡ c2ff7e0c-d624-4d6b-8e84-ac2f3fbaff20
+r2 = get_record(s2)
+
+# ╔═╡ 767a92b1-0a5c-4360-a976-876905710888
+# acceleration
+@time s3 = DouglasRachford(
+    N2,
+    cost,
+    [prox1, prox2],
+    p0;
+	n=2,
+    λ=i -> λ,
+    α=i -> α,
+	debug =  [:Iteration, " | ", :Cost, :Change, "\n", 100, :Stop],
+    record = [:Iteration, :Cost],
+    stopping_criterion=sc,
+    return_state=true,
+)
+
+# ╔═╡ 3217cd53-fddb-4fd4-bf7f-1a0586812de5
+q3 = get_solver_result(s3)[1];
+
+# ╔═╡ 96045d19-96ad-4ec6-a292-9bd908c53b7e
+r3 = get_record(s3)
+
+# ╔═╡ cee27e3d-0e12-4cd5-967e-1997e8bac89a
+# ineratia
+@time s4 = DouglasRachford(
+    N2,
+    cost,
+    [prox1, prox2],
+    p0;
+	θ=i -> 0.0,
+    λ=i -> λ,
+    α=i -> α,
+	debug =  [:Iteration, " | ", :Cost, :Change, "\n", 100, :Stop],
+    record = [:Iteration, :Cost],
+    stopping_criterion=sc,
+    return_state=true,
+)
+
+# ╔═╡ cbf627bc-7433-4ae8-bd32-33a8492eb16f
+q4 = get_solver_result(s4)[1];
+
+# ╔═╡ 8ae6501b-960f-4620-b63c-7299369259a6
+r4 = get_record(s4)
+
 # ╔═╡ 23102383-7643-4113-af35-d3a08d868d6c
-iterates = [ [e[1] for e in get_record(s, :Iteration)] for s in [s1]];
+iterates = [ [e[1] for e in get_record(s, :Iteration)] for s in [s1, s2, s3, s4]];
 
 # ╔═╡ 2b24563f-d1b7-4908-b06d-de836a5126c4
-costs = [ [e[2] for e in get_record(s, :Iteration)] for s in [s1]];
+costs = [ [e[2] for e in get_record(s, :Iteration)] for s in [s1, s2, s3, s4]];
 
 # ╔═╡ 2276a077-0e85-4b03-afcc-a8b185803c62
 begin
@@ -117,10 +240,9 @@ begin
 		#xlim = (0,34),
     );
 	plot!(fig, iterates[1], costs[1], color=indigo, label="Douglas-Rachford");
-#	plot!(fig, iterates[2], costs[2], color=green, label=raw"DR with intertia of $0.12$");
-#	plot!(fig, iterates[3], costs[3], color=sand, label=raw"DR with 3-acceleration");
-#	plot!(fig, iterates[4], costs[4], color=teal, label=raw"DR both previous ones");
-#	plot!(fig, iterates[5], costs[5], color=wine, label=raw"Euclidean L-BFGS");
+	#plot!(fig, iterates[2], costs[2], color=green, label=raw"DR with inplace test");
+	plot!(fig, iterates[3], costs[3], color=sand, label=raw"DR with 3-acceleration");
+	plot!(fig, iterates[4], costs[4], color=teal, label=raw"DR with intertia");
 	fig
 end
 
@@ -133,9 +255,20 @@ end
 # ╠═dab95acd-efcc-4f9c-aa05-a02e21d035fb
 # ╠═25edd57a-2c23-46fc-a1ee-fd199cafd36c
 # ╠═f882b4c6-b124-4a5e-9e6c-5857a9e17045
+# ╠═b166715e-00ee-4661-8ff4-80e3d281b9ec
+# ╠═0fa3ccff-d866-4922-a5b0-18fa55676536
 # ╠═a604aaa8-b929-4e50-a0e0-4b90dd97a7f2
 # ╠═c686565c-4639-4fb8-b80c-2e51fb17e9f8
 # ╠═1f2366dd-1965-4138-bbb3-1da7fed6547d
+# ╠═730b20f7-5f44-4ed7-91a2-737aa7d4647a
+# ╠═979746e1-9e60-4a19-bd99-c315c2726221
+# ╠═c2ff7e0c-d624-4d6b-8e84-ac2f3fbaff20
+# ╠═767a92b1-0a5c-4360-a976-876905710888
+# ╠═3217cd53-fddb-4fd4-bf7f-1a0586812de5
+# ╠═96045d19-96ad-4ec6-a292-9bd908c53b7e
+# ╠═cee27e3d-0e12-4cd5-967e-1997e8bac89a
+# ╠═cbf627bc-7433-4ae8-bd32-33a8492eb16f
+# ╠═8ae6501b-960f-4620-b63c-7299369259a6
 # ╠═23102383-7643-4113-af35-d3a08d868d6c
 # ╠═2b24563f-d1b7-4908-b06d-de836a5126c4
 # ╠═2276a077-0e85-4b03-afcc-a8b185803c62
