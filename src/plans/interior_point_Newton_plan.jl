6138dbdc15b4547e25d56e5d367cde68e139a453
mstokkenes <77609740+mstokkenes@users.noreply.github.com>
2024-04-04 14:46:33 +0200

    Fixed LagrangianCost and LagrangianGrad

--- src/plans/interior_point_Newton_plan.jl
+++ src/plans/interior_point_Newton_plan.jl
@@ -9,8 +9,8 @@ based on the internal [`ConstrainedManifoldObjective`](@ref) it computes
 
 ```math
 \mathcal L_\rho(p, μ, λ)
-= f(p) + \sum_{i=1}^m λ_i g_i(p) + \sum_{j=1}^n μ_j h_j(p)
-= f(p) + λ^{\intercal} g(p) + μ^{\intercal} h(p)
+= f(p) + \sum_{i=1}^m μ_i g_i(p) + \sum_{j=1}^n λ_j h_j(p)
+= f(p) + μ^{\intercal} g(p) + λ^{\intercal} h(p)
 ```
 
 ## Fields
@@ -22,6 +22,7 @@ vector type used.
 
     LagrangianCost(co, μ, λ)
 """
+
 mutable struct LagrangianCost{CO,T}
     co::CO
     μ::T
@@ -35,17 +36,22 @@ function set_manopt_parameter!(lc::LagrangianCost, ::Val{:λ}, λ)
     lc.λ = λ
     return lc
 end
-function (L::LagrangianCost)(M::AbstractManifold, p)
-    c = get_cost(M, L.co, p)
-    gp = get_equality_constraints(M, L.co, p)
-    hp = get_inequality_constraints(M, L.co, p)
-    return c + (L.λ)'gp + (L.μ)'hp
+
+function (lc::LagrangianCost)(M::AbstractManifold, p)
+    gp = get_inequality_constraints(M, lc.co, p)
+    hp = get_equality_constraints(M, lc.co, p)
+    m = length(gp)
+    n = length(hp)
+    L = get_cost(M, lc.co, p)
+    (m > 0) && (L += (lc.μ)'gp)
+    (n > 0) && (L += (lc.λ)'hp)
+    return L
 end
 
 @doc raw"""
     LagrangianGrad{CO,R,T}
 
-Stores the parameters ``ρ ∈ ℝ``, ``μ ∈ ℝ^m``, ``λ ∈ ℝ^n``
+Stores the parameters ``μ ∈ ℝ^m``, ``λ ∈ ℝ^n``
 of the Lagrangian associated to the [`ConstrainedManifoldObjective`](@ref) `co`.
 
 This struct is also a functor in both formats
@@ -53,7 +59,7 @@ This struct is also a functor in both formats
 * `(M, X, p)` to compute the gradient in in-place fashion.
 
 based on the internal [`ConstrainedManifoldObjective`](@ref) and computes the gradient
-``\operatorname{grad} \mathcal L_{ρ}(p, μ, λ)``, see also [`LagrangianCost`](@ref).
+``\operatorname{grad} \mathcal L(p, μ, λ)``, see also [`LagrangianCost`](@ref).
 
 ## Fields
 
@@ -70,10 +76,12 @@ mutable struct LagrangianGrad{CO,T}
     μ::T
     λ::T
 end
-function (LG::LagrangianGrad)(M::AbstractManifold, p)
+
+function (lg::LagrangianGrad)(M::AbstractManifold, p)
     X = zero_vector(M, p)
-    return LG(M, X, p)
+    return lg(M, X, p)
 end
+
 function set_manopt_parameter!(lg::LagrangianGrad, ::Val{:μ}, μ)
     lg.μ = μ
     return lg
@@ -84,34 +92,72 @@ function set_manopt_parameter!(lg::LagrangianGrad, ::Val{:λ}, λ)
 end
 
 # default, that is especially when the `grad_g` and `grad_h` are functions.
-function (LG::LagrangianGrad)(M::AbstractManifold, X, p)
-    grad_fp = get_gradient(M, X, LC.co, p)
-    grad_gp = get_grad_equality_constraints(M, LG.co, p) 
-    grad_hp = get_grad_equality_constraints(M, LG.co, p) 
-    return grad_fp + (LG.λ)'grad_gp + (LG.μ)'grad_hp
+function (lg::LagrangianGrad)(M::AbstractManifold, X, p)
+    grad_gp = get_grad_inequality_constraints(M, lg.co, p) 
+    grad_hp = get_grad_equality_constraints(M, lg.co, p)
+    m = length(grad_gp)
+    n = length(grad_hp)
+    get_gradient!(M, X, lg.co, p)
+    (m > 0) && (X .+= sum(lg.μ .* grad_gp))
+    (n > 0) && (X .+= sum(lg.λ .* grad_hp))
+    return X
 end
+
 # Allocating vector -> omit a few of the inequality gradient evaluations.
 function (
-    LG::LagrangianGrad{
+    lg::LagrangianGrad{
         <:ConstrainedManifoldObjective{AllocatingEvaluation,<:VectorConstraint}
     }
 )(
     M::AbstractManifold, X, p
 )
-    m = length(LG.co.g)
-    n = length(LG.co.h)
-    get_gradient!(M, X, LG.co, p)
+    m = length(lg.co.g)
+    n = length(lg.co.h)
+    get_gradient!(M, X, lg.co, p)
     for i in 1:m
-        X .+= LG.λ[i] .* get_grad_equality_constraint(M, LG.co, p, i)
+        gpi = get_inequality_constraint(M, lg.co, p, i)
+        (gpi < 0) && (X .+= lg.μ[i] .* get_grad_inequality_constraint(M, lg.co, p, i))
     end
     for j in 1:n
-        hpj = get_inequality_constraint(M, LG.co, p, i)
-        if hpj < 0 # only evaluate gradient if necessary
-            X .+= LG.μ[j] .* get_grad_inequality_constraint(M, LG.co, p, j)
-        end
+        X .+= lg.λ[j] .* get_grad_equality_constraint(M, lg.co, p, j)
     end
     return X
 end
 
 
 
+
+# mutable struct LagrangianHess{CO,T}
+#     co::CO
+#     μ::T
+#     λ::T
+# end
+
+# function (lh::LagrangianHess)(M::AbstractManifold, p)
+#     d = manifold_dimension(M)
+#     Y = zeros(d, d)
+#     return lh(M, H, p)
+# end
+
+# function set_manopt_parameter!(lh::LagrangianHess, ::Val{:μ}, μ)
+#     lh.μ = μ
+#     return lh
+# end
+# function set_manopt_parameter!(lh::LagrangianHess, ::Val{:λ}, λ)
+#     lh.λ = λ
+#     return lh
+# end
+
+# # default, that is especially when the `grad_g` and `grad_h` are functions.
+# function (lh::LagrangianHess)(M::AbstractManifold, Y, p, X)
+#     H = get_hessian_function(lh.co)
+#     hess_gp = get_grad_inequality_constraints(M, lg.co, p) 
+#     hess_hp = get_grad_equality_constraints(M, lg.co, p)
+#     m = length(grad_gp)
+#     n = length(grad_hp)
+
+#     get_gradient!(M, X, lg.co, p)
+#     (m > 0) && (X .+= sum(lg.μ .* grad_gp))
+#     (n > 0) && (X .+= sum(lg.λ .* grad_hp))
+#     return X
+# end
--- src/solvers/interior_point_Newton.jl
+++ src/solvers/interior_point_Newton.jl
@@ -4,19 +4,24 @@
 
 # # Fields
 
-# * `p`:                  position on M
-# * `λ`:                  Lagrange multiplier corresponding to equality constraints
+# * `p`:                  point on M
 # * `μ`:                  Lagrange multiplier corresponding to inequality constrains 
+# * `λ`:                  Lagrange multiplier corresponding to equality constraints
 # * `s`:                  slack variable
+# * `γ`:
+# * `σ`:
+# * `ρ`:
 # * `stopping_criterion`: stopping criterion
+# * `step_size`
+# * `retraction_method`:  retraction method
 
 # # Constructor
 
 # InteriorPointNewtonState( M::AbstractManifold, 
 #                           co::ConstrainedManifoldObjective, 
 #                           p = rand(M),
-#                           λ = ones(m),
-#                           μ = ones(n),
+#                           μ = ones(m),
+#                           λ = ones(n),
 #                           stopping_criterion = StopAfterIteration(300); 
 #                           kwargs...
 #                         )
@@ -31,38 +36,38 @@ mutable struct InteriorPointNewtonState{
     TRTM<:AbstractRetractionMethod,
 } <: AbstractManoptSolverState
     p::P
-    λ::T
     μ::T
+    λ::T
     s::T
     γ::R
     σ::R
     ρ::R
-    stop::TStop
-    stepsize::TStepsize
+    stopping_criterion::TStop
+    step_size::TStepsize
     retraction_method::TRTM
     function InteriorPointNewtonState{P,T}(
         M::AbstractManifold,
         p::P,
-        λ::T,
         μ::T,
+        λ::T,
         s::T,
         γ::Real                                     = 0.5*rand() + 1,
         σ::Real                                     = rand(),
         ρ::Real                                     = 0.0,
-        stop::StoppingCriterion                     = StopAfterIteration(100),
-        step::Stepsize                              = default_stepsize(M, InteriorPointNewtonState),
+        stopping_criterion::StoppingCriterion       = StopAfterIteration(100),
+        step_size::Stepsize                         = default_stepsize(M, InteriorPointNewtonState),
         retraction_method::AbstractRetractionMethod = default_retraction_method(M, typeof(p)),
     ) where {P,T}
-        state = new{P, T, Real, typeof(stop), typeof(step), typeof(direction), typeof(retraction_method)}()
+        state = new{P, T, Real, typeof(stopping_criterion), typeof(step_size), typeof(retraction_method)}()
         state.p = p
-        state.λ = λ
         state.μ = μ
+        state.λ = λ
         state.s = s
         state.γ = γ
         state.σ = σ
         state.ρ = ρ
-        state.stop = stop
-        state.stepsize = step
+        state.stopping_criterion = stopping_criterion
+        state.step_size = step_size
         state.retraction_method = retraction_method
         return state
     end
@@ -72,18 +77,18 @@ function InteriorPointNewtonState(
     M::AbstractManifold,
     co::ConstrainedManifoldObjective,
     p::P                                        = rand(M);
-    λ::T                                        = ones( length( get_equality_constraints(M, co, p) ) ),
     μ::T                                        = ones( length( get_inequality_constraints(M, co, p) ) ),
+    λ::T                                        = ones( length( get_equality_constraints(M, co, p) ) ),
     s::T                                        = ones( length( get_inequality_constraints(M, co, p) ) ),
     γ::Real                                     = 0.5*rand() + 1,
     σ::Real                                     = rand(),
     ρ::Real                                     = 0.0,
     stopping_criterion::StoppingCriterion       = StopAfterIteration(100),
     retraction_method::AbstractRetractionMethod = default_retraction_method(M, typeof(p)),
-    stepsize::Stepsize                          = default_stepsize(M, InteriorPointNewtonState; retraction_method = retraction_method),
+    step_size::Stepsize                         = default_stepsize(M, InteriorPointNewtonState; retraction_method = retraction_method),
 ) where {P,T}
     return InteriorPointNewtonState{P,T}(
-        M, p, λ, μ, s, γ, σ, ρ, stopping_criterion, stepsize, direction, retraction_method
+        M, p, μ, λ, s, γ, σ, ρ, stopping_criterion, step_size, retraction_method
     )
 end
 
@@ -102,46 +107,86 @@ function is_feasible(
     p
 )
     # evaluate constraint functions at p
-    gp = get_equality_constraints(M, co, p)
-    hp = get_inequality_constraints(M, co, p)
+    gp = get_inequality_constraints(M, co, p)
+    hp = get_equality_constraints(M, co, p)
 
     # check feasibility
-    return all(gp .== 0) && all(hp .<= 0)
+    return all(gp .<= 0) && all(hp .== 0)
 end
 
-function KKTVectorField(
-    M::AbstractManifold,
-    co::ConstrainedManifoldObjective,
-    p,
-    λ,
-    μ,
-    s
-)
-    gradL = LagrangianGrad(co, λ, μ)
-    gp = get_equality_constraints(M, p)
-    hp = get_inequality_constraints(M, p)
-    diagM = Diagonal(μ)
-    diagS = Diagonal(s)
-    e = ones(length(hp))
-    return [gradL, gp, hp+s, diagM*diagS*e]
-end
+# function KKTVectorField(
+#     M::AbstractManifold,
+#     co::ConstrainedManifoldObjective,
+#     p,
+#     λ,
+#     μ,
+#     s
+# )
+#     gradL = LagrangianGrad(co, λ, μ)
+#     gp = get_inequality_constraints(M, p)
+#     hp = get_equality_constraints(M, p)
+#     diagM = Diagonal(μ)
+#     diagS = Diagonal(s)
+#     e = ones(length(hp))
+#     return [gradL, gp, hp+s, diagM*diagS*e]
+# end
 
-function subsolver(
-    M::AbstractManifold, 
-    co::ConstrainedManifoldObjective,
-    ipns::InteriorPointNewtonState,
-    X
-)
-    F = KKTVectorField(M, co, ipns.p, ipns.λ, ipns.μ, ipns.s)
-    # compute covariant derivative of kkt
-    # solve for X: ∇F[X] = -F + σ_k ρ_k ̂e
-    # return X
+# function covariant_derivative_KKTVectorField(
+#     M::AbstractManifold,
+#     co::ConstrainedManifoldObjective,
+#     p,
+#     λ,
+#     μ,
+#     s
+# )
+#     L = LagrangianCost(co, λ, μ)
+#     gp = get_equality_constraints(M, p)
+#     hp = get_inequality_constraints(M, p)
+#     diagM = Diagonal(μ)
+#     diagS = Diagonal(s)
+#     I     = ones()
+#     e = ones(length(hp))
 
-    # The matrix representation of the covariant derivative
-    # of the KKT vector field is given by
-    #      
-    #       | ∇^2 L   J_g^T   J_h^T   0 |
-    #  ∇F = |  J_g      0       0     0 |
-    #       |  J_h      0       0     I |
-    #       |   0       0       S     M |
-end
+
+# function subsolver(
+#     M::AbstractManifold, 
+#     co::ConstrainedManifoldObjective,
+#     ipns::InteriorPointNewtonState,
+# )
+#     F = KKTVectorField(M, co, ipns.p, ipns.λ, ipns.μ, ipns.s)
+#     # compute covariant derivative of kkt
+#     # solve for X: ∇F[X] = -F + σ_k ρ_k ̂e
+#     # return X
+
+#     # The matrix representation of the covariant derivative
+#     # of the KKT vector field is given by
+#     #      
+#     #       | ∇^2 L   J_g^T   J_h^T   0 |
+#     #  ∇F = |  J_g      0       0     0 |
+#     #       |  J_h      0       0     I |
+#     #       |   0       0       S     M |
+# end
+
+# function step_solver(M::AbstractManifold, co::ConstrainedManifoldObjective, ipns::InteriorPointNewtonState, iteration)
+
+#     Δq = subsolver(M, co, ipns)
+#     γ = (1-γ_hat)*rand() + γ_hat
+#     α = min( 1, γ*min(...), γ*min(...) )
+#     q_next = R(α*Δq)
+#     β_next = β*rand()
+#     ipns
+
+#     return ipns
+
+
+# Proto-RIPM(q_00, R, ̂γ, β_0)
+# 1 k ← 0
+# 2 while stopping criterion not satisfied
+# 3   Solve for ∆qk : 
+#         ∇F(q_k)[∆q_k] = −F(q_k) + β_k ̂e
+# 4   Choose γ_k ∈ [̂γ, 1]
+# 5 α_k ← min { 1, γ_k min_i { -μ_k^i / Δμ_k^i : μ_k^i < 0 }, γ_k min_i { -s_k^i / Δs_k^i : s_k^i < 0 }}
+# 6 q_{k+1} ← ̄R_{q_k}(α_k Δq_k)
+# 7 Choose β_{k+1} ∈ (0, β_k)
+# 8 k ← k + 1
+
