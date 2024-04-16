344de832f5e247caab46a7c0d4f9c231a43587e7
mstokkenes <77609740+mstokkenes@users.noreply.github.com>
2024-04-16 16:59:48 +0200

    Rewrote class structures and generalized the subsolver

--- src/solvers/interior_point_Newton.jl
+++ src/solvers/interior_point_Newton.jl
@@ -1,182 +1,204 @@
-@doc raw"""
-InteriorPointNewtonState <: AbstractManoptSolverState
+mutable struct InteriorPointProblem
+    M::AbstractManifold
+    ob::ConstrainedManifoldObjective
+    dims::Tuple{Int,Int}
+end
 
-# Fields
+mutable struct InteriorPointParams{Q}
+    q::Q    
+    b::Real
+end
 
-* `p`:                  point on M
-* `μ`:                  Lagrange multiplier corresponding to inequality constrains
-* `λ`:                  Lagrange multiplier corresponding to equality constraints
-* `s`:                  slack variable
-* `γ`:
-* `σ`:
-* `ρ`:
-* `stopping_criterion`: stopping criterion
-* `step_size`
-* `retraction_method`:  retraction method
+function InteriorPointParams(problem::InteriorPointProblem)
+    M = problem.M
+    m, n = problem.dims
+    q = rand(M × ℝ^m × ℝ^n × ℝ^m)
+    b = rand()
+    return InteriorPointParams(q, b)
+end
 
-# Constructor
+mutable struct InteriorPointLagrangian
+    problem::InteriorPointProblem
+    params::InteriorPointParams
+end
 
-InteriorPointNewtonState( M::AbstractManifold,
-                          co::ConstrainedManifoldObjective,
-                          p = rand(M),
-                          μ = ones(m),
-                          λ = ones(n),
-                          stopping_criterion = StopAfterIteration(300);
-                          kwargs...
-                        )
-"""
-mutable struct InteriorPointNewtonState{
-    P,T,R<:Real,TStop<:StoppingCriterion,TStepsize<:Stepsize,TRTM<:AbstractRetractionMethod
-} <: AbstractManoptSolverState
-    p::P
-    μ::T
-    λ::T
-    s::T
-    γ::R
-    σ::R
-    ρ::R
-    stopping_criterion::TStop
-    step_size::TStepsize
-    retraction_method::TRTM
-    function InteriorPointNewtonState{P,T}(
-        M::AbstractManifold,
-        p::P,
-        μ::T,
-        λ::T,
-        s::T,
-        γ::Real=0.5 * rand() + 1,
-        σ::Real=rand(),
-        ρ::Real=0.0,
-        stopping_criterion::StoppingCriterion=StopAfterIteration(100),
-        step_size::Stepsize=default_stepsize(M, InteriorPointNewtonState),
-        retraction_method::AbstractRetractionMethod=default_retraction_method(M, typeof(p)),
-    ) where {P,T}
-        state = new{
-            P,T,Real,typeof(stopping_criterion),typeof(step_size),typeof(retraction_method)
-        }()
-        state.p = p
-        state.μ = μ
-        state.λ = λ
-        state.s = s
-        state.γ = γ
-        state.σ = σ
-        state.ρ = ρ
-        state.stopping_criterion = stopping_criterion
-        state.step_size = step_size
-        state.retraction_method = retraction_method
-        return state
+function grad(L::InteriorPointLagrangian)
+
+    M = L.problem.M
+    ob = L.problem.ob
+
+    p, μ, λ, s = L.params.q.x
+    b = L.params.b
+
+    gp = get_inequality_constraints(M, ob, p)
+    hp = get_equality_constraints(M, ob, p)
+    Jgp = get_grad_inequality_constraints(M, ob, p) 
+    Jhp = get_grad_equality_constraints(M, ob, p)
+
+    m, n = L.problem.dims
+
+    Xp = get_gradient(M, ob, p)
+    Xμ = Float64[]
+    Xλ = Float64[]
+    Xs = Float64[]
+
+    if m > 0
+        Xp += Jgp'μ
+        Xμ = gp + s
+        Xs = μ - b*ones(m)./s
     end
+    if n > 0
+        Xp += Jhp'λ
+        Xλ = hp
+    end
+
+    return ArrayPartition(Xp, Xμ, Xλ, Xs)
 end
 
-function InteriorPointNewtonState(
-    M::AbstractManifold,
-    co::ConstrainedManifoldObjective,
-    p::P=rand(M);
-    μ::T=ones(length(get_inequality_constraints(M, co, p))),
-    λ::T=ones(length(get_equality_constraints(M, co, p))),
-    s::T=ones(length(get_inequality_constraints(M, co, p))),
-    γ::Real=0.5 * rand() + 1,
-    σ::Real=rand(),
-    ρ::Real=0.0,
-    stopping_criterion::StoppingCriterion=StopAfterIteration(100),
-    retraction_method::AbstractRetractionMethod=default_retraction_method(M, typeof(p)),
-    step_size::Stepsize=default_stepsize(
-        M, InteriorPointNewtonState; retraction_method=retraction_method
-    ),
-) where {P,T}
-    return InteriorPointNewtonState{P,T}(
-        M, p, μ, λ, s, γ, σ, ρ, stopping_criterion, step_size, retraction_method
-    )
+function Hess(L::InteriorPointLagrangian, Xq)
+    ob = L.problem.ob
+    p, μ, λ, s = L.params.q.x
+    b = L.params.b
+    Xp, Xμ, Xλ, Xs = Xq.x
+    Jgp = get_grad_inequality_constraints(M,ob, p) 
+    Jhp = get_grad_equality_constraints(M,ob, p)
+
+    m = length(μ)
+    n = length(λ)
+
+    Yp = zero_vector(M, p)
+    Yμ = Float64[]
+    Yλ = Float64[]
+    Ys = Float64[]
+
+    if m > 0
+        Yp += Jgp'Xμ
+        Yμ = Jgp*Xp + Xs
+        Ys = Xμ + b*Xs ./ (s.*s)
+    end
+    if n > 0
+        Yp += Jhp'Xλ
+        Yλ = Jhp*Xp
+    end
+    
+    return ArrayPartition(Yp, Yμ, Yλ, Ys)
 end
 
-function default_stepsize(
+mutable struct InteriorPointState
+    L::InteriorPointLagrangian
+    stopping_criterion::StoppingCriterion
+    retraction_method::AbstractRetractionMethod
+    step_size::Stepsize
+end
+
+function InteriorPointState(L::InteriorPointLagrangian)
+    M = L.problem.M
+    p, μ, λ, s = L.params.q.x
+    m = length(μ)
+    n = length(λ)
+    N = M × ℝ^m × ℝ^n × ℝ^m
+    stopping_criterion = StopAfterIteration(100)
+    retraction_method = default_retraction_method(N)
+    step_size = ArmijoLinesearch(N; retraction_method=retraction_method, initial_stepsize=1.0)
+
+    return InteriorPointState(L, stopping_criterion, retraction_method, step_size)
+end
+    
+function is_feasible(
     M::AbstractManifold,
-    ::Type{InteriorPointNewtonState};
-    retraction_method=default_retraction_method(M),
+   ob::ConstrainedManifoldObjective,
+    p
 )
-    # take a default with a slightly defensive initial step size.
-    return ArmijoLinesearch(M; retraction_method=retraction_method, initial_stepsize=1.0)
-end
-
-function is_feasible(M::AbstractManifold, co::ConstrainedManifoldObjective, p)
-    # evaluate constraint functions at p
-    gp = get_inequality_constraints(M, co, p)
-    hp = get_equality_constraints(M, co, p)
+    # evaluateobnstraint functions at p
+    gp = get_inequality_constraints(M,ob, p)
+    hp = get_equality_constraints(M,ob, p)
 
     # check feasibility
     return all(gp .<= 0) && all(hp .== 0)
 end
 
-# function KKTVectorField(
-#     M::AbstractManifold,
-#     co::ConstrainedManifoldObjective,
-#     p,
-#     λ,
-#     μ,
-#     s
-# )
-#     gradL = LagrangianGrad(co, λ, μ)
-#     gp = get_inequality_constraints(M, p)
-#     hp = get_equality_constraints(M, p)
-#     diagM = Diagonal(μ)
-#     diagS = Diagonal(s)
-#     e = ones(length(hp))
-#     return [gradL, gp, hp+s, diagM*diagS*e]
-# end
+function step_solver(state::InteriorPointState)
 
-# function covariant_derivative_KKTVectorField(
-#     M::AbstractManifold,
-#     co::ConstrainedManifoldObjective,
-#     p,
-#     λ,
-#     μ,
-#     s
-# )
-#     L = LagrangianCost(co, λ, μ)
-#     gp = get_equality_constraints(M, p)
-#     hp = get_inequality_constraints(M, p)
-#     diagM = Diagonal(μ)
-#     diagS = Diagonal(s)
-#     I     = ones()
-#     e = ones(length(hp))
+    # get Lagrangian
+    L = state.L
 
-# function subsolver(
-#     M::AbstractManifold,
-#     co::ConstrainedManifoldObjective,
-#     ipns::InteriorPointNewtonState,
-# )
-#     F = KKTVectorField(M, co, ipns.p, ipns.λ, ipns.μ, ipns.s)
-#     # compute covariant derivative of kkt
-#     # solve for X: ∇F[X] = -F + σ_k ρ_k ̂e
-#     # return X
+    # get manifold
+    M = L.problem.M
 
-#     # The matrix representation of the covariant derivative
-#     # of the KKT vector field is given by
-#     #
-#     #       | ∇^2 L   J_g^T   J_h^T   0 |
-#     #  ∇F = |  J_g      0       0     0 |
-#     #       |  J_h      0       0     I |
-#     #       |   0       0       S     M |
-# end
+    # get current iterate
+    q = L.params.q
 
-# function step_solver(M::AbstractManifold, co::ConstrainedManifoldObjective, ipns::InteriorPointNewtonState, iteration)
+    # build product manifold
+    m, n = L.problem.dims
+    N = M × ℝ^m × ℝ^n × ℝ^m
 
-#     Δq = subsolver(M, co, ipns)
-#     γ = (1-γ_hat)*rand() + γ_hat
-#     α = min( 1, γ*min(...), γ*min(...) )
-#     q_next = R(α*Δq)
-#     β_next = β*rand()
-#     ipns
+    # random initial search direction
+    X = rand(N, vector_at = q) 
 
-#     return ipns
+    # get search direction
+    X = subsolver(Hess, X, .-grad(L))
 
-# Proto-RIPM(q_00, R, ̂γ, β_0)
-# 1 k ← 0
-# 2 while stopping criterion not satisfied
-# 3   Solve for ∆qk :
-#         ∇F(q_k)[∆q_k] = −F(q_k) + β_k ̂e
-# 4   Choose γ_k ∈ [̂γ, 1]
-# 5 α_k ← min { 1, γ_k min_i { -μ_k^i / Δμ_k^i : μ_k^i < 0 }, γ_k min_i { -s_k^i / Δs_k^i : s_k^i < 0 }}
-# 6 q_{k+1} ← ̄R_{q_k}(α_k Δq_k)
-# 7 Choose β_{k+1} ∈ (0, β_k)
-# 8 k ← k + 1
+    R = state.retraction_method
+    α = state.step_size
+
+    # Get next iterate
+    q = retract(N, q, X, R)
+
+    state.L.params.q = q
+end
+
+#obnjugate residudal method, 
+function subsolver(A, X_0, b)
+
+    k = 0
+    r_0 = b - A(L, X_0)
+    p_0 = r_0
+    Hr_0 = A(L, r_0)
+    Hp_0 = A(L, p_0)
+    
+    r = r_0
+    p = p_0
+    Hr = Hr_0
+    Hp = Hp_0
+    
+    X = X_0
+    
+    while inner(N, q, r, r) > 1e-5
+
+        α = inner(N, q, r, Hr) / inner(N, q, Hp, Hp)
+        X += α*p
+        r_next = r - α*Hp
+        Hr_next = A(L, r_next)
+        β = inner(N, q, r_next, Hr_next) / inner(N, q, r, Hr)
+        p_next = r_next + β*p
+        Hp_next = A(L, r_next) + β*Hp
+        r = r_next
+        p = p_next
+        Hr = Hr_next
+        Hp = Hp_next
+
+        k += 1
+    end
+    return X
+end
+
+function interior_point_Newton(M::AbstractManifold,ob::ConstrainedManifoldObjective)
+    p = rand(M)
+    m = length(get_inequality_constraints(M,ob, p))
+    n = length(get_equality_constraints(M,ob, p))
+    problem = InteriorPointProblem(M,ob, (m,n))
+    params = InteriorPointParams(problem)
+    L = InteriorPointLagrangian(problem, params)
+
+    # initial state
+    state = InteriorPointState(L)
+
+    for i in range(1, 10)
+        step_solver(state)
+    end
+
+    return state.L.params.q[N,1]
+end
+
+
+
--- test/solvers/test_interior_point_solver.jl
+++ test/solvers/test_interior_point_solver.jl
@@ -1,7 +1,50 @@
-using Manopt, Test
+using Manifolds, Manopt, LinearAlgebra, Random, Test
 
-@testset "IP Newton Tests" begin
-    @test 1 == 1
-    a = 1 / 2
-    @test a ≈ 0.5 atol = 1e-9
+# <<<<<<< Updated upstream
+# @testset "IP Newton Tests" begin
+#     @test 1 == 1
+#     a = 1 / 2
+#     @test a ≈ 0.5 atol = 1e-9
+# end
+# =======
+include("../../src/solvers/interior_point_Newton.jl")
+# include("../../src/plans/interior_point_Newton_plan.jl")
+
+
+A = Symmetric(rand(3,3))
+
+function f(M, p)
+    return 0.5 * p' * A * p
 end
+
+function grad_f(M, p)
+    return ( A - f(M, p)*I ) * p
+end
+
+function g(M, p)
+    return p - ones(3)
+end
+
+function grad_g(M, p)
+    return [1 0 0;
+            0 1 0;
+            0 0 1]
+end
+
+m = 3
+n = 0
+M = Sphere(2)
+N = M × ℝ^m × ℝ^n × ℝ^m
+q = rand(N)
+X = rand(N, vector_at = q)
+co = ConstrainedManifoldObjective(f, grad_f, g, grad_g)
+problem = InteriorPointProblem(M, co, (m,n))
+params = InteriorPointParams(problem)
+L = InteriorPointLagrangian(problem, params)
+state = InteriorPointState(L)
+
+# @testset "IP Newton Tests" begin
+#     @test 1==1
+#     a = 1/2
+#     @test a ≈ 0.5 atol=1e-9
+# end
