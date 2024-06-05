9aae78570b5f1e8cff45fe7b00180409887d788f
Ronny Bergmann <git@ronnybergmann.net>
2024-06-05 23:49:19 +0200

    trigger all ambiguity errors.

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -7,11 +7,11 @@ using Aqua, Manopt, Test
             exclude=[#For now exclude some high-level functions, since in their
                 # different call schemes some ambiguities appear
                 # We should carefully check these
-                Manopt.truncated_conjugate_gradient_descent, # ambiguity corresponds a problem with p and the Hessian and both being positional
-                Manopt.difference_of_convex_proximal_point, # should be fixed
-                Manopt.particle_swarm, # should be fixed
-                Manopt.stochastic_gradient_descent, # should be fixed
-                Manopt.truncated_conjugate_gradient_descent!, # will be fixed by removing deprecated methods
+                #Manopt.truncated_conjugate_gradient_descent, # ambiguity corresponds a problem with p and the Hessian and both being positional
+                #Manopt.difference_of_convex_proximal_point, # should be fixed
+                #Manopt.particle_swarm, # should be fixed
+                #Manopt.stochastic_gradient_descent, # should be fixed
+                #Manopt.truncated_conjugate_gradient_descent!, # will be fixed by removing deprecated methods
             ],
             broken=false,
         ),
