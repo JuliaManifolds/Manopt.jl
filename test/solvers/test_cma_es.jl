ade064a562e2e7038a3765e533a7a5647a0dbff3
Ronny Bergmann <git@ronnybergmann.net>
2024-06-12 13:44:38 +0200

    Update test/solvers/test_cma_es.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test/solvers/test_cma_es.jl
+++ test/solvers/test_cma_es.jl
@@ -138,6 +138,5 @@ flat_example(::AbstractManifold, p) = 0.0
         p1 = cma_es(M, griewank, [0.0, 1.0, 0.0]; σ=1.0, rng=MersenneTwister(123))
         @test griewank(M, p1) < 0.17
     end
-    @testset "Special Stopping Criteria" begin
-    end
+    @testset "Special Stopping Criteria" begin end
 end
