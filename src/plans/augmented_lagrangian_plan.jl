67056e82639b60ca775c8e9226818cb629683391
Ronny Bergmann <git@ronnybergmann.net>
2024-06-26 14:59:21 +0200

    Start ALM.

--- src/plans/augmented_lagrangian_plan.jl
+++ src/plans/augmented_lagrangian_plan.jl
@@ -1,12 +1,4 @@
-@doc raw"""
-    AugmentedLagrangianCost{CO,R,T}
-
-Stores the parameters ``ρ ∈ ℝ``, ``μ ∈ ℝ^m``, ``λ ∈ ℝ^n``
-of the augmented Lagrangian associated to the [`ConstrainedManifoldObjective`](@ref) `co`.
-
-This struct is also a functor `(M,p) -> v` that can be used as a cost function within a solver,
-based on the internal [`ConstrainedManifoldObjective`](@ref) it computes
-
+_doc_AL_Cost = raw"""
 ```math
 \mathcal L_\rho(p, μ, λ)
 = f(x) + \frac{ρ}{2} \biggl(
@@ -15,6 +7,17 @@ based on the internal [`ConstrainedManifoldObjective`](@ref) it computes
     \sum_{i=1}^m \max\Bigl\{ 0, \frac{μ_i}{ρ} + g_i(p) \Bigr\}^2
 \Bigr)
 ```
+"""
+@doc """
+    AugmentedLagrangianCost{CO,R,T}
+
+Stores the parameters ``ρ ∈ ℝ``, ``μ ∈ ℝ^m``, ``λ ∈ ℝ^n``
+of the augmented Lagrangian associated to the [`ConstrainedManifoldObjective`](@ref) `co`.
+
+This struct is also a functor `(M,p) -> v` that can be used as a cost function within a solver,
+based on the internal [`ConstrainedManifoldObjective`](@ref) it computes
+
+$_doc_AL_Cost
 
 ## Fields
 
@@ -55,7 +58,11 @@ function (L::AugmentedLagrangianCost)(M::AbstractManifold, p)
     return c + (L.ρ / 2) * d
 end
 
-@doc raw"""
+_doc_AL_Grad = raw"""
+
+"""
+
+@doc """
     AugmentedLagrangianGrad{CO,R,T}
 
 Stores the parameters ``ρ ∈ ℝ``, ``μ ∈ ℝ^m``, ``λ ∈ ℝ^n``
@@ -69,7 +76,7 @@ additionally this gradient does accept a positional last argument to specify the
 for the internal gradient call of the constrained objective.
 
 based on the internal [`ConstrainedManifoldObjective`](@ref) and computes the gradient
-``\operatorname{grad} \mathcal L_{ρ}(p, μ, λ)``, see also [`AugmentedLagrangianCost`](@ref).
+`$_l_grad $(_l_cal("L"))_{ρ}(p, μ, λ)``, see also [`AugmentedLagrangianCost`](@ref).
 
 ## Fields
 
--- src/solvers/augmented_Lagrangian_method.jl
+++ src/solvers/augmented_Lagrangian_method.jl
@@ -1,7 +1,7 @@
 #
 # State
 #
-@doc raw"""
+@doc """
     AugmentedLagrangianMethodState{P,T} <: AbstractManoptSolverState
 
 Describes the augmented Lagrangian method, with
@@ -10,31 +10,41 @@ Describes the augmented Lagrangian method, with
 
 a default value is given in brackets if a parameter can be left out in initialization.
 
-* `p`:                  a point on a manifold as starting point and current iterate
-* `sub_problem`:        an [`AbstractManoptProblem`](@ref) problem for the subsolver
-* `sub_state`:          an [`AbstractManoptSolverState`](@ref) for the subsolver
 * `ϵ`:                  (`1e–3`) the accuracy tolerance
 * `ϵ_min`:              (`1e-6`) the lower bound for the accuracy tolerance
 * `λ`:                  (`ones(n)`) the Lagrange multiplier with respect to the equality constraints
 * `λ_max`:              (`20.0`) an upper bound for the Lagrange multiplier belonging to the equality constraints
 * `λ_min`:              (`- λ_max`) a lower bound for the Lagrange multiplier belonging to the equality constraints
+* $_field_p
+* `penalty`:            evaluation of the current penalty term, initialized to `Inf`.
 * `μ`:                  (`ones(m)`) the Lagrange multiplier with respect to the inequality constraints
 * `μ_max`:              (`20.0`) an upper bound for the Lagrange multiplier belonging to the inequality constraints
 * `ρ`:                  (`1.0`) the penalty parameter
+* $_field_sub_problem
+* $_fieldsub_state
 * `τ`:                  (`0.8`) factor for the improvement of the evaluation of the penalty parameter
 * `θ_ρ`:                (`0.3`) the scaling factor of the penalty parameter
 * `θ_ϵ`:                ((`(ϵ_min/ϵ)^(ϵ_exponent)`) the scaling factor of the accuracy tolerance
-* `penalty`:            evaluation of the current penalty term, initialized to `Inf`.
-* `stopping_criterion`: (`(`[`StopAfterIteration`](@ref)`(300) | (`[`StopWhenSmallerOrEqual`](@ref)`(ϵ, ϵ_min) & `[`StopWhenChangeLess`](@ref)`(1e-10))`) a functor inheriting from [`StoppingCriterion`](@ref) indicating when to stop.
+* $_field_stop
 
 
 # Constructor
 
-    AugmentedLagrangianMethodState(M::AbstractManifold, co::ConstrainedManifoldObjective, p; kwargs...)
+    AugmentedLagrangianMethodState(
+        M::AbstractManifold,
+        co::ConstrainedManifoldObjective,
+        p,
+        sub_problem,
+        sub_state;
+        kwargs...
+    )
 
-construct an augmented Lagrangian method options with the fields and defaults as stated before,
-where the manifold `M` and the [`ConstrainedManifoldObjective`](@ref) `co` can be helpful for
+construct an augmented Lagrangian method options, where $(_arg_inline_M) and the [`ConstrainedManifoldObjective`](@ref) `co` are used for
 manifold- or objective specific defaults.
+
+## Keyword arguments
+
+
 
 # See also
 
