2e239afe25fdb21c24108ca7fdcf8780988d9a07
Laura Weigl <Laura.Weigl@uni-bayreuth.de>
2024-07-16 16:07:45 +0200

    write documentation and a bit of a future plan

--- src/solvers/vectorbundle_newton.jl
+++ src/solvers/vectorbundle_newton.jl
@@ -11,7 +11,25 @@ Is state for the vectorbundle Newton method
 * `retraction_method`:  the retraction to used
 * 'vector_transport_method': the vector transport to use
 * 'connection_map': connection map used in the Newton equation 
-* 
+
+# Constructor
+
+    VectorbundleNewtonState(M, E, F, connection_map, p=rand(M); kwargs...)
+
+# Input
+
+* 'M': domain manifold
+* 'E': range vector bundle
+* 'F': bundle map ``F:\mathcal M \to \mathcal E`` from Newton's method
+* 'connection_map': connection map to compute a generlalized covariant derivative of ``F``
+* 'p': initial point
+
+# Keyword arguments
+
+* X=zero_vector(M, p)
+* `retraction_method=``default_retraction_method`(M, typeof(p)),
+* `stopping_criterion=`[`StopAfterIteration`](@ref)`(1000)``,
+* `vector_transport_method=``default_vector_transport_method`(E, typeof(F(p)))
 
 """
 mutable struct VectorbundleNewtonState{
@@ -32,12 +50,14 @@ end
 
 function VectorbundleNewtonState(
     M::AbstractManifold,
-    p::P,
-    connection_map::F;
+    E::AbstractManifold,
+    F, #bundle_map
+    connection_map::F,
+    p::P;
     X::T=zero_vector(M, p),
     retraction_method::RM=default_retraction_method(M, typeof(p)),
     stopping_criterion::SC=StopAfterIteration(1000),
-    vector_transport_method::VTM=default_vector_transport_method(M, typeof(p)),
+    vector_transport_method::VTM=default_vector_transport_method(E, typeof(F(p))),
 ) where {
     P,
     T,
@@ -49,4 +69,47 @@ function VectorbundleNewtonState(
     return VectorbundleNewtonState{P,T,SC,RM,VTM,F}(
         p, X, stopping_criterion, retraction_method, vector_transport_method, connection_map
     )
+end
+
+@doc raw"""
+    VectorbundleObjective{T<:AbstractEvaluationType} <: AbstractManifoldObjective{T}
+
+specify an objective containing a cost and its gradient
+
+# Fields
+
+* `bundle_map!!`:       a mapping ``F: \mathcal M → \mathcal E`` into a vector bundle
+* `derivative!!`: the derivative ``F': T\mathcal M → T\mathcal E`` of the bundle map ``F``.
+
+# Constructors
+    VectorbundleObjective(bundle_map, derivative; evaluation=AllocatingEvaluation())
+
+"""
+struct VectorbundleObjective{T<:AbstractEvaluationType,C,G} <:
+       AbstractManifoldGradientObjective{T,C,G}
+    bundle_map!!::C
+    derivative!!::G
+end
+function VectorbundleObjective(
+    bundle_map::C, derivative::G; evaluation::E=AllocatingEvaluation()
+) where {C,G,E<:AbstractEvaluationType}
+    return VectorbundleObjective{E,C,G}(bundle_map, derivative)
+end
+
+# get_bundle_map, get_derivative similiar to get_gradient 
+
+# We need a VectorbundleProblem with domain M, codomain vector bundle E, and an objective
+
+function initialize_solver!(::AbstractManoptProblem, s::VectorbundleNewtonState)
+    return s
+end
+function step_solver!(mp::AbstractManoptProblem, s::VectorbundleNewtonState, k)
+    # compute Newton direction
+    E = get_manifold(mp) # vector bundle (codomain of F)
+    o = get_objective(mp)
+    # We need a representation of the equation system (use basis of tangent spaces or constraint representation of the tangent space -> augmented system)
+
+    # retract
+    retract!(get_manifold(mp), s.p, s.p, s.X, s.retraction_method)
+    return s
 end
