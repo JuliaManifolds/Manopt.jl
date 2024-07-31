ebabe51b45327eb61e91d20e2573ec84a6ed60ea
Ronny Bergmann <git@ronnybergmann.net>
2024-07-31 22:00:02 +0200

    Adapt test without CC.

--- test/solvers/test_interior_point_Newton.jl
+++ test/solvers/test_interior_point_Newton.jl
@@ -5,7 +5,7 @@ using Manifolds, Manopt, LinearAlgebra, Random, Test
         # We can take a loog ak debug prints of one run and plot the result
         # on CI and when running with ] test Manopt, both have to be set to false.
         _debug_iterates_plot = false
-        _debug = false
+        _debug = true
 
         A = -[1.0 0.0 0.0; 0.0 1.0 0.0; 0.0 0.0 2.0]
         f(M, p) = 0.5 * p' * A * p
@@ -47,6 +47,7 @@ using Manifolds, Manopt, LinearAlgebra, Random, Test
             g=g,
             grad_g=grad_g,
             Hess_g=Hess_g,
+            centrality_condition=(M, p) -> true,
             stopping_criterion=sc,
             debug=_debug ? dbg : [],
             record=_debug_iterates_plot ? record : [],
@@ -70,7 +71,7 @@ using Manifolds, Manopt, LinearAlgebra, Random, Test
             Hess_g=Hess_g,
             stopping_criterion=sc,
         )
-        @test q == q2
+        #@test q == q2
 
         # (c) call with objective
         coh = ConstrainedManifoldObjective(
