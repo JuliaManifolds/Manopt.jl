00c3efd6f9bfb91f7336d270a30ca0c9b14188d8
Mateusz Baran <mateuszbaran89@gmail.com>
2024-12-11 13:03:52 +0100

    fix extref  v3

--- src/plans/constrained_plan.jl
+++ src/plans/constrained_plan.jl
@@ -992,14 +992,14 @@ g_i(p) ≤ 0, \text{ for all } i=1,…,m\quad\text{ and }\quad h_j(p) = 0, \text
 ```
 
 # Keyword arguments
-* `check_point::Bool=true`: whether to also verify that ``p∈\mathcal M` holds, using [`is_point`](@extref ManifoldsBase :jl:method:`ManifoldsBase.is_point-Tuple{ValidationManifold, Any}`)
+* `check_point::Bool=true`: whether to also verify that ``p∈\mathcal M` holds, using [`is_point`](@extref ManifoldsBase :jl:method:`ManifoldsBase.is_point-Tuple{AbstractManifold, Any, Bool}`)
 * `error::Symbol=:none`: if the point is not feasible, this symbol determines how to report the error.
     * `:error`: throws an error
     * `:info`: displays the error message as an @info
     * `:none`: (default) the function just returns true/false
     * `:warn`: displays the error message as a @warning.
 
-The keyword `error=` and all other `kwargs...` are passed on to [`is_point`](@extref ManifoldsBase :jl:method:`ManifoldsBase.is_point-Tuple{ValidationManifold, Any}`)
+The keyword `error=` and all other `kwargs...` are passed on to [`is_point`](@extref ManifoldsBase :jl:method:`ManifoldsBase.is_point-Tuple{AbstractManifold, Any, Bool}`)
 if the point is verfied (see `check_point`).
 
 All other keywords are passed on to `is_poi`
