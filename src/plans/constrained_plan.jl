52473a17c7959365b7c77cbc5bd0766d2fb25c51
Mateusz Baran <mateuszbaran89@gmail.com>
2024-12-11 12:05:59 +0100

    fix extref?

--- src/plans/constrained_plan.jl
+++ src/plans/constrained_plan.jl
@@ -992,14 +992,14 @@ g_i(p) ≤ 0, \text{ for all } i=1,…,m\quad\text{ and }\quad h_j(p) = 0, \text
 ```
 
 # Keyword arguments
-* `check_point::Bool=true`: whether to also verify that ``p∈\mathcal M` holds, using [`is_point`](@extref ManifoldsBase.is_point)
+* `check_point::Bool=true`: whether to also verify that ``p∈\mathcal M` holds, using [`is_point`](@extref `ManifoldsBase.is_point`)
 * `error::Symbol=:none`: if the point is not feasible, this symbol determines how to report the error.
     * `:error`: throws an error
     * `:info`: displays the error message as an @info
     * `:none`: (default) the function just returns true/false
     * `:warn`: displays the error message as a @warning.
 
-The keyword `error=` and all other `kwargs...` are passed on to [`is_point`](@extref ManifoldsBase.is_point) if the point is verfied (see `check_point`).
+The keyword `error=` and all other `kwargs...` are passed on to [`is_point`](@extref `ManifoldsBase.is_point`) if the point is verfied (see `check_point`).
 
 All other keywords are passed on to `is_poi`
 """
