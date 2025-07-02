e546b53eb6495d5e1700bdb24e8cda0d5bd75db7
Laura Weigl <Laura.Weigl@uni-bayreuth.de>
2025-07-02 17:19:14 +0200

    improved documentation

--- temp/geodesic_obstacle_new.jl
+++ temp/geodesic_obstacle_new.jl
@@ -17,24 +17,67 @@ begin
 	using Manifolds
 	using Random
     using WGLMakie, Makie, GeometryTypes, Colors
-	#using CairoMakie
-	#using FileIO
 end;
+
+# ╔═╡ aa62a032-baec-40ab-918a-971dace0d844
+md"""
+In this example we compute a geodesic on the sphere avoiding an obstacle by applying Newton's method on vector bundles which was introduced in \ref{paper}. This example reproduces the results from \ref{paper}.
+"""
+
+# ╔═╡ 0a68dba2-3b3f-4ad4-bf4a-bdc37bc28d43
+md"""
+We consider the sphere $$\mathbb{S}^2$$ equipped with the Riemannian metric $\langle \cdot, \cdot \rangle$ given by the Euclidean inner product with corresponding norm $\|\cdot\|$ and a time interval $\Omega =[0,T]$.
+
+Let $\mathcal X = H^1(\Omega, \mathbb S^2)$ and  $\mathcal E^* = T^*\mathcal X$ its cotangent bundle.
+
+Consider the minimization problem
+
+$\min_{\gamma \in H^1(\Omega, \mathbb S^2)} \; \frac12 \int_0^T \|\dot \gamma(t)\|^2 \; dt$
+under the constraint that $\gamma_3(t) \leq 1-h_{\mathrm{ref}} \; \text{ for all } t\in [0,T]$ where $\gamma_3(t)$ denotes the third component of $\gamma(t)\in \mathbb{S}^2$ and $h_{\mathrm{ref}} \in (0,1)$ is a given height. Additionally, we have to take into account that boundary conditions $\gamma(0) = \gamma_0$ and $\gamma(T) = \gamma_T$ for given $\gamma_0, \gamma_T \in \mathbb S^2$ are satisfied.
+
+Using a penalty method, also known as Moreau-Yosida regularization (cf. \cite{HintermuellerKunisch:2006:1}), with a quadratic penalty term we can rewrite this as an unconstrained minimization problem with a penalty coefficient $p\in \mathbb R$:
+
+$\min_{\gamma \in H^1(\Omega, \mathbb S^2)} \; \frac12 \int_0^T \|\dot \gamma(t)\|^2 + p \max(0, \gamma_3(t) - 1 + h_{\mathrm{ref}})^2 \; dt$
+
+Let $m \colon\mathbb R \to \mathbb R, \, m(x) \coloneqq \max(0, x)$. 
+The objective is a semismooth function $f : \mathcal X \to \mathbb R$ with a Newton-derivative 
+
+$f'(\gamma)\delta \gamma = \int_0^T \langle \dot\gamma(t), \dot{\delta \gamma}(t)\rangle + p \cdot m(\gamma_3(t) - 1 + h_{\mathrm{ref}})\delta \gamma_3(t)$
+
+for $\delta \gamma \in T_\gamma \mathcal X$. 
+Our goal is to find a zero of this Newton-derivative. 
+
+This yields a geodesic avoiding the north pol cap and connecting $\gamma_0$ and $\gamma_T$.
+"""
+
+# ╔═╡ 00fe7ab7-3cb7-455c-872d-336770503c02
+md"""
+For our example we set
+"""
 
 # ╔═╡ 12e32b83-ae65-406c-be51-3f21935eaae5
 begin
 	N=200
+	
+	S = Manifolds.Sphere(2)
+	power = PowerManifold(S, NestedPowerRepresentation(), N) # power manifold of S
+	
 	st = -pi/2 + 0.5
 	halt = pi/2 - 0.5
 	h = (halt-st)/(N+1)
-	#halt = pi - st
-	Omega = range(; start=st, stop = halt, length=N+2)[2:end-1]
+	Omega = range(; start=st, stop = halt, length=N+2)[2:end-1] # equidistant discrete time points
+	
 	theta = pi/4
 	y0 = [sin(theta)*cos(st),sin(theta)*sin(st),cos(theta)] # startpoint of geodesic
 	yT = [sin(theta)*cos(halt),sin(theta)*sin(halt),cos(theta)] # endpoint of geodesic
 
 	h_ref = 0.04050702638550263
 end;
+
+# ╔═╡ b8d10b0f-cbe1-4b05-a5f3-00d9906429e0
+md"""
+As a starting point, we use the geodesic connecting $\gamma_0$ and $\gamma_T$:
+"""
 
 # ╔═╡ 29043ca3-afe0-4280-a76a-7c160a117fdf
 function y(t)
@@ -44,12 +87,29 @@ end;
 # ╔═╡ 5c0980c5-284e-4406-bab8-9b9aff9391ba
 discretized_y = [y(Ωi) for Ωi in Omega];
 
-# ╔═╡ bc449c2d-1f23-4c72-86ab-a46acbf64129
+# ╔═╡ 81c14a72-309c-470e-8c8c-bcc7dd843e43
+md"""
+In order to apply Newton's method to find a zero of $F$, we need the linear mapping $Q_{F(\gamma)}^*\circ F'(\gamma)$ (cf. \ref{paper}) which can be seen as a covariant derivative. Since the sphere is an embedded submanifold of $\mathbb R^3$, we can use the formular 
+
+$Q_{f'(\gamma)}^*\circ f^{\prime\prime}(\gamma)\delta \gamma\,\phi = f'(\gamma)(\overset{\rightarrow}{V}_\gamma'(\gamma)\delta \gamma\,\phi) + f^{\prime\prime}_{\mathbb R^3}(\gamma)\delta \gamma\,\phi$
+
+for $\delta \gamma, \, \phi \in T_\gamma \mathcal X$, where $\overset{\rightarrow}{V}_\gamma(\hat \gamma) \in L(T_\gamma \mathcal X, T_{\hat{\gamma}}\mathcal X)$ is a vector transport and 
+
+$f_{\mathbb R^3}^{\prime\prime}(\gamma)\delta \gamma\, \phi = \int_0^T \langle \dot{\delta \gamma}(t), \dot{\phi}(t)\rangle + p \cdot m'(\gamma_3(t) - 1 + h_{\mathrm{ref}})\phi_{3}(t) \delta \gamma_{3}(t)$
+
+is the euclidean second derivative of the objective.
+
+
 """
-Such a structure has to be filled for two purposes:
+
+# ╔═╡ c61ac584-e3be-47c8-8801-684141d9e1f9
+md"""
+We define a structure that has to be filled for two purposes:
 * Definition of an integrand and its derivative
 * Definition of a vector transport and its derivative
 """
+
+# ╔═╡ bc449c2d-1f23-4c72-86ab-a46acbf64129
 mutable struct DifferentiableMapping{M<:AbstractManifold,F1<:Function,F2<:Function,T}
 	domain::M
 	value::F1
@@ -59,27 +119,46 @@ mutable struct DifferentiableMapping{M<:AbstractManifold,F1<:Function,F2<:Functi
 end
 
 
+# ╔═╡ e7f10756-4ecd-4a43-a560-87676f26914a
+md"""
+The following routines define a vector transport and its euclidean derivative. As seen above, they are needed to derive a covariant derivative of $F$.
+
+As a vector transport we use the (pointwise) orthogonal projection onto the tangent spaces, i.e. for $p, q \in \mathbb S^2$ and $X \in T_p\mathbb S^2$ we set 
+
+$\overset{\rightarrow}{V}_{p}(q)X = (I-q\cdot q^T)X \in T_q\mathbb S^2.$
+
+The derivative of the vector transport is then given by 
+
+$\left(\frac{d}{dq}\overset{\rightarrow}{V}_{p}(q)\big\vert_{q=p}\delta q\right)X = \left( - \delta q\cdot p^T - p\cdot \delta q^T\right)\cdot X.$
+
+"""
+
 # ╔═╡ 50a51e47-b6b1-4e43-b4b9-aad23f6ec390
-"""
- The following two routines define the vector transport and its derivative. The second is needed to obtain covariant derivative from the ordinary derivative.
-"""
-function transport_by_proj(S, p, X, q)
-	return X - q*(q'*X)
-end
-
-# ╔═╡ 9cdd4289-c49d-4733-8487-f471e38fc402
-function transport_by_proj_prime(S, p, X, dq)
-	return (- dq*p' - p*dq')*X
-end
-
-# ╔═╡ a9b96a80-b19b-4b1a-972e-54dd3ed6c9c8
-function max_prime(y)
-	if y[3] < 1.0 - h_ref
-		return 0.0
-	else
-		return 1.0
+begin 
+	
+	function transport_by_proj(S, p, X, q)
+		return X - q*(q'*X)
 	end
-end
+
+	function transport_by_proj_prime(S, p, X, dq)
+		return (- dq*p' - p*dq')*X
+	end
+
+	transport=DifferentiableMapping(S,transport_by_proj,transport_by_proj_prime,nothing, nothing)
+end;
+
+# ╔═╡ fde5a441-9ff5-45f9-ad00-57078a16dba8
+md"""
+The following two routines define the integrand of a Newton-derivative of the objective and its euclidean second derivative. Here, a Newton-derivative of the maximum function given by 
+
+$m'(x) \coloneqq \begin{cases}
+        0 &: \; x<0 \\
+        \text{arbitrary} &: \; x= 0\\
+        1 &: \; x>0
+    \end{cases}$
+
+is used. A scaling parameter for the penalty parameter is also employed.
+"""
 
 # ╔═╡ 808db8aa-64f7-4b36-8c6c-929ba4fa22db
 """
@@ -91,14 +170,6 @@ function w(p, c)
 		return c*[1.0, 0.0, 0.0]
 end
 
-# ╔═╡ 56ae7f53-061e-4414-90ad-85c7a12d51e2
-"""
-The following two routines define the integrand and its ordinary derivative. They use a vector field w, which is defined, below. A scaling parameter is also employed.
-"""
-function F_at(Integrand, y, ydot, B, Bdot)
-	  return ydot'*Bdot+w(y,Integrand.scaling_force)'*B + Integrand.scaling_penalty * max(0.0, y[3] - 1.0 + h_ref)*B[3]
-end
-
 # ╔═╡ 288b9637-0500-40b8-a1f9-90cb9591402b
 function w_prime(p, c)
 	#nenner = p[1]^2+p[2]^2
@@ -107,18 +178,27 @@ function w_prime(p, c)
 	return [0.0 0.0 0.0; 0.0 0.0 0.0; 0.0 0.0 0.0]
 end
 
-# ╔═╡ ac04e6ec-61c2-475f-bb2f-83755c04bd72
-function F_prime_at(Integrand,y,ydot,B1,B1dot,B2,B2dot)
-	return B1dot'*B2dot+(w_prime(y,Integrand.scaling_force)*B1)'*B2 + Integrand.scaling_penalty*max_prime(y)*B1[3]*B2[3]
-end
-
-# ╔═╡ 684508bd-4525-418b-b89a-85d56c01b188
+# ╔═╡ 56ae7f53-061e-4414-90ad-85c7a12d51e2
 begin
-S = Manifolds.Sphere(2)
-power = PowerManifold(S, NestedPowerRepresentation(), N);
-integrand=DifferentiableMapping(S,F_at,F_prime_at,0.0,1.0)
-transport=DifferentiableMapping(S,transport_by_proj,transport_by_proj_prime,nothing, nothing)
-end;
+	
+	function F_at(Integrand, y, ydot, B, Bdot)
+	  	return ydot'*Bdot+w(y,Integrand.scaling_force)'*B + Integrand.scaling_penalty * max(0.0, y[3] - 1.0 + h_ref)*B[3]
+	end
+
+	function max_prime(y)
+		if y[3] < 1.0 - h_ref
+			return 0.0
+		else
+			return 1.0
+		end
+	end
+
+	function F_prime_at(Integrand,y,ydot,B1,B1dot,B2,B2dot)
+		return B1dot'*B2dot+(w_prime(y,Integrand.scaling_force)*B1)'*B2 + Integrand.scaling_penalty*max_prime(y)*B1[3]*B2[3]
+	end
+
+	integrand=DifferentiableMapping(S,F_at,F_prime_at,0.0,1.0)
+end
 
 # ╔═╡ eb3cb1db-229a-44c0-8591-90142cbb0885
 """
@@ -133,6 +213,21 @@ function w_doubleprime(p, v, c)
 	w3 = 1/(nenner^2)*[2*p[1]*p[2] -p[1]^2+p[2]^2 0.0; p[2]^2-p[1]^2 -2*p[1]*p[2] 0.0; 0.0 0.0 0.0]
 	return c*(v[1]*w1 + v[2]*w2 + v[3]*w3)
 end
+
+# ╔═╡ eda0a587-a19d-4f80-80bf-c4cc5a21854c
+md"""
+`NewtonEquation`
+
+In this example we implement a functor to compute the Newton matrix and the right hand side for the Newton equation \ref{paper}
+
+$$Q^*_{F(\gamma)}\circ F'(\gamma)\delta \gamma + F(\gamma) = 0$$
+
+by using the assembler provided in ManoptExamples.jl (cf. Referenz).
+	
+It returns the matrix and the right hand side in base representation.
+Moreover, for the computation of the simplified Newton direction (which is necessary for affine covariant damping) a method for assembling the right hand side for the simplified Newton equation is provided.
+	
+"""
 
 # ╔═╡ 1adf467b-81e8-4438-98ce-4420ad1f5bda
 begin
@@ -177,15 +272,22 @@ function (ne::NewtonEquation)(M, VB, p, p_trial)
 end
 end;
 
+# ╔═╡ f7ba0044-ecfe-4714-9ec3-b4792d38b21c
+md"""
+We compute the Newton direction $\delta \gamma$ by solving the linear system given by the base representation of the Newton equation directly and return the Newton direction in vector representation:
+"""
+
 # ╔═╡ 910e85fe-2db4-43f3-8cf9-a805858f3627
-"""
-	Computes the Newton direction by solving the linear system given by the base representation of the Newton equation directly and returns the Newton direction in vector representation
-"""
 function solve_in_basis_repr(problem, newtonstate) 
 	Xc = (problem.newton_equation.A) \ (-problem.newton_equation.b)
 	res_c = get_vector(problem.manifold, newtonstate.p, Xc, DefaultOrthogonalBasis())
 	return res_c
 end
+
+# ╔═╡ 1798ca75-1c1a-4760-af68-87bbd60544b4
+md"""
+For the computation of a solution of the penalized problem we use a simple path-following method increasing the penalty parameter by a factor 1.2 in each iteration.
+"""
 
 # ╔═╡ 4c26b3d0-51ed-48b1-9efe-1a4ba0949e04
 begin
@@ -232,6 +334,11 @@ begin
  
 end
 
+# ╔═╡ faaefaa7-198d-4d05-9393-f37f48d404d6
+md"""
+We extract the recorded values
+"""
+
 # ╔═╡ ac510e7a-5d46-4c22-89aa-1e6310e076a0
 change = get_record(st_res, :Iteration, :Change)[2:end];
 
@@ -245,6 +352,11 @@ begin
     scatterlines!(change[1:end], color = :blue)
 	f
 end
+
+# ╔═╡ da4fca13-23d3-4f4a-bc35-ace2b5dacaf8
+md"""
+This yields the geodesic shown below avoiding the north pole cap and connecting two almost antipodal points $\gamma_0$ and $\gamma_T$.
+"""
 
 # ╔═╡ 6f6eb0f9-21af-481a-a2ae-020a0ff305bf
 begin
@@ -279,7 +391,7 @@ wireframe!(ax, sx, sy, sz, color = RGBA(0.5,0.5,0.7,0.1); transparency=true)
 
 	scatterlines!(ax, sx[:,5], sy[:,5], sz[:,5]; markersize =2, color=:black, linewidth=2)
 
-	println(1-cos(v[5])) #das ist mein h_ref
+	println(1-cos(v[5])) #this is h_ref
 	
 	scatterlines!(ax, π1.(y_0), π2.(y_0), π3.(y_0); markersize =8, color=:orange, linewidth=2)
 	
@@ -295,23 +407,32 @@ end
 
 # ╔═╡ Cell order:
 # ╠═c9994bc4-b7bb-11ef-3430-8976c5eabdeb
+# ╟─aa62a032-baec-40ab-918a-971dace0d844
 # ╠═9fb54416-3909-49c4-b1bf-cc868e580652
+# ╟─0a68dba2-3b3f-4ad4-bf4a-bdc37bc28d43
+# ╟─00fe7ab7-3cb7-455c-872d-336770503c02
 # ╠═12e32b83-ae65-406c-be51-3f21935eaae5
+# ╟─b8d10b0f-cbe1-4b05-a5f3-00d9906429e0
 # ╠═29043ca3-afe0-4280-a76a-7c160a117fdf
 # ╠═5c0980c5-284e-4406-bab8-9b9aff9391ba
+# ╟─81c14a72-309c-470e-8c8c-bcc7dd843e43
+# ╟─c61ac584-e3be-47c8-8801-684141d9e1f9
 # ╠═bc449c2d-1f23-4c72-86ab-a46acbf64129
+# ╟─e7f10756-4ecd-4a43-a560-87676f26914a
 # ╠═50a51e47-b6b1-4e43-b4b9-aad23f6ec390
-# ╠═9cdd4289-c49d-4733-8487-f471e38fc402
+# ╟─fde5a441-9ff5-45f9-ad00-57078a16dba8
 # ╠═56ae7f53-061e-4414-90ad-85c7a12d51e2
-# ╠═a9b96a80-b19b-4b1a-972e-54dd3ed6c9c8
-# ╠═ac04e6ec-61c2-475f-bb2f-83755c04bd72
-# ╠═684508bd-4525-418b-b89a-85d56c01b188
 # ╠═808db8aa-64f7-4b36-8c6c-929ba4fa22db
 # ╠═288b9637-0500-40b8-a1f9-90cb9591402b
 # ╠═eb3cb1db-229a-44c0-8591-90142cbb0885
+# ╟─eda0a587-a19d-4f80-80bf-c4cc5a21854c
 # ╠═1adf467b-81e8-4438-98ce-4420ad1f5bda
+# ╠═f7ba0044-ecfe-4714-9ec3-b4792d38b21c
 # ╠═910e85fe-2db4-43f3-8cf9-a805858f3627
+# ╟─1798ca75-1c1a-4760-af68-87bbd60544b4
 # ╠═4c26b3d0-51ed-48b1-9efe-1a4ba0949e04
+# ╟─faaefaa7-198d-4d05-9393-f37f48d404d6
 # ╠═ac510e7a-5d46-4c22-89aa-1e6310e076a0
 # ╠═a08b8946-5adb-43c7-98aa-113875c954b1
+# ╟─da4fca13-23d3-4f4a-bc35-ace2b5dacaf8
 # ╠═6f6eb0f9-21af-481a-a2ae-020a0ff305bf
