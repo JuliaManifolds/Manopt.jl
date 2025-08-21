e9534d9154c9342c64276d082653dafe814fa570
Ronny Bergmann <git@ronnybergmann.net>
2025-08-21 16:08:01 +0200

    First few steps towards handling Keywords.

---
+++ src/plans/keywords.jl
@@ -0,0 +1,81 @@
+"""
+    Keywords
+
+A small internal struct to represent a set of keywords,
+
+# Fields
+* `accepted`
+* `deprecated`
+
+# Constructor
+
+    Keywords(accepted=Set{Symbol}(), deprecated=Set{Symbol}(); type::Type=Nothing)
+
+Generate a Keywords wrapper, where both default to being the empty set.
+For pretty printing you can provide a type they belong to.
+
+    Keywords(nothing; type=Nothing)
+
+A constructor to indicate that a certain function or element of type `T` is
+not yet set up to work in this scenario.
+"""
+struct Keywords{I,A<:Union{Nothing,Set{Symbol}},D<:Union{Nothing,Set{Symbol}}}
+    accepted::A
+    deprecated::D
+end
+function Keywords(
+    accepted::A,
+    deprecated::D=Set{Symbol}();
+    in=nothing
+) where {A<:Union{Nothing,Set{Symbol}},D<:Union{Nothing,Set{Symbol}}}
+    return Keywords{in,A,D}(accepted, deprecated)
+end
+Keywords(::Nothing=nothing; in=Nothing) = Keywords{in,Nothing,Nothing}(nothing,nothing)
+
+function Base.show(io::IO, ::Keywords{Nothing})
+    print(io, "Keywords()")
+end
+
+function Base.show(io::IO, keywords::Keywords{I})
+    print(io, """
+    Keywords for $I:
+
+    accepted: $(keywords.accepted)
+    deprecated: $(keywords.deprecated)
+    """
+    )
+end
+
+"""
+    accepted_keywords(problem)
+    accepted_keywords(objective)
+    accepted_keywords(solver)
+    accepted_keywords(stepsize)
+
+Return a set of keywords, see [`Keywords`](@ref), a certain element of `Manopt.jl`
+accepts when constructed.
+
+this also includes keywords that are passed on to internal structures.
+"""
+accepted_keywords(::Union{AbstractManoptProblem, AbstractManifoldObjective, AbstractManoptSolverState, Stepsize, StoppingCriterion, Function})
+
+# default: all direct ones
+accepted_keywords(T) = direct_keywords(T)
+
+function direct_keywords end
+"""
+    direct_keywords(problem)
+    direct_keywords(objective)
+    direct_keywords(solver)
+    direct_keywords(stepsize)
+
+Return a set of keywords a function directly would work with,
+i.e. that are directly defined in one of its dispatch variants, including
+a mutating variant.
+
+For example `direct_keywords(gradient_descent)` and `direct_keywords(gradient_descent!)`
+should return the same set [`Keywords!`](@ref).
+"""
+direct_keywords(::Union{AbstractManoptProblem, AbstractManifoldObjective, AbstractManoptSolverState, Stepsize,  StoppingCriterion, Function})
+
+direct_keywords(T::Union{AbstractManoptProblem, AbstractManifoldObjective, AbstractManoptSolverState, Stepsize, StoppingCriterion, Function}) = Keywords(; type=T)
