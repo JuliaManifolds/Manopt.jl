cf0fad2f1a24a9a990532f211162fe4e98fc3703
Laura Weigl <Laura.Weigl@uni-bayreuth.de>
2025-08-27 16:30:38 +0200

    Routinen aus ManoptExamples eingebaut und Dämpfung repariert

--- temp/optimal_control_geodesics.jl
+++ temp/optimal_control_geodesics.jl
@@ -21,9 +21,6 @@ begin
 	using DataFrames, CSV
 end;
 
-# ╔═╡ 66fbe00e-1d30-468d-baa5-05e099cf329e
-#Pkg.develop(path="/Users/bt308990/Documents/julia/Manopt-old.jl")
-
 # ╔═╡ aeceb735-da1b-4db5-9964-538b316441c2
 begin
 	N=25
@@ -294,211 +291,6 @@ integrand_state_eq = DifferentiableMapping(S,S,state_equation_at, state_equation
 	
 end;
 
-# ╔═╡ 9cad614b-f5b1-43ac-a612-6f32458f7cdd
-"""
- A:      Matrix to be written into\\
-row_idx: row index of block inside system\\
-col_idx: column index of block inside system\\
-
-h:       length of interval\\
-i:       index of interval\\
-
-yl:      left value of iterate\\
-yr:      right value of iterate\\
-
-B:       basis vector for basis function\\
-bfl:     0/1 scaling factor at left boundary\\
-bfr:     0/1 scaling factor at right boundary \\
-
-T:       basis vector for test function\\
-tfl:     0/1 scaling factor at left boundary\\
-tfr:     0/1 scaling factor at right boundary \\
-...
-"""
-function assemble_local_Jac_with_connection!(A,row_idx, col_idx, h, i, yl, yr, B, bfl, bfr, T, tfl, tfr, integrand, transport)
- dim = manifold_dimension(integrand.domain)
- dimc = manifold_dimension(integrand.precodomain)
-if tfr == 1
-	idxc=dimc*(i-1)
-else 
-	idxc=dimc*(i-2)
-end
-if bfr == 1
-	idx=dim*(i-1)
-else 
-	idx=dim*(i-2)
-end
-
- ydot=(yr-yl)/h
- quadwght=0.5*h
- nA1=size(A,1)
- nA2=size(A,2)
- #	Schleife über Komponenten der Testfunktion
- for k in 1:dimc
-    # Schleife über Komponenten der Basisfunktion
-	for j in 1:dim
-		# Sicherstellen, dass wir in Indexgrenzen der Matrix bleiben
-        if idx+j >= 1 && idxc+k >= 1 && idx+j <= nA2 && idxc+k <= nA1
-
-		# Zeit-Ableitungen der Basis- und Testfunktionen (=0 am jeweils anderen Rand)
-     	Tdot=(tfr-tfl)*T[k]/h
-		Bdot=(bfr-bfl)*B[j]/h
-
-
-    	# Ableitung in der Einbettung am rechten und linken Quadraturpunkt
-        tmp=integrand.derivative(integrand,yl,ydot,bfl*B[j],Bdot,tfl*T[k],Tdot)	
-			
-		tmp+=integrand.derivative(integrand,yr,ydot,bfr*B[j],Bdot,tfr*T[k],Tdot)	
-			
-		# Modifikation für Kovariante Ableitung:	
-		# y-Ableitungen der Projektionen am linken Punkt
-		# P'(yl)bfl*B[j] (tfl*T(k))
-		Pprimel=transport.derivative(integrand.domain,yl.x[row_idx],bfl*B[j],tfl*T[k])
-		Pprimer=transport.derivative(integrand.domain,yr.x[row_idx],bfr*B[j],tfr*T[k])
-			
-		# Zeit- und y-Ableitungen der Projektionen
-		Pprimedotl=(bfr-bfl)*Pprimel/h
-		Pprimedotr=(bfr-bfl)*Pprimer/h	
-
-		Pprimedot_neu = (Pprimer - Pprimel)/h
-			
-		# Einsetzen in die rechte Seite am rechten und linken Quadraturpunkt
-		tmp+=integrand.value(integrand,yl,ydot,bfl*Pprimel,Pprimedot_neu)
-		#tmp+=integrand.value(integrand,yr,ydot,bfr*Pprimel,Pprimedot_neu)
-			
-		# y-Ableitungen der Projektionen am rechten Punkt
-		
-		#tmp+=integrand.value(integrand,yl,ydot,bfl*Pprimer,Pprimedot_neu)
-		tmp+=integrand.value(integrand,yr,ydot,bfr*Pprimer,Pprimedot_neu)
-			
-        # Update des Matrixeintrags
-			
-		A[idxc+k,idx+j]+=quadwght*tmp
-		end
-	end
- end
-end
-
-# ╔═╡ ea918bcc-2d49-47cf-ad9c-79ae3bc9484c
-"""
-If no vector transport is needed, leave it away, then a zero dummy transport is used
-"""
-function get_Jac!(A,row_idx,col_idx,h,nCells,y,integrand)
-	get_Jac!(A,row_idx,col_idx,h,nCells,y,integrand,zerotransport)
-end
-
-# ╔═╡ f7d66dfb-d246-486a-9033-ab82fde3d586
-"""
-  T          : Test function
-  tlf        : 1/0 : scaling of test function at left interval point
-  trf        : 1/0 : scaling of test function at right interval point
-"""
-function assemble_local_rhs!(b,row_idx, h, i, yl, yr, T, tlf, trf, integrand)
-    dimc = manifold_dimension(integrand.precodomain)
-	if trf == 1
-     idx=dimc*(i-1)
-	else 
-	 idx=dimc*(i-2)
-	end
-	ydotl=(yr-yl)/h
-	ydotr=(yr-yl)/h
-	# Trapezregel
-	quadwght = 0.5*h   
-	for k in 1:dimc
-		# finite differences, taking into account values of test function at both endpoints
-        if idx+k > 0 && idx+k <= length(b)
-			
-			Tdot = (trf-tlf)*T[k]/h
-			#linker Quadraturpunkt
-			tmp =  integrand.value(integrand,yl,ydotl,tlf*T[k],Tdot)	
-			#rechter Quadraturpunkt
-			tmp += integrand.value(integrand,yr,ydotr,trf*T[k],Tdot)	
-			# Update der rechten Seite
-		  	b[idx+k]+= quadwght*tmp	
-		end
-	end
-end
-
-# ╔═╡ 6cf5ad9a-b3dc-4a11-bf01-3aa188c9667b
-"""
- A:      Matrix to be written into\\
-row_idx: row index of block inside system\\
-col_idx: column index of block inside system\\
-
-h:       length of interval\\
-i:       index of interval\\
-
-yl:      left value of iterate\\
-yr:      right value of iterate\\
-
-B:       basis vector for basis function\\
-bfl:     0/1 scaling factor at left boundary\\
-bfr:     0/1 scaling factor at right boundary \\
-
-T:       basis vector for test function\\
-tfl:     0/1 scaling factor at left boundary\\
-tfr:     0/1 scaling factor at right boundary \\
-...
-"""
-function assemble_local_Jac_Lyy!(A,row_idx, col_idx, h, i, yl, yr, B, bfl, bfr, T, tfl, tfr, integrand, transport)
- dim = manifold_dimension(integrand.domain)
- dimc = manifold_dimension(integrand.precodomain)
-if tfr == 1
-	idxc=dimc*(i-1)
-else 
-	idxc=dimc*(i-2)
-end
-if bfr == 1
-	idx=dim*(i-1)
-else 
-	idx=dim*(i-2)
-end
-
- ydot=(yr-yl)/h
- quadwght=0.5*h
- nA1=size(A,1)
- nA2=size(A,2)
- #	Schleife über Komponenten der Testfunktion
- for k in 1:dimc
-    # Schleife über Komponenten der Basisfunktion
-	for j in 1:dim
-		# Sicherstellen, dass wir in Indexgrenzen der Matrix bleiben
-        if idx+j >= 1 && idxc+k >= 1 && idx+j <= nA2 && idxc+k <= nA1
-
-		# Zeit-Ableitungen der Basis- und Testfunktionen (=0 am jeweils anderen Rand)
-     	Tdot=(tfr-tfl)*T[k]/h
-		Bdot=(bfr-bfl)*B[j]/h
-			
-		# Modifikation für Kovariante Ableitung:	
-		# y-Ableitungen der Projektionen am linken Punkt
-		# P'(yl)bfl*B[j] (tfl*T(k))
-
-		Pprimel=transport.derivative(integrand.domain,yl,bfl*B[j],tfl*T[k])
-		Pprimer=transport.derivative(integrand.domain,yr,bfr*B[j],tfr*T[k])
-			
-		# Zeit- und y-Ableitungen der Projektionen
-		Pprimedotl=(bfr-bfl)*Pprimel/h
-		Pprimedotr=(bfr-bfl)*Pprimer/h	
-
-		Pprimedot_neu = (Pprimer - Pprimel)/h
-			
-		# Einsetzen in die rechte Seite am rechten und linken Quadraturpunkt
-
-		tmp = integrand.derivative(integrand,yl,ydot,bfl*B[j],Bdot,bfl*Pprimel,Pprimedot_neu)
-		#tmp+=integrand.derivative(integrand,yr,ydot,bfr*B[j],Bdot,bfr*Pprimel,Pprimedot_neu)
-			
-		
-        # Update des Matrixeintrags
-
-		#tmp+=integrand.derivative(integrand,yl,ydot,bfl*B[j], Bdot,bfl*Pprimer,Pprimedot_neu)
-		tmp+=integrand.derivative(integrand,yr,ydot,bfr*B[j],Bdot,bfr*Pprimer,Pprimedot_neu)
-			
-		A[idxc+k,idx+j]+=quadwght*tmp
-		end
-	end
- end
-end
-
 # ╔═╡ 707d23d1-f7b4-49cb-b0ff-11ec536939fa
 function evaluate(y, i, tloc)
 	return ArrayPartition(
@@ -506,155 +298,6 @@ function evaluate(y, i, tloc)
 		(1.0-tloc)*y.x[2][i-1]+tloc*y.x[2][i],
 		(1.0-tloc)*y.x[3][i-1]+tloc*y.x[3][i]
 	)
-end
-
-# ╔═╡ c9b6a526-7b30-44bf-b652-ff9c17a4599c
-"""
- A:      Matrix to be written into\\
-row_idx: row index of block inside system\\
-detT:    degree of test function: 1: linear, 0: constant\\
-col_idx: column index of block inside system\\
-detB:    degree of basis function: 1: linear, 0: constant\\
-h:       length of interval\\
-nCell:    total number of intervals\\
-y:       iterate\\
-...
-"""
-function get_Jac!(A,row_idx,col_idx,h, nCells,y,integrand,transport)
-	M = integrand.domain
-	N = integrand.precodomain
-	# Schleife über Intervalle
-	for i in 1:nCells
-
-		# Evaluation of the current iterate. This routine has to be provided from outside, because Knowledge about the basis functions is needed
-		yl=evaluate(y,i,0.0)
-		yr=evaluate(y,i,1.0)
-
-		col_base_idx = col_idx
-
-		if col_idx == 3
-			col_base_idx = 1
-		end
-
-		Bcl= get_basis(M,yl.x[col_base_idx],DefaultOrthonormalBasis())
-	    Bl = get_vectors(M,yl.x[col_base_idx], Bcl)
-		Bcr= get_basis(M,yr.x[col_base_idx],DefaultOrthonormalBasis())
-	    Br = get_vectors(M,yr.x[col_base_idx], Bcr)
-
-		Tcl= get_basis(N,yl.x[row_idx],DefaultOrthonormalBasis())
-	    Tl = get_vectors(N,yl.x[row_idx], Tcl)
-		Tcr= get_basis(N,yr.x[row_idx],DefaultOrthonormalBasis())
-	    Tr = get_vectors(N,yr.x[row_idx], Tcr)
-
-        # In the following, all combinations of test and basis functions have to be considered.
-		
-    	assemble_local_Jac_with_connection!(A,row_idx,col_idx,h,i,yl,yr, Bl,1,0,  			Tl,1,0, integrand, transport)	
-		#println("A1", A)
-		assemble_local_Jac_with_connection!(A,row_idx,col_idx,h,i,yl,yr, Br,0,1,  Tl,1,0, integrand, transport)	
-		#println("A2", A)
-		assemble_local_Jac_with_connection!(A,row_idx,col_idx,h,i,yl,yr, Bl,1,0,  Tr,0,1, integrand, transport)
-		#println("A3", A)
-		assemble_local_Jac_with_connection!(A,row_idx,col_idx,h,i,yl,yr, Br,0,1,  Tr,0,1, integrand, transport)	
-		#println("A4", A)
-	end
-end
-
-# ╔═╡ 9480c8ef-0f01-4983-8915-4f2513f89c5f
-function get_rhs_row!(b,row_idx,h,nCells,y,integrand)
-	CoDom = integrand.precodomain
-	# Schleife über Intervalle
-	for i in 1:nCells
-		yl=evaluate(y,i,0.0)
-		yr=evaluate(y,i,1.0)
-
-		row_base_idx = row_idx
-
-		if row_idx == 3
-			row_base_idx = 1
-		end
-
-		Tcl=get_basis(CoDom,yl.x[row_base_idx],DefaultOrthonormalBasis())
-	    Tl = get_vectors(CoDom, yl.x[row_base_idx], Tcl)
-		Tcr=get_basis(CoDom,yr.x[row_base_idx],DefaultOrthonormalBasis())
-	    Tr = get_vectors(CoDom, yr.x[row_base_idx], Tcr)
-		
-		assemble_local_rhs!(b,row_idx, h, i, yl, yr, Tl, 1, 0, integrand)		
-        assemble_local_rhs!(b,row_idx, h, i, yl, yr, Tr, 0, 1, integrand)		
-	end
-end
-
-# ╔═╡ 42b3c2e4-c06e-4a9a-acd3-5a5a06a55e9a
-function get_rhs_simplified!(b,row_idx,h,nCells,y,y_trial,integrand,transport)
-	S = integrand.precodomain
-	# Schleife über Intervalle
-	for i in 1:nCells
-			yl=evaluate(y,i,0.0)
-		    yr=evaluate(y,i,1.0)
-		
-			yl_trial=evaluate(y,i,0.0)
-			yr_trial=evaluate(y,i,1.0)
-		
-			Tcl=get_basis(S,yl.x[row_idx],DefaultOrthonormalBasis())
-			Tl=get_vectors(S, yl.x[row_idx],Tcl)
-		
-			Tcr=get_basis(S,yr.x[row_idx],DefaultOrthonormalBasis())
-	    	Tr = get_vectors(S, yr.x[row_idx], Tcr)
-		
-			dim = manifold_dimension(S)
-		# Transport der Testfunktionen auf $T_{x_k}S$
-            for k=1:dim
-				Tl[k]=transport.value(S,yl.x[row_idx],Tl[k],yl_trial.x[row_idx])
-				Tr[k]=transport.value(S,yr.x[row_idx],Tr[k],yr_trial.x[row_idx])
-			end
-
-			assemble_local_rhs!(b,row_idx, h, i, yl_trial, yr_trial, Tl, 1, 0, integrand)		
-        	assemble_local_rhs!(b,row_idx, h, i, yl_trial, yr_trial, Tr, 0, 1, integrand)		
-	end
-end
-
-# ╔═╡ b26f9e81-bd4e-48f5-b6de-31d46daac22c
-"""
- A:      Matrix to be written into\\
-row_idx: row index of block inside system\\
-detT:    degree of test function: 1: linear, 0: constant\\
-col_idx: column index of block inside system\\
-detB:    degree of basis function: 1: linear, 0: constant\\
-h:       length of interval\\
-nCell:    total number of intervals\\
-y:       iterate\\
-...
-"""
-function get_Jac_Lyy!(A,row_idx,col_idx,h, nCells,y,integrand,transport)
-	M = integrand.domain
-	N = integrand.precodomain
-	# Schleife über Intervalle
-	for i in 1:nCells
-
-		# Evaluation of the current iterate. This routine has to be provided from outside, because Knowledge about the basis functions is needed
-		yl=evaluate(y,i,0.0)
-		yr=evaluate(y,i,1.0)
-
-		#yl=ArrayPartition(getindex.(y.x, (i-1...,)))
-		#yr=ArrayPartition(getindex.(y.x, (i...,)))
-
-		Bcl=get_basis(M,yl.x[col_idx],DefaultOrthonormalBasis())
-	    Bl = get_vectors(M,yl.x[col_idx], Bcl)
-		Bcr=get_basis(M,yr.x[col_idx],DefaultOrthonormalBasis())
-	    Br = get_vectors(M,yr.x[col_idx], Bcr)
-
-		Tcl=get_basis(N,yl.x[row_idx],DefaultOrthonormalBasis())
-	    Tl = get_vectors(N,yl.x[row_idx], Tcl)
-		Tcr=get_basis(N,yr.x[row_idx],DefaultOrthonormalBasis())
-	    Tr = get_vectors(N,yr.x[row_idx], Tcr)
-
-        # In the following, all combinations of test and basis functions have to be considered.
-		
-        assemble_local_Jac_Lyy!(A,row_idx,col_idx,h,i,yl,yr, Bl,1,0, Tl,1,0, integrand, transport)		
-		assemble_local_Jac_Lyy!(A,row_idx,col_idx,h,i,yl,yr, Br,0,1, Tl,1,0, integrand, transport)		
-		assemble_local_Jac_Lyy!(A,row_idx,col_idx,h,i,yl,yr, Bl,1,0, Tr,0,1, integrand, transport)		
-		assemble_local_Jac_Lyy!(A,row_idx,col_idx,h,i,yl,yr, Br,0,1, Tr,0,1, integrand, transport)		
-
-	end
 end
 
 # ╔═╡ 1c0028e5-6beb-4512-b1f3-c18780b68ad8
@@ -740,33 +383,26 @@ function (ne::NewtonEquation)(M, VB, p)
 	println("Assemble:")
 
 	ManoptExamples.get_Jac!(evaluate,ne.A11,1,1,1,1,h,nCells,Oy,ne.integrand_Lprime,ne.VT)
-	#get_Jac!(ne.A11,1,1,h,nCells,Oy,ne.integrand_Lprime,ne.VT)
-	get_Jac_Lyy!(A11_helper,1,1,h,nCells,Oy,ne.integrand_Lyy1,ne.transport_Lyy1)
+	ManoptExamples.get_Jac_Lyy!(evaluate,A11_helper,1,1,h,nCells,Oy,ne.integrand_Lyy1,ne.transport_Lyy1)
 	ne.A11 += A11_helper + A11_helper'	
 	A11_helper *= 0.0
-	get_Jac_Lyy!(A11_helper,1,1,h,nCells,Oy,ne.integrand_Lyy2,ne.transport_Lyy2)
+	ManoptExamples.get_Jac_Lyy!(evaluate,A11_helper,1,1,h,nCells,Oy,ne.integrand_Lyy2,ne.transport_Lyy2)
 	ne.A11 += A11_helper
-	
+
 	ManoptExamples.get_Jac!(evaluate,ne.A12,1,1,2,1,h,nCells,Oy, ne.integrand_L_yu, zerotransport)
-	#ManoptExamples.get_Jac!(evaluate,ne.A23,2,1,3,1,h,nCells,Oy, ne.integrand_L_uλ, zerotransport)
-	#ManoptExamples.get_Jac!(evaluate,ne.A13,1,1,3,1,h,nCells,Oy,ne.integrand_L_λy,ne.VT)
+	#ManoptExamples.get_Jac!(evaluate,ne.A23,2,1,3,1,h,nCells,Oy, ne.integrand_L_uλ, zerotransport) siehe b3
+	ManoptExamples.get_Jac!(evaluate,ne.A23,2,1,1,1,h,nCells,Oy, ne.integrand_L_uλ, zerotransport)
+	#ManoptExamples.get_Jac!(evaluate,ne.A13,1,1,3,1,h,nCells,Oy,ne.integrand_L_λy,ne.VT) siehe b3
+	ManoptExamples.get_Jac!(evaluate,ne.A13,1,1,1,1,h,nCells,Oy,ne.integrand_L_λy,ne.VT)
 	ManoptExamples.get_Jac!(evaluate,ne.A22,2,1,2,1,h,nCells,Oy,ne.integrand_L_uu, zerotransport)
-	
-	#get_Jac!(ne.A12,1,2,h,nCells,Oy, ne.integrand_L_yu)
-	get_Jac!(ne.A23,2,3,h,nCells,Oy, ne.integrand_L_uλ)
-	get_Jac!(ne.A13,1,3,h,nCells,Oy,ne.integrand_L_λy,ne.VT)
-	#get_Jac!(ne.A22,2,2,h,nCells,Oy,ne.integrand_L_uu)
 	
 	lambda_helper = get_coordinates(powerS, p[M,1], p[M,3], DefaultOrthogonalBasis())
 	ne.b1 = ne.A13 * lambda_helper
 	
 	ManoptExamples.get_rhs_row!(evaluate,ne.b1,1,1,h,nCells,Oy,ne.integrandJ_1)
 	ManoptExamples.get_rhs_row!(evaluate,ne.b2,2,1,h,nCells,Oy,ne.integrand_L_uλ)
-	#ManoptExamples.get_rhs_row!(evaluate,ne.b3,3,1,h,nCells,Oy,ne.integrand_stateeq)
-	
-	#get_rhs_row!(ne.b1,1,h,nCells,Oy,ne.integrandJ_1)
-	#get_rhs_row!(ne.b2,2,h,nCells,Oy,ne.integrand_L_uλ)
-	get_rhs_row!(ne.b3,3,h,nCells,Oy,ne.integrand_stateeq)
+	# ManoptExamples.get_rhs_row!(evaluate,ne.b3,3,1,h,nCells,Oy,ne.integrand_stateeq) das wäre eigentlich der "richtige" Aufruf, col_idx wird per hand auf 1 (d.h. so, dass er y als Punkt verwendet und nicht λ) gesetzt)
+	ManoptExamples.get_rhs_row!(evaluate,ne.b3,1,1,h,nCells,Oy,ne.integrand_stateeq)
 	
 	#A33 = 0 
 
@@ -782,7 +418,7 @@ end
 function (ne::NewtonEquation)(M, VB, p, p_trial)
 	n1 = Int(manifold_dimension(submanifold(M, 1)))
 	n2 = Int(manifold_dimension(submanifold(M, 2)))
-	n3 = Int(manifold_dimension(submanifold(M, 3)))
+	n3 = n1
 	
 	bctrial1=zeros(n1)
 	bctrial2=zeros(n2)
@@ -796,17 +432,26 @@ function (ne::NewtonEquation)(M, VB, p, p_trial)
 	Oy3 = OffsetArray([y03, p[M, 3]..., yT3], 0:(length(ne.interval)+1))
 	Oy = ArrayPartition(Oy1,Oy2,Oy3);
 
+	projected_λ_trial = project(powerS, p_trial[M,1], p_trial[M, 3]) 
+	p_trial[M, 3] = projected_λ_trial
 	
 	Oytrial1 = OffsetArray([y01, p_trial[M,1]..., yT1], 0:(length(ne.interval)+1))
 	Oytrial2 = OffsetArray([y02, p_trial[M,2]..., yT2], 0:(length(ne.interval)+1))
 	Oytrial3 = OffsetArray([y03, p_trial[M,3]..., yT3], 0:(length(ne.interval)+1))
 	Oytrial = ArrayPartition(Oytrial1,Oytrial2,Oytrial3);
 
-	nCells = length(ne.Omega) + 1
+	nCells = length(ne.interval) + 1
 
-	get_rhs_simplified!(bctrial2,2,h,nCells,Oy,Oytrial,ne.integrand_L_yu,ne.VT)
-	get_rhs_simplified!(bctrial3,3,h,nCells,Oy,Oytrial,ne.integrand_L_uλ, zerotransport)
+	A13_trial = spzeros(n1,n3)
+	ManoptExamples.get_Jac!(evaluate,A13_trial,1,1,1,1,h,nCells,Oytrial,ne.integrand_L_λy,ne.VT)
+	
+	lambda_helper = get_coordinates(powerS, p_trial[M,1], p_trial[M,3], DefaultOrthogonalBasis())
+	bctrial1 = A13_trial * lambda_helper
 
+	ManoptExamples.get_rhs_simplified!(evaluate,bctrial1,1,1,h,nCells,Oy,Oytrial,ne.integrandJ_1, ne.VT)
+	ManoptExamples.get_rhs_simplified!(evaluate,bctrial2,2,1,h,nCells,Oy,Oytrial,ne.integrand_L_uλ, zerotransport)
+	ManoptExamples.get_rhs_simplified!(evaluate,bctrial3,1,1,h,nCells,Oy,Oytrial,ne.integrand_stateeq, ne.VT)
+	
 	return vcat(bctrial1,bctrial2, bctrial3)
 end
 end;
@@ -831,17 +476,14 @@ begin
 	NE = NewtonEquation(product, integrand_L_prime, integrand_Lyy_1, integrand_Lyy_2, integrand_Lyu, integrand_Lλy, integrand_Luλ, integrand_Luu, integrandJ1, integrand_state_eq, transport, transport_Lyy_1, transport_Lyy_2, Omega)
 
 	st_res = vectorbundle_newton(product, TangentBundle(product), NE, y_0; sub_problem=solve_in_basis_repr, sub_state=AllocatingEvaluation(),
-	stopping_criterion=(StopAfterIteration(150)|StopWhenChangeLess(product,1e-11; outer_norm=Inf)),
+	stopping_criterion=(StopAfterIteration(150)|StopWhenChangeLess(product,1e-10; outer_norm=Inf)),
 	retraction_method=pr,
-	#stepsize=Manopt.AffineCovariantStepsize(product, theta_des=0.5, outer_norm=Inf),
-	debug=[:Iteration, (:Change, "Change: %1.8e"), "\n", :Stop],
+	stepsize=Manopt.AffineCovariantStepsize(product, theta_des=0.05, outer_norm=Inf),
+	debug=[:Iteration, (:Change, "Change: %1.8e"), :Stepsize, "\n", :Stop],
 	record=[:Iterate, :Change],
 	return_state=true
 )
 end
-
-# ╔═╡ 74d553d3-b1d1-47af-ba1f-6d4297148b51
-iterates = get_record(st_res, :Iteration, :Iterate)
 
 # ╔═╡ 6920c45e-4ccb-4b60-a90c-c8df0792269c
 change = get_record(st_res, :Iteration, :Change)[2:end];
@@ -901,12 +543,14 @@ wireframe!(ax, sx, sy, sz, color = RGBA(0.5,0.5,0.7,0.1); transparency=true)
 	fig
 end
 
+# ╔═╡ f70db0f8-3398-459c-90da-00af14aa7478
+p_res
+
 # ╔═╡ e212110d-abce-4be4-8327-bd5b3a4e1329
 norm(p_res.x[2])
 
 # ╔═╡ Cell order:
 # ╠═6362be32-ed1a-11ef-0352-c7ccb14267de
-# ╠═66fbe00e-1d30-468d-baa5-05e099cf329e
 # ╠═bc7188d7-38a4-49a3-9271-c2ad4b4ebf91
 # ╠═aeceb735-da1b-4db5-9964-538b316441c2
 # ╠═c6b1fd66-ef1f-427e-b8cc-d2da9c127ee3
@@ -937,23 +581,15 @@ norm(p_res.x[2])
 # ╠═06d1f25c-80d8-4a07-8b53-3cabc90e70a9
 # ╟─8552ff47-9c4c-4028-b6cd-635f144ae522
 # ╠═4d79c53a-06ee-4c42-8fdf-678ca6a8c7e8
-# ╠═9cad614b-f5b1-43ac-a612-6f32458f7cdd
-# ╠═ea918bcc-2d49-47cf-ad9c-79ae3bc9484c
-# ╠═f7d66dfb-d246-486a-9033-ab82fde3d586
-# ╠═6cf5ad9a-b3dc-4a11-bf01-3aa188c9667b
-# ╠═c9b6a526-7b30-44bf-b652-ff9c17a4599c
-# ╠═9480c8ef-0f01-4983-8915-4f2513f89c5f
-# ╠═42b3c2e4-c06e-4a9a-acd3-5a5a06a55e9a
-# ╠═b26f9e81-bd4e-48f5-b6de-31d46daac22c
 # ╠═707d23d1-f7b4-49cb-b0ff-11ec536939fa
 # ╠═1c0028e5-6beb-4512-b1f3-c18780b68ad8
 # ╠═45141a59-510a-4177-bab1-31be7450fcd9
 # ╠═b3d4e21b-8183-4522-a463-63bef1810357
 # ╠═b7f97707-786c-47c7-a3b6-afaedf2083d3
 # ╠═5220a0dd-9484-4556-a7d0-ecf94955bf6c
-# ╠═74d553d3-b1d1-47af-ba1f-6d4297148b51
 # ╠═6920c45e-4ccb-4b60-a90c-c8df0792269c
 # ╠═921845f5-2114-4e8f-a3c4-e1677e9134dd
 # ╠═817c3466-eeb7-4979-a738-1036b719361f
 # ╠═ac49e504-f3ba-4a5e-9a66-21160f38874e
+# ╠═f70db0f8-3398-459c-90da-00af14aa7478
 # ╠═e212110d-abce-4be4-8327-bd5b3a4e1329
