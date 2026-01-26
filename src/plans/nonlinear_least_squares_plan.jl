47ff327128b344fdb19509037affebbdbc739c30
Mateusz Baran <mateuszbaran89@gmail.com>
2026-01-26 12:00:33 +0100

    a TODO and a bit of speedup

--- src/plans/nonlinear_least_squares_plan.jl
+++ src/plans/nonlinear_least_squares_plan.jl
@@ -1131,8 +1131,9 @@ function vector_field!(
     F_p_norm2 = sum(abs2, y)
     (ρ_value, ρ_prime, ρ_double_prime) = get_robustifier_values(r, F_p_norm2)
     α = 1 - sqrt(1 + 2 * (ismissing(ρ_double_prime) ? 0.0 : ρ_double_prime / ρ_prime) * F_p_norm2)
+    # TODO: truncate α to 1-ϵ to avoid division by zero
     # Compute y = (sqrt(ρ(p)) / (1-α)) F(p)
-    y .= (sqrt(ρ_prime) / (1 - α)) * y
+    y .*= sqrt(ρ_prime) / (1 - α)
     return y
 end
 
