da0e4ed9f8c1f7e6f1749e5045454fd48fb84513
Ronny Bergmann <git@ronnybergmann.net>
2026-02-19 09:11:51 +0900

    Check with Rotations, I am convinced this works now. \o/

--- temp/LM-Jacobian-test-median.jl
+++ temp/LM-Jacobian-test-median.jl
@@ -13,55 +13,43 @@ pts = [
     Rxy(-0.05) * Rxz(0.125) * Ryz(-0.25),
     Rxy(0.05) * Rxz(-0.125) * Ryz(0.25),
     #outliers
-    #Rxy(0.125)*Rxz(0.25)*Ryz(0.05),
-    #Rxy(-0.125)*Rxz(0.25)*Ryz(0.05),
+    Rxy(0.125)*Rxz(0.25)*Ryz(0.05),
+    Rxy(-0.125)*Rxz(0.25)*Ryz(0.05),
 ]
-# M = Rotations(4)
-# pts = rand(M, 5)
 p0 = copy(M, pts[2])
-# We do a full function approach here
 
-F(M, p) = [distance(M, p, q) for q in pts]
-function JF(M, p, B::AbstractBasis = DefaultOrthonormalBasis())
-    d = manifold_dimension(M)
-    n = length(pts)
-    J = zeros(n, d)
-    for (i, q) in enumerate(pts)
-        dpq = distance(M, p, q)
-        if dpq > 0
-            J[i, :] = -1 / dpq .* get_coordinates(M, p, log(M, p, q), B)
-        end
-    end
-    return J
-end
+Fi = [ (M, p) -> distance(M, p, q) for q in pts]
+grad_Fi = [ (M, p) -> distance(M, p, q) == 0 ? zero_vector(M, p) : (- log(M, p, q) / distance(M, p, q)) for q in pts]
 
-f = VectorGradientFunction(
-    F, JF, length(pts);
-    evaluation = AllocatingEvaluation(),
-    function_type = FunctionVectorialType(),
-    jacobian_type = CoefficientVectorialType(DefaultOrthonormalBasis())
-)
+# Block s normal ones
+Fs = [VectorGradientFunction(
+    [Fi[i]], [grad_Fi[i]], 1;
+    evaluation = AllocatingEvaluation(), function_type = ComponentVectorialType(), jacobian_type = ComponentVectorialType()
+) for i in eachindex(pts)]
+rs = fill((1e-4) ∘ HuberRobustifier(), length(Fs))
+
+#
+#
+# TODO: Try an approach with Jacobians in bases - then 2 should be faster
 
 qc = median(M, pts)
 cost(M, p) = sum(distance(M, p, q) for q in pts)
 
-
 # Default Residual CG on this approach – works but probably allocates a bit too much (matrices coordinates/vector...)
 q1 = LevenbergMarquardt(
-    M, [f], p0;
-    β = 2.0, η = 0.01, damping_term_min = 1.0e-5, ε = 1.0e-1, α_mode = :Strict,
-    robustifier = [HuberRobustifier()],
-    debug = [:Iteration, :Cost, " ", :Change, " ", :damping_term, "\n", :Stop],
-    stopping_criterion = StopWhenGradientNormLess(1.0e-16) | StopAfterIteration(100)
+    M, Fs, p0;
+    β = 8.0, η = 0.2, damping_term_min = 1.0e-5, ε = 1.0e-1, α_mode = :Strict,
+    robustifier = rs,
+    debug = [:Iteration, :Cost, " ", :Change, " ", :damping_term, "\n", :Stop, 25],
 )
 # ... but works
 @info "Cost of median (qc) $(cost(M, qc)), Cost of LM (q1): $(cost(M, q1)), difference (of q1 - qc): $(cost(M, q1) - cost(M, qc))"
 
 q2 = LevenbergMarquardt(
-    M, [f], p0;
-    β = 2.0, η = 0.2, damping_term_min = 1.0e-5, ε = 0.5,
-    robustifier = [HuberRobustifier()],
-    debug = [:Iteration, (:Cost, "f(x): %8.8e "), :damping_term, "\n", :Stop],
+    M, Fs, p0;
+    β = 8.0, η = 0.2, damping_term_min = 1.0e-5, ε = 1.0e-1, α_mode = :Strict,
+    robustifier = rs,
+    debug = [:Iteration, (:Cost, "f(x): %8.8e "), :damping_term, "\n", :Stop, 25],
     sub_state = CoordinatesNormalSystemState(M),
 )
 # ... but works
@@ -71,21 +59,18 @@ q1b = copy(M, p0)
 
 (
     @b LevenbergMarquardt!(
-        M, [f], q1b;
-        β = 8.0, η = 0.2, damping_term_min = 1.0e-5,
-        robustifier = [HuberRobustifier()]
+        M, Fs, q1b; β = 8.0, η = 0.2, damping_term_min = 1.0e-5, robustifier = rs
     )
 ) |> repr |> println
 
 @info "Distance from alloc q1 to in place q1b (from benchmark) $(distance(M, q1, q1b))"
 
 q2b = copy(M, p0)
-# TODO: Still errors on first step?
 (
     @b LevenbergMarquardt!(
-        M, [f], q2b;
+        M, Fs, q2b;
         β = 8.0, η = 0.2, damping_term_min = 1.0e-5,
-        robustifier = [HuberRobustifier()], sub_state = CoordinatesNormalSystemState(M),
+        robustifier = rs, sub_state = CoordinatesNormalSystemState(M),
     )
 ) |> repr |> println
 
