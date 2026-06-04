aa2819ec7315cddbcb87d765c0c53ca52c2b5a6f
Ronny Bergmann <git@ronnybergmann.net>
2026-06-04 14:14:53 +0200

    rephrase the SOn examples and add proper benchmark, Start Grassmann, which still needs a new differential and adjoint differential

--- temp/LM-robust-Procrustes_SO3.jl
+++ temp/LM-robust-Procrustes_SO3.jl
@@ -1,4 +1,4 @@
-using Manopt, Manifolds, LinearAlgebra
+using Chairmarks, Manopt, Manifolds, LinearAlgebra
 
 raw"""
     generate_data(d)
@@ -40,17 +40,21 @@ f(M, p; A, B) = sum(norm(A[:, i] - p * B[:, i]) for i in 1:size(A, 2))
 
 """
     Fi(M, p; i, A, B)
+    Fi!(M, v, p; i, A, B)
 
 For given matrices ``A, B ∈ ℝ^{d,n}`` compute the residual of the ith column
 
 ```math
 F_i(p) = (A - pB)_i = a_i - pb_i
 ```
+
+This can be computed in-place of `v`
 """
 Fi(M, p; i, A, B) = A[:, i] - p * B[:, i]
-Fi!(M, c, p; i, A, B) = (c .= A[:, i] .- p * B[:, i])
+Fi!(M, v, p; i, A, B) = (v .= A[:, i] .- p * B[:, i])
 
 """
+    DFi(M, p, X; i, A, B)
     DFi!(M, y, p, X; i, A, B)
 
 For given matrices ``A, B ∈ ℝ^{d,n}`` compute the differential of the residual of the ith column
@@ -63,12 +67,12 @@ with respect to the rotation ``p`` that is, ``X ∈ 𝔰𝔬(d)`` which reads
 This is computed in-place of `y`
 
 """
-DFi!(M, y, p, X; i, A, B) = (y .= - p * X * B[:, i])
-
 DFi(M, p, X; i, A, B) = - p * X * B[:, i]
+DFi!(M, y, p, X; i, A, B) = (y .= - p * X * B[:, i])
 
 
 raw"""
+    adjointDFi(M, p, y; i, A, B)
     adjointDFi!(M, X, p, y; i, A, B)
 
 For given matrices ``A, B ∈ ℝ^{d,n}`` compute the adjoint differential of the residual of the ith column
@@ -81,24 +85,32 @@ D*{F_i}(p)[y] = -\mathrm{skew}(p^{\mathrm{T}}yb_i^{\mathrm{T}})
 
 This is computed in-place of `X`.
 """
-adjointDFi!(M, a, p, y; i, A, B) = (a .= - skew(p' * y * B[:, i]'))
-
 adjointDFi(M, p, y; i, A, B) = - skew(p' * y * B[:, i]')
+adjointDFi!(M, a, p, y; i, A, B) = (a .= - skew(p' * y * B[:, i]'))
 
 
 #
 #
 # A first experiment
 
+"""
+    rotation_matrix(d, i, j, α)
+
+Create the rotation matrix in ``ℝ^{d×d}`` with a rotation in the ``i,j``-plance of an angle of `α`.
+"""
+function rotation_matrix(d, i, j, α)
+    R = Matrix{Float64}(I, d, d)
+    R[i, i] = cos(α)
+    R[j, i] = cos(α)
+    R[j, i] = -sin(α)
+    R[i, j] = sin(α)
+    return R
+end
+
 d = 3
-A = generate_data(3)
+A = generate_data(d)
 n = size(A, 2) # number of summands in the vectorial cost sum
-p_star = Matrix{Float64}(I, d, d)
-# Lets take a very simple rotation for now
-p_star[1, 1] = cos(π / 4)
-p_star[2, 2] = cos(π / 4)
-p_star[2, 1] = -sin(π / 4)
-p_star[1, 2] = sin(π / 4)
+p_star = rotation_matrix(d, 1, 2, π / 4)
 
 B = copy(A)
 # and generate a few outliers
@@ -127,31 +139,33 @@ vgfs = [
             (M, y, p, X) -> DFi!(M, y, p, X; i = i, A = A, B = B),
             (M, X, p, y) -> adjointDFi!(M, X, p, y; i = i, A = A, B = B),
             d;
-            function_type = FunctionVectorialType(),
-            jacobian_type = FunctionVectorialType(),
-            adjoint_jacobian_type = FunctionVectorialType(),
-            evaluation = InplaceEvaluation(),
+            function_type = FunctionVectorialType(), jacobian_type = FunctionVectorialType(),
+            adjoint_jacobian_type = FunctionVectorialType(), evaluation = InplaceEvaluation(),
         ) for i in 1:n
 ]
-rs = [ 1.0e-6 ∘ HuberRobustifier() for _ in 1:n ]
+rs = [ 1.0e-7 ∘ HuberRobustifier() for _ in 1:n ]
 
-M = Rotations(3)
+M = Rotations(d)
 # Start with the identity
 p0 = Matrix{Float64}(I, d, d)
 
 # Least Squares Hubertized
 p1 = LevenbergMarquardt(
     M, vgfs, p0; robustifier = rs,
-    damping_increase_factor = 2.0, candidate_acceptance_threshold = 0.2, damping_term_min = 1.0e-3,
-    debug = [:Iteration, (:Cost, "f(x): %8.8e "), :damping_term, :GradientNorm, "\n", :Stop],
+    damping_increase_factor = 4.0, candidate_acceptance_threshold = 0.2, damping_term_min = 1.0e-7,
+    debug = [
+        # :Iteration, (:Cost, "f(x): %8.8e "), :damping_term, :GradientNorm, "\n",
+        :Stop,
+    ],
 )
 @info "LM time"
-@time LevenbergMarquardt(M, vgfs, p0; robustifier = rs,
-    damping_increase_factor = 2.0, candidate_acceptance_threshold = 0.2, damping_term_min = 1.0e-3,
+time1 = @be LevenbergMarquardt(
+    $M, $vgfs, $p0; robustifier = $rs,
+    damping_increase_factor = 4.0, candidate_acceptance_threshold = 0.2, damping_term_min = 1.0e-7,
 )
-
+show(stdout, MIME"text/plain"(),time1)
 p2 = mesh_adaptive_direct_search(M, (M, p) -> f(M, p; A = A, B = B), p0; debug = [:Stop])
 @info "LTMADS time"
-@time mesh_adaptive_direct_search(M, (M, p) -> f(M, p; A = A, B = B), p0)
-
+time2 = @be mesh_adaptive_direct_search($M, $((M, p) -> f(M, p; A = A, B = B)), $p0)
+show(stdout, MIME"text/plain"(),time2)
 @info "Solution difference: $(distance(M, p1, p2)); costs LM: $(f(M, p1; A = A, B = B)) LTMADS: $(f(M, p2; A = A, B = B))"
