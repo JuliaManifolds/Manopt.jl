c27ec601417da4957fb77b6bf7e87c5b674ca3f4
Ronny Bergmann <git@ronnybergmann.net>
2026-07-08 17:04:26 +0200

    Extend docs and write a small processor.

--- src/base/state/callback.jl
+++ src/base/state/callback.jl
@@ -5,7 +5,9 @@ const _MANOPT_EMPTY_ANY_CALLBACK = (symbol::Symbol, problem, state, iteration) -
 """
     available_callbacks(state::AbstractManoptSolverState)
 
-For a given state, indicate, which callbacks are stored.
+For a given state, indicate, which callbacks are in use, i.e. stored and also called from this solver.
+
+See also: [`possible_callbacks`](@ref).
 """
 function available_callbacks(state::AbstractManoptSolverState)
     ac = intersect(keys(get_callbacks(state)), possible_callbacks(typeof(state)))
@@ -52,9 +54,45 @@ _get_callbacks(state::AbstractManoptSolverState, ::Val{true}) = get_iterate(stat
 """
     possible_callbacks(state_type::Type{S}) where {S<:AbstractManoptSolverState})
 
-For a solver of type `S` return the callbacks actually available by returning a vector
+For a solver of type `S` return the callbacks actually uses in practive, i.e. the ones
+which from within the solver are actually called. This function returns a vector
 `Symbol`s that can be used.
 """
 function possible_callbacks(::Type{S}) where {S <: AbstractManoptSolverState}
     return _MANOPT_DEFAULT_CALLBACKS
+end
+
+"""
+    process_callbacks_arg(callbacks)
+
+Given an array `callbacks` a user has passed to a solver, this helper function processes the
+array in the following way
+
+* a pair `:Hook => fct` is kept as is, where the `fct`` can also be some callable structure
+* a single element `function` is turned into `:Any => fct` allowing the case a single callback
+  to be specified just with the function
+* a pair `[Hook1, Hook2] => fct` is a shortcut for an array of pairs and split into these here.
+
+The result is then wrapped into a dictionary. Be aware that from an array of pairs this function
+reduces, the dictionary “takes” the last `:Hook` pair as the entry in the dictionary. This
+function does not check for duplicates.
+"""
+function process_callbacks_arg(callbacks::Array)
+    c = Pair{Symbol, Any}[]
+    for cb in callbacks
+        if cb isa Pair
+            if cb[1] isa Symbol
+                push!(c, cb)
+            elseif cb[1] isa AbstractVector{Symbol}
+                for s in cb[1]
+                    push!(c, s => cb[2])
+                end
+            else
+                error("Unkown key $(cb[1])")
+            end
+        else
+            push!(c, :Any => cb)
+        end
+    end
+    return Dict(c...)
 end
