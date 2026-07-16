01b354c638ca6c3360032988ec3c3ce27d507329
Mateusz Baran <mateuszbaran89@gmail.com>
2026-07-16 09:36:21 +0200

    improve coverage

--- test/base/test_callbacks.jl
+++ test/base/test_callbacks.jl
@@ -1,5 +1,7 @@
 using Manifolds, Manopt, Test
 
+struct DummyState <: AbstractManoptSolverState end
+
 @testset "Test callback utilities" begin
     # (a) providing a symbol and an array of symbol keys, expands the array
     cb = [:A => sin, [:B, :C] => cos]
@@ -9,4 +11,11 @@ using Manifolds, Manopt, Test
     @test_throws ArgumentError Manopt.process_callbacks_arg(["A" => sin])
     # test warning for unknown callbacks – GD does not have a :A callback
     @test_logs (:warn,) Manopt.process_callbacks_arg([:A => sin], GradientDescentState)
+
+    @test Manopt._get_callbacks(DummyState(), Val(false)) == Dict{Symbol, Any}()
+
+    M = Euclidean(2)
+    f(M, x) = norm(x)^2
+    dmp = DefaultManoptProblem(M, ManifoldCostObjective(f))
+    @test Manopt._MANOPT_EMPTY_ANY_CALLBACK(:Any, dmp, DummyState(), 1) === nothing
 end
