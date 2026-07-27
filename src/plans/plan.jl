fc6ac2d0f8c2c6aaf7fdc3ff6231a020e6fefde6
Ronny Bergmann <git@ronnybergmann.net>
2026-07-27 15:45:58 +0200

    Organise to do list: 20 files in the plan to do. Probably mainly only embedded and scaled objective left (maybe cache&count) until Manopt compiles again.

--- src/plans/plan.jl
+++ src/plans/plan.jl
@@ -1,17 +1,10 @@
 include("stopping_criterion.jl")
-
 include("stepsize/initial_guess.jl")
 include("stepsize/stepsize_message.jl")
 include("stepsize/linesearch.jl")
 include("stepsize/stepsize.jl")
-
-# Generic plans I: based on objective structure
 include("hessian_plan.jl")
-
-# Linear systems
 include("conjugate_residual_plan.jl")
-
-# Specific solver plans
 include("alternating_gradient_plan.jl")
 include("augmented_lagrangian_plan.jl")
 include("conjugate_gradient_plan.jl")
@@ -19,13 +12,9 @@ include("exact_penalty_method_plan.jl")
 include("interior_point_Newton_plan.jl")
 include("quasi_newton_plan.jl")
 include("difference_of_convex_plan.jl")
-
 include("stochastic_gradient_plan.jl")
-
 include("box_plan.jl")
-
 include("embedded_objective.jl")
 include("scaled_objective.jl")
-
 include("cache.jl")
 include("count.jl")
