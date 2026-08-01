d63cdfdff8554f8718b23ba039bf2a05f7f56bcb
Ronny Bergmann <git@ronnybergmann.net>
2026-08-01 14:16:22 +0200

    Runs runic.

--- docs/make.jl
+++ docs/make.jl
@@ -162,7 +162,7 @@ makedocs(;
         size_threshold_warn = 900 * 2^10, # raise from 500 KiB to 1.1 MB (for search index)
         search_size_threshold_warn = 2000 * 2^10,
     ),
-#= #Consider switching to Vitepress in the end?
+    #= #Consider switching to Vitepress in the end?
     format = DocumenterVitepress.MarkdownVitepress(
         repo = "github.com/JuliaManifolds/Manopt.jl",
         devbranch = "master",
